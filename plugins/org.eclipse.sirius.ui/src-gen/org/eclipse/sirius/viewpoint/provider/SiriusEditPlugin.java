595e816ce4bc965a3236ff117ebf98e1117a5ff9
Laurent Redor <laurent.redor@obeo.fr>
2013-10-09 17:11:00 +0200

    Remove useless copyright fields in generated ui code.
    This has already be done in org.eclise.sirius but not in
    org.eclise.sirius.ui.
    
    Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>

--- plugins/org.eclipse.sirius.ui/src-gen/org/eclipse/sirius/viewpoint/provider/SiriusEditPlugin.java
+++ plugins/org.eclipse.sirius.ui/src-gen/org/eclipse/sirius/viewpoint/provider/SiriusEditPlugin.java
@@ -77,13 +77,6 @@ import org.osgi.framework.BundleContext;
  */
 public final class SiriusEditPlugin extends EMFPlugin {
     /**
-     * <!-- begin-user-doc --> <!-- end-user-doc -->
-     * 
-     * @generated
-     */
-    public static final String copyright = "Copyright (c) 2007, 2013 THALES GLOBAL SERVICES.\nAll rights reserved. This program and the accompanying materials\nare made available under the terms of the Eclipse Public License v1.0\nwhich accompanies this distribution, and is available at\nhttp://www.eclipse.org/legal/epl-v10.html\n\nContributors:\n   Obeo - initial API and implementation\n";
-
-    /**
      * Keep track of the singleton. <!-- begin-user-doc --> <!-- end-user-doc
      * -->
      * 
