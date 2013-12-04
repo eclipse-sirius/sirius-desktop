48693804124fa95627e94fe445d9abe904864600
Maxime PORHEL <maxime.porhel@obeo.fr>
2013-12-04 18:23:52 +0100

    [423576] Add version tags during preSave
    This should be done on Group and DAnalysis to remember the creation
    version. Otherwise, the next loads will trigger the migration (until the
    next save).
    
    This is done in resource handlers during preSave. eDeliver has been
    deactivated during the version modification to avoid to trigger
    notification while save is performed.
    
    Correctly launch the indirectly collapsed node migration. It has been
    introduced in 6.5.3 et 6.7.0 versions. The migration application
    condition must trigger the migration for version < 6.5.3 or version
    between 6.6.0 and 6.7. The sequence migration part has also been
    modified.
    
    Cherry-picked from 9c2c27d1896b53c20069f92ebd1af0d59325bb7c
    Signed-off-by: Maxime PORHEL <maxime.porhel@obeo.fr>

--- plugins/org.eclipse.sirius/src/org/eclipse/sirius/business/internal/migration/RepresentationsFileResourceHandler.java
+++ plugins/org.eclipse.sirius/src/org/eclipse/sirius/business/internal/migration/RepresentationsFileResourceHandler.java
@@ -11,10 +11,13 @@
 package org.eclipse.sirius.business.internal.migration;
 
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Map;
 
 import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.emf.ecore.xmi.impl.BasicResourceHandler;
+import org.eclipse.sirius.viewpoint.DAnalysis;
+import org.osgi.framework.Version;
 
 /**
  * Implementation for viewpoint migration post-treatment. Delegates to
@@ -37,10 +40,32 @@ public class RepresentationsFileResourceHandler extends BasicResourceHandler {
         this.loadedVersion = loadedVersion;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void postLoad(XMLResource resource, InputStream inputStream, Map<?, ?> options) {
         RepresentationsFileMigrationService.getInstance().postLoad(resource, loadedVersion);
         super.postLoad(resource, inputStream, options);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void preSave(XMLResource resource, OutputStream outputStream, Map<?, ?> options) {
+        super.preSave(resource, outputStream, options);
+
+        if (!resource.getContents().isEmpty() && resource.getContents().get(0) instanceof DAnalysis) {
+            DAnalysis analysis = (DAnalysis) resource.getContents().get(0);
+            Version lastMigrationVersion = RepresentationsFileMigrationService.getInstance().getLastMigrationVersion();
+
+            boolean previousDeliver = analysis.eDeliver();
+            analysis.eSetDeliver(false);
+
+            analysis.setVersion(lastMigrationVersion.toString());
+
+            analysis.eSetDeliver(previousDeliver);
+        }
+    }
 }
