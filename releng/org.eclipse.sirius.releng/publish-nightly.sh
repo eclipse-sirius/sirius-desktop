56ae1c6e848b5e9d04e9485c06cf7d881895cb56
Pierre-Charles David <pierre-charles.david@obeo.fr>
2013-12-06 12:05:00 +0100

    [build] Add a script to publish "nighly" builds"
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>
    Change-Id: I67a73a01bc9a4e63ac23ba6ce5d4bffd73548438

---
+++ releng/org.eclipse.sirius.releng/publish-nightly.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+# ====================================================================
+# Copyright (c) 2013 Obeo
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#    Obeo - initial API and implementation
+# ====================================================================
+
+[ -z "$WORKSPACE"  -o -z "$PLATFORM" -o -z "$GIT_BRANCH" ] && {
+     echo "Execution aborted.
+
+One or more of the required variables is not set. They are normally
+provided by the Hudson build.
+
+- WORKSPACE  : the build workspace root.
+- PLATFORM   : the name of the target Eclipse release (e.g. kepler).
+- GIT_BRANCH : the name fo the Git branch being build/published.
+"
+    exit 1
+}
+
+######################################################################
+# Setup
+######################################################################
+
+# Exit on error
+set -e
+
+# The full version (should be taken as an argument)
+export VERSION="1.0.0"
+
+# The type of build being published
+export BUILD_TYPE="nightly"
+export BUILD_TYPE_PREFIX="N"
+
+# The root folder for all Sirius udpate sites
+export SIRIUS_UPDATES_ROOT="/home/data/httpd/download.eclipse.org/sirius/updates"
+
+# Streams are of the form 1.0.x: only keep major and minor version number parts
+export STREAM=$(echo "$VERSION" | sed -r -e 's/^([0-9]+\.[0-9]+\.).*$/\1x/')
+
+# The short version, common to all versions in that stream
+export SHORT_VERSION=$(echo "$VERSION" | sed -r -e 's/^([0-9]+\.[0-9]+)\..*$/\1/')
+
+# Converts the Hudson BUILD_ID (e.g. 2013-10-15_07-07-07) into the
+# syntax we want for our update-sites (e.g. 20131015-070707)
+export BUILD_TIMESTAMP=$(echo "$BUILD_ID" | sed -e 's/-//g' -e 's/_/-/')
+
+# The full version for this build, e.g. 0.9.0-N20131015-070707
+export FULL_VERSION="${VERSION}-${BUILD_TYPE_PREFIX}${BUILD_TIMESTAMP}"
+
+# The root folder where all the builds of the same type as this one
+# are published
+export TARGET_ROOT="$SIRIUS_UPDATES_ROOT/$BUILD_TYPE"
+
+# The folder for this particular build
+export TARGET_DIR="$TARGET_ROOT/$FULL_VERSION/$PLATFORM"
+
+######################################################################
+# Publish the build
+######################################################################
+
+# Ensure the target folder exists
+mkdir -p "$TARGET_DIR"
+# The actual publication of the p2 repo produced by the build
+cp -a "$WORKSPACE"/packaging/org.eclipse.sirius.update/target/repository/* "$TARGET_DIR"
+# Also publish a dump of the build environment, may be useful to debug
+env > "$TARGET_DIR/build_env.txt"
+
+######################################################################
+# Setup or update the symlinks to the build
+######################################################################
+
+# First, a link for the $VERSION (e.g. "1.2.0/luna" => "1.2.0-NYYYYMMDD-HHMM/luna")
+mkdir -p "$TARGET_ROOT/$VERSION"
+rm -f    "$TARGET_ROOT/$VERSION/$PLATFORM"
+ln -s    "../../$FULL_VERSION/$PLATFORM" "$TARGET_ROOT/$VERSION/$PLATFORM"
+
+# Also update the global "latest" links if we are building master
+if [ "master" = "$GIT_BRANCH" ]; then
+    mkdir -p "$TARGET_ROOT/latest"
+    rm -f    "$TARGET_ROOT/latest/$PLATFORM"
+    ln -s    "../../$FULL_VERSION/$PLATFORM" "$TARGET_ROOT/latest/$PLATFORM"
+fi
