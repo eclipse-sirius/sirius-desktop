2673390e301deb5af4672bb33392f391207e3a03
Maxime PORHEL <maxime.porhel@obeo.fr>
2014-04-03 17:54:39 +0200

    [431359] Remove inheritance to GMFResourceFactory
    The little code we needed was directly copied in our own
    AirDResourceFactory.
    
    Bug: 431359
    Change-Id: I53d1e59d8365d9e2c6ee3019a27090cc0791535f
    Signed-off-by: Maxime PORHEL <maxime.porhel@obeo.fr>
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/business/internal/parser/AirDResourceFactory.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/business/internal/parser/AirDResourceFactory.java
@@ -7,6 +7,8 @@
  *
  * Contributors:
  *    Obeo - initial API and implementation
+ *    IBM Corporation and others - The code for default load/save options was lifted
+ *      from GMF's org.eclipse.gmf.runtime.emf.core.resources.GMFResourceFactory.
  *******************************************************************************/
 package org.eclipse.sirius.diagram.ui.business.internal.parser;
 
@@ -18,9 +20,9 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.xmi.XMIResource;
 import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMLParserPoolImpl;
-import org.eclipse.gmf.runtime.emf.core.internal.util.EMFCoreConstants;
-import org.eclipse.gmf.runtime.emf.core.resources.GMFResourceFactory;
 import org.eclipse.sirius.business.internal.migration.RepresentationsFileExtendedMetaData;
 import org.eclipse.sirius.business.internal.migration.RepresentationsFileMigrationService;
 import org.eclipse.sirius.business.internal.migration.RepresentationsFileResourceHandler;
@@ -30,16 +32,63 @@ import org.osgi.framework.Version;
 
 import com.google.common.base.Predicates;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Maps;
 
 /**
  * A resource factory decorator to set XMI encodings.
  * 
  * @author ymortier
  */
-public class AirDResourceFactory extends GMFResourceFactory {
+public class AirDResourceFactory extends XMIResourceFactoryImpl {
+
+    private static final String XMI_ENCODING = "UTF-8"; //$NON-NLS-1$
+
+    // default load options.
+    private static final Map<Object, Object> DEFAULT_LOAD_OPTIONS = Maps.newHashMap();
+
+    // default save options.
+    private static final Map<Object, Object> DEFAULT_SAVE_OPTIONS = Maps.newHashMap();
+
     private RepresentationsFileExtendedMetaData extendedMetaData;
 
     private RepresentationsFileResourceHandler resourceHandler;
+
+    static {
+
+        XMIResource resource = new XMIResourceImpl();
+
+        // default load options.
+        DEFAULT_LOAD_OPTIONS.putAll(resource.getDefaultLoadOptions());
+        DEFAULT_LOAD_OPTIONS.put(XMIResource.OPTION_LAX_FEATURE_PROCESSING, Boolean.TRUE);
+
+        // default save options.
+        DEFAULT_SAVE_OPTIONS.putAll(resource.getDefaultSaveOptions());
+        DEFAULT_SAVE_OPTIONS.put(XMIResource.OPTION_DECLARE_XML, Boolean.TRUE);
+        DEFAULT_SAVE_OPTIONS.put(XMIResource.OPTION_PROCESS_DANGLING_HREF, XMIResource.OPTION_PROCESS_DANGLING_HREF_DISCARD);
+        DEFAULT_SAVE_OPTIONS.put(XMIResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
+        DEFAULT_SAVE_OPTIONS.put(XMIResource.OPTION_USE_XMI_TYPE, Boolean.TRUE);
+        DEFAULT_SAVE_OPTIONS.put(XMIResource.OPTION_SAVE_TYPE_INFORMATION, Boolean.TRUE);
+        DEFAULT_SAVE_OPTIONS.put(XMIResource.OPTION_SKIP_ESCAPE_URI, Boolean.FALSE);
+        DEFAULT_SAVE_OPTIONS.put(XMIResource.OPTION_ENCODING, XMI_ENCODING);
+    }
+
+    /**
+     * Get default load options.
+     * 
+     * @return the default load options
+     */
+    public static Map<Object, Object> getDefaultLoadOptions() {
+        return DEFAULT_LOAD_OPTIONS;
+    }
+
+    /**
+     * Get default save options.
+     * 
+     * @return the default save options
+     */
+    public static Map<Object, Object> getDefaultSaveOptions() {
+        return DEFAULT_SAVE_OPTIONS;
+    }
 
     /**
      * 
@@ -64,8 +113,8 @@ public class AirDResourceFactory extends GMFResourceFactory {
         setLoadOptions(resource, migrationIsNeeded);
         setSaveOptions(resource, migrationIsNeeded);
 
-        if (!resource.getEncoding().equals(EMFCoreConstants.XMI_ENCODING)) {
-            resource.setEncoding(EMFCoreConstants.XMI_ENCODING);
+        if (!resource.getEncoding().equals(XMI_ENCODING)) {
+            resource.setEncoding(XMI_ENCODING);
         }
 
         if (!Iterables.any(resource.eAdapters(), Predicates.instanceOf(AirDCrossReferenceAdapter.class))) {
@@ -102,7 +151,7 @@ public class AirDResourceFactory extends GMFResourceFactory {
         options.put(XMLResource.OPTION_DEFER_IDREF_RESOLUTION, true);
         options.put(XMLResource.OPTION_USE_DEPRECATED_METHODS, false);
         options.put(XMLResource.OPTION_USE_PARSER_POOL, new XMLParserPoolImpl(true));
-        options.put(XMLResource.OPTION_USE_XML_NAME_TO_FEATURE_MAP, new HashMap<Object, Object>());
+        options.put(XMLResource.OPTION_USE_XML_NAME_TO_FEATURE_MAP, Maps.newHashMap());
 
         // extendedMetaData and resourceHandler
 
