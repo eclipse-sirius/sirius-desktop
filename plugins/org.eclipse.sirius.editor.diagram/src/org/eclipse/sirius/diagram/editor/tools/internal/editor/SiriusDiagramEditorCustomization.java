1818c687dd4bea8ce535abe3a4d5f2dabc40aa19
Maxime PORHEL <maxime.porhel@obeo.fr>
2014-05-27 11:54:51 +0200

    [435879] Do not expose new child menus for unsupported diagram structure
    Bug: 435879
    Change-Id: I1978fd669a80818b4615b0d6e443004a8dcc806c
    Signed-off-by: Maxime PORHEL <maxime.porhel@obeo.fr>

--- plugins/org.eclipse.sirius.editor.diagram/src/org/eclipse/sirius/diagram/editor/tools/internal/editor/SiriusDiagramEditorCustomization.java
+++ plugins/org.eclipse.sirius.editor.diagram/src/org/eclipse/sirius/diagram/editor/tools/internal/editor/SiriusDiagramEditorCustomization.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 THALES GLOBAL SERVICES.
+ * Copyright (c) 2014 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,9 +10,13 @@
  *******************************************************************************/
 package org.eclipse.sirius.diagram.editor.tools.internal.editor;
 
+import java.util.LinkedHashSet;
+
 import org.eclipse.emf.ecore.EModelElement;
 import org.eclipse.sirius.diagram.description.DescriptionPackage;
+import org.eclipse.sirius.diagram.description.style.StylePackage;
 import org.eclipse.sirius.editor.tools.api.editor.EditorCustomization;
+import org.eclipse.sirius.viewpoint.description.tool.ToolPackage;
 
 /**
  * We deactivate the tool section creation menu on DiagramDescription. We
@@ -22,11 +26,32 @@ import org.eclipse.sirius.editor.tools.api.editor.EditorCustomization;
  * 
  */
 public class SiriusDiagramEditorCustomization implements EditorCustomization {
+
+    // Features referenced in this list will not have corresponding new child creation menus.
+    private final LinkedHashSet<EModelElement> deprecation;
+
+    /**
+     * Create the customization.
+     */
+    public SiriusDiagramEditorCustomization() {
+        deprecation = new LinkedHashSet<EModelElement>();
+        deprecation.add(DescriptionPackage.eINSTANCE.getDiagramDescription_NodeMappings());
+        deprecation.add(DescriptionPackage.eINSTANCE.getDiagramDescription_EdgeMappings());
+        deprecation.add(DescriptionPackage.eINSTANCE.getDiagramDescription_ContainerMappings());
+        deprecation.add(DescriptionPackage.eINSTANCE.getDiagramDescription_EdgeMappingImports());
+        deprecation.add(DescriptionPackage.eINSTANCE.getDiagramDescription_ToolSection());
+        deprecation.add(DescriptionPackage.eINSTANCE.getDiagramDescription_DiagramInitialisation());
+        deprecation.add(DescriptionPackage.eINSTANCE.getDiagramDescription_ReusedTools());
+        deprecation.add(DescriptionPackage.eINSTANCE.getDiagramDescription_ReusedMappings());
+        deprecation.add(StylePackage.eINSTANCE.getDotDescription());
+        deprecation.add(ToolPackage.eINSTANCE.getSetObject());
+    }
+
     /**
      * {@inheritDoc}
      */
-    public boolean isHidden(EModelElement metaElement) {
-        return metaElement.equals(DescriptionPackage.eINSTANCE.getDiagramDescription_ToolSection());
+    public boolean isHidden(final EModelElement metaElement) {
+        return deprecation.contains(metaElement);
     }
 
     /**
