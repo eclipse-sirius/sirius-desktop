e7a4bc533be313a4e24868aa89f7a771039984b4
Maxime PORHEL <maxime.porhel@obeo.fr>
2014-06-03 14:24:16 +0200

    [436427] Create a New Reorder menu
    Bug: 436427
    Change-Id: Id4a4ded438da37fbda7e5b7badd1ab7a5fb14abf
    Signed-off-by: Maxime PORHEL <maxime.porhel@obeo.fr>

---
+++ plugins/org.eclipse.sirius.editor.sequence/src/org/eclipse/sirius/diagram/sequence/editor/tools/internal/menu/child/ReorderToolsMenuBuilder.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.diagram.sequence.editor.tools.internal.menu.child;
+
+import org.eclipse.sirius.diagram.sequence.description.tool.ToolPackage;
+import org.eclipse.sirius.editor.tools.api.menu.AbstractTypeRestrictingMenuBuilder;
+
+/**
+ * The reorder tools menu.
+ * 
+ * @author mporhel
+ * 
+ */
+public class ReorderToolsMenuBuilder extends AbstractTypeRestrictingMenuBuilder {
+    /**
+     * build the menu.
+     */
+    public ReorderToolsMenuBuilder() {
+        super();
+        addValidType(ToolPackage.eINSTANCE.getReorderTool());
+        addValidType(ToolPackage.eINSTANCE.getInstanceRoleReorderTool());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getLabel() {
+        return "New Reorder";
+    }
+
+}
