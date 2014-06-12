276633164f66d41d412c884b1c2dbac05595c450
Pierre-Charles David <pierre-charles.david@obeo.fr>
2014-06-12 12:07:06 +0200

    [430536] Add script to retrieve the URI of the latest platform I-build
    Bug: 430536
    Change-Id: I638d7f5e6a3084630bbb7df8a71d1a5349880056
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

---
+++ releng/org.eclipse.sirius.releng/latest-ibuild.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+BASE="http://download.eclipse.org/eclipse/updates/4.4-I-builds"
+TEMPFILE=$(tempfile -s .jar) || exit
+curl -s "$BASE/compositeContent.jar" > "$TEMPFILE"
+LATEST=$(unzip -p "$TEMPFILE" compositeContent.xml | grep "<child location='I" | sort -r | head -1 | sed -e "s/\s*<child location='//" -e "s|'/>\s*||")
+echo "$BASE/$LATEST"
+rm "$TEMPFILE"
