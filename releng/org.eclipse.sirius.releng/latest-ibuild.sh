4904378dc0f967c0bf6dedf943033d71069c29c3
Pierre-Charles David <pierre-charles.david@obeo.fr>
2014-06-12 12:52:39 +0200

    [430536] Use mktemp instead of tempfile in script
    Change-Id: I80142356d4ec168d42d21659ae8720a02dd9d369
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- releng/org.eclipse.sirius.releng/latest-ibuild.sh
+++ releng/org.eclipse.sirius.releng/latest-ibuild.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 BASE="http://download.eclipse.org/eclipse/updates/4.4-I-builds"
-TEMPFILE=$(tempfile -s .jar) || exit
+TEMPFILE=$(mktemp --suffix=.jar) || exit
 curl -s "$BASE/compositeContent.jar" > "$TEMPFILE"
 LATEST=$(unzip -p "$TEMPFILE" compositeContent.xml | grep "<child location='I" | sort -r | head -1 | sed -e "s/\s*<child location='//" -e "s|'/>\s*||")
 echo "$BASE/$LATEST"
