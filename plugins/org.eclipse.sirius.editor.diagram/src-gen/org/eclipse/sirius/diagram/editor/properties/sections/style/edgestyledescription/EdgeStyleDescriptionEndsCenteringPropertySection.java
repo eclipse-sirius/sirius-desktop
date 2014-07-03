d60d7a6e19a029b5acfa7506b0400bd65f9c480e
Florian Barbin <florian.barbin@obeo.fr>
2014-07-03 17:53:47 +0200

    [437528] Generate the VSM properties views
    Bug: 437538
    
    Change-Id: I3c31572cb00ddf3ded8f074c73a10d65ed280a3c
    Signed-off-by: Florian Barbin <florian.barbin@obeo.fr>

---
+++ plugins/org.eclipse.sirius.editor.diagram/src-gen/org/eclipse/sirius/diagram/editor/properties/sections/style/edgestyledescription/EdgeStyleDescriptionEndsCenteringPropertySection.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2013 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.diagram.editor.properties.sections.style.edgestyledescription;
+
+// Start of user code imports
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.sirius.diagram.description.CenteringStyle;
+import org.eclipse.sirius.diagram.description.style.StylePackage;
+import org.eclipse.sirius.editor.properties.sections.common.AbstractRadioButtonPropertySection;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
+
+// End of user code imports
+
+/**
+ * A section for the endsCentering property of a EdgeStyleDescription object.
+ */
+public class EdgeStyleDescriptionEndsCenteringPropertySection extends AbstractRadioButtonPropertySection {
+    /**
+     * @see org.eclipse.sirius.diagram.editor.properties.sections.AbstractRadioButtonPropertySection#getDefaultLabelText()
+     */
+    protected String getDefaultLabelText() {
+        return "EndsCentering"; //$NON-NLS-1$
+    }
+
+    /**
+     * @see org.eclipse.sirius.diagram.editor.properties.sections.AbstractRadioButtonPropertySection#getLabelText()
+     */
+    protected String getLabelText() {
+        String labelText;
+        labelText = super.getLabelText() + ":"; //$NON-NLS-1$
+        // Start of user code get label text
+
+        // End of user code get label text
+        return labelText;
+    }
+
+    /**
+     * @see org.eclipse.sirius.diagram.editor.properties.sections.AbstractRadioButtonPropertySection#getFeature()
+     */
+    protected EAttribute getFeature() {
+        return StylePackage.eINSTANCE.getEdgeStyleDescription_EndsCentering();
+    }
+
+    /**
+     * @see org.eclipse.sirius.diagram.editor.properties.sections.AbstractRadioButtonPropertySection#getFeatureValue(int)
+     */
+    protected Object getFeatureValue(int index) {
+        return getChoiceOfValues().get(index);
+    }
+
+    /**
+     * @see org.eclipse.sirius.diagram.editor.properties.sections.AbstractRadioButtonPropertySection#isEqual(int)
+     */
+    protected boolean isEqual(int index) {
+        return getChoiceOfValues().get(index).equals(eObject.eGet(getFeature()));
+    }
+
+    /**
+     * @see org.eclipse.sirius.diagram.editor.properties.sections.AbstractRadioButtonPropertySection#getEnumerationFeatureValues()
+     */
+    protected List<?> getChoiceOfValues() {
+        return CenteringStyle.VALUES;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void createControls(Composite parent, TabbedPropertySheetPage tabbedPropertySheetPage) {
+        super.createControls(parent, tabbedPropertySheetPage);
+
+    }
+}
