4b8e0c57da412df5b9e49732621ed785dc53eaf5
Maxime PORHEL <maxime.porhel@obeo.fr>
2014-07-24 16:10:30 +0200

    [440315] Display tooltip for bordered nodes with labelPosition=border
    This commit also corrects the border refresh for non bordered nodes.
    
    Bug: 440315
    Change-Id: I273cc37fc4e00ef413a5e7cf3519b300afbd87b7
    Signed-off-by: Maxime PORHEL <maxime.porhel@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/edit/internal/part/DiagramNodeEditPartOperation.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/edit/internal/part/DiagramNodeEditPartOperation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 THALES GLOBAL SERVICES.
+ * Copyright (c) 2007, 2014 THALES GLOBAL SERVICES and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -134,13 +134,13 @@ public final class DiagramNodeEditPartOperation {
                     if (styleConfiguration != null) {
                         styleConfiguration.adaptNodeLabel(viewNode, self.getNodeLabel());
                     }
-
-                    // The border.
-                    if (viewNode.getStyle() instanceof BorderedStyle) {
-                        final BorderedStyle borderedStyle = (BorderedStyle) viewNode.getStyle();
-                        DiagramNodeEditPartOperation.refreshBorderFigure(borderedStyle, styledFigure);
-                    }
                 }
+
+                if (viewNode.getStyle() instanceof BorderedStyle) {
+                    BorderedStyle borderedStyle = (BorderedStyle) viewNode.getStyle();
+                    DiagramNodeEditPartOperation.refreshBorderFigure(borderedStyle, styledFigure);
+                }
+
                 self.setTooltipText(viewNode.getTooltipText());
                 self.getNodeLabel().revalidate();
             }
