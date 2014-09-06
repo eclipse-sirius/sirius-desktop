61ab40b4cbfc4dd017b0def61c3287bffb2b945b
Pierre-Charles David <pierre-charles.david@obeo.fr>
2014-09-06 17:14:53 +0200

    [cleanup] Cleanup javadoc
    Change-Id: I03043adfc39216f8c7711597f13c95a193dbfc34

--- plugins/org.eclipse.sirius/src/org/eclipse/sirius/tools/internal/interpreter/ODesignGenericInterpreter.java
+++ plugins/org.eclipse.sirius/src/org/eclipse/sirius/tools/internal/interpreter/ODesignGenericInterpreter.java
@@ -54,7 +54,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
     private Collection<String> dependencies = Sets.newLinkedHashSet();
 
     /**
-     * If viewpoint knows of any additional metamodel that may be necessary for
+     * If Sirius knows of any additional metamodel that may be necessary for
      * the interpreter, they'll be registered here.
      */
     private Collection<MetamodelDescriptor> additionalMetamodels = Sets.newLinkedHashSet();
@@ -77,11 +77,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
     /** The profiler tasks. */
     private final Map<String, ProfilerTask> profilerTasks = new HashMap<String, ProfilerTask>();
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#activateMetamodels(java.util.Collection)
-     */
+    @Override
     public void activateMetamodels(Collection<MetamodelDescriptor> metamodels) {
         this.additionalMetamodels.addAll(metamodels);
         for (final IInterpreter interpreter : this.loadedInterpreters.values()) {
@@ -91,11 +87,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         }
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#addImport(java.lang.String)
-     */
+    @Override
     public void addImport(final String dependency) {
         this.dependencies.add(dependency);
         for (final IInterpreter interpreter : this.loadedInterpreters.values()) {
@@ -105,11 +97,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         }
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#clearImports()
-     */
+    @Override
     public void clearImports() {
         this.dependencies.clear();
         for (final IInterpreter interpreter : this.loadedInterpreters.values()) {
@@ -117,33 +105,17 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         }
     }
 
-    /**
-     * 
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#supportsValidation()
-     */
+    @Override
     public boolean supportsValidation() {
         return true;
     }
 
-    /**
-     * 
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#validateExpression(org.eclipse.emf.ecore.EClass,
-     *      java.lang.String)
-     */
+    @Override
     public Collection<IInterpreterStatus> validateExpression(IInterpreterContext context, String expression) {
         return getInterpreter(expression).validateExpression(context, expression);
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#evaluate(org.eclipse.emf.ecore.EObject,
-     *      java.lang.String)
-     */
+    @Override
     public Object evaluate(final EObject target, final String expression) throws EvaluationException {
         preEvaluation(expression);
         final Object evaluate = getInterpreter(expression).evaluate(target, expression);
@@ -151,12 +123,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         return evaluate;
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#evaluateBoolean(org.eclipse.emf.ecore.EObject,
-     *      java.lang.String)
-     */
+    @Override
     public boolean evaluateBoolean(final EObject context, final String expression) throws EvaluationException {
         preEvaluation(expression);
         final boolean evaluateBoolean = getInterpreter(expression).evaluateBoolean(context, expression);
@@ -164,12 +131,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         return evaluateBoolean;
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#evaluateCollection(org.eclipse.emf.ecore.EObject,
-     *      java.lang.String)
-     */
+    @Override
     public Collection<EObject> evaluateCollection(final EObject context, final String expression) throws EvaluationException {
         preEvaluation(expression);
         final Collection<EObject> evaluateCollection = getInterpreter(expression).evaluateCollection(context, expression);
@@ -177,12 +139,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         return evaluateCollection;
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#evaluateEObject(org.eclipse.emf.ecore.EObject,
-     *      java.lang.String)
-     */
+    @Override
     public EObject evaluateEObject(final EObject context, final String expression) throws EvaluationException {
         preEvaluation(expression);
         final EObject evaluateEObject = getInterpreter(expression).evaluateEObject(context, expression);
@@ -190,12 +147,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         return evaluateEObject;
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#evaluateInteger(org.eclipse.emf.ecore.EObject,
-     *      java.lang.String)
-     */
+    @Override
     public Integer evaluateInteger(final EObject context, final String expression) throws EvaluationException {
         preEvaluation(expression);
         final Integer evaluateInteger = getInterpreter(expression).evaluateInteger(context, expression);
@@ -203,12 +155,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         return evaluateInteger;
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#evaluateString(org.eclipse.emf.ecore.EObject,
-     *      java.lang.String)
-     */
+    @Override
     public String evaluateString(final EObject context, final String expression) throws EvaluationException {
         preEvaluation(expression);
         final String evaluateString = getInterpreter(expression).evaluateString(context, expression);
@@ -216,11 +163,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         return evaluateString;
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#provides(java.lang.String)
-     */
+    @Override
     public boolean provides(final String expression) {
         return getInterpreter(expression) != null;
     }
@@ -256,12 +199,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         return result;
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#setProperty(java.lang.Object,
-     *      java.lang.Object)
-     */
+    @Override
     public void setProperty(final Object key, final Object value) {
         this.properties.put(key, value);
         for (final IInterpreter interpreter : this.loadedInterpreters.values()) {
@@ -269,11 +207,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         }
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#clearVariables()
-     */
+    @Override
     public void clearVariables() {
         this.variables.clearVariables();
         for (final IInterpreter interpreter : this.loadedInterpreters.values()) {
@@ -283,22 +217,14 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         }
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#dispose()
-     */
+    @Override
     public void dispose() {
         for (final IInterpreter interpreter : this.loadedInterpreters.values()) {
             interpreter.dispose();
         }
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#getVariable(java.lang.String)
-     */
+    @Override
     public Object getVariable(final String name) {
         for (final IInterpreter interpreter : this.loadedInterpreters.values()) {
             if (interpreter.getVariable(name) != null) {
@@ -308,12 +234,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         return null;
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#setVariable(java.lang.String,
-     *      java.lang.Object)
-     */
+    @Override
     public void setVariable(final String name, final Object value) {
         this.variables.setVariable(name, value);
         for (final IInterpreter interpreter : this.loadedInterpreters.values()) {
@@ -321,11 +242,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         }
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#unSetVariable(java.lang.String)
-     */
+    @Override
     public void unSetVariable(final String name) {
         this.variables.unSetVariable(name);
         for (final IInterpreter interpreter : this.loadedInterpreters.values()) {
@@ -333,11 +250,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         }
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#addVariableStatusListener(org.eclipse.sirius.common.tools.api.interpreter.IVariableStatusListener)
-     */
+    @Override
     public void addVariableStatusListener(final IVariableStatusListener newListener) {
         this.listeners.add(newListener);
         for (final IInterpreter interpreter : this.loadedInterpreters.values()) {
@@ -345,11 +258,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         }
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#getVariables()
-     */
+    @Override
     public Map<String, Object> getVariables() {
         final Map<String, Object> result = new HashMap<String, Object>();
         for (final IInterpreter interpreter : this.loadedInterpreters.values()) {
@@ -360,11 +269,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         return result;
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#removeVariableStatusListener(org.eclipse.sirius.common.tools.api.interpreter.IVariableStatusListener)
-     */
+    @Override
     public void removeVariableStatusListener(final IVariableStatusListener listener) {
         this.listeners.remove(listener);
         for (final IInterpreter interpreter : this.loadedInterpreters.values()) {
@@ -372,11 +277,7 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         }
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#setModelAccessor(org.eclipse.sirius.ecore.extender.business.api.accessor.ModelAccessor)
-     */
+    @Override
     public void setModelAccessor(final ModelAccessor modelAccessor) {
         this.modelAccessor = modelAccessor;
         for (final IInterpreter interpreter : this.loadedInterpreters.values()) {
@@ -384,53 +285,32 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         }
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.contentassist.IProposalProvider#getProposals(org.eclipse.sirius.common.tools.api.interpreter.IInterpreter,
-     *      org.eclipse.sirius.common.tools.api.contentassist.ContentContext)
-     */
+    @Override
     public List<ContentProposal> getProposals(IInterpreter interpreter, ContentContext context) {
         return CompoundInterpreter.INSTANCE.getProposals(interpreter, context);
     }
 
-    /**
-     * {@inheritDoc} return always <code>null</code>.
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#getPrefix()
-     */
+    @Override
     public String getPrefix() {
         return CompoundInterpreter.INSTANCE.getPrefix();
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#getNewEmtpyExpression()
-     */
+    @Override
     public ContentProposal getNewEmtpyExpression() {
         return CompoundInterpreter.INSTANCE.getNewEmtpyExpression();
     }
 
-    /**
-     * {@inheritDoc} return always <code>null</code>.
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#getVariablePrefix()
-     */
+    @Override
     public String getVariablePrefix() {
         return CompoundInterpreter.INSTANCE.getVariablePrefix();
     }
 
-    /**
-     * 
-     * {@inheritDoc}
-     */
+    @Override
     public void setCrossReferencer(final ECrossReferenceAdapter crossReferencer) {
         this.crossReferencer = crossReferencer;
         for (final IInterpreter interpreter : this.loadedInterpreters.values()) {
             interpreter.setCrossReferencer(crossReferencer);
         }
-
     }
 
     private void preEvaluation(final String toEval) {
@@ -456,30 +336,17 @@ public class ODesignGenericInterpreter implements IInterpreter, IProposalProvide
         return result;
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.contentassist.IProposalProvider#getProposals(org.eclipse.sirius.common.tools.api.interpreter.IInterpreter,
-     *      org.eclipse.sirius.common.tools.api.contentassist.ContentInstanceContext)
-     */
+    @Override
     public List<ContentProposal> getProposals(IInterpreter interpreter, ContentInstanceContext context) {
         return CompoundInterpreter.INSTANCE.getProposals(interpreter, context);
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#getImports()
-     */
+    @Override
     public Collection<String> getImports() {
         return Collections.<String> unmodifiableCollection(this.dependencies);
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.interpreter.IInterpreter#removeImport(String)
-     */
+    @Override
     public void removeImport(String dependency) {
         this.dependencies.remove(dependency);
         for (IInterpreter interpreter : this.loadedInterpreters.values()) {
