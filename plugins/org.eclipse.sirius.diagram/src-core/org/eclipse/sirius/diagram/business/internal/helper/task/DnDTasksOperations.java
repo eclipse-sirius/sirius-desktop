eclipse-sirius/sirius-desktop

commit 447cbd28dee23009afd52d3f6e7f2bc9c55950c9

[427247] Remove NodeMapping.createListElement()
Remove the EOperation and inline the implementation at call sites.

Bug: 427247
Change-Id: I8b7c98bf786b39ca26c3e4482182d52b181abd22
Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

plugins/org.eclipse.sirius.diagram/src-core/org/eclipse/sirius/diagram/business/internal/helper/task/DnDTasksOperations.java
@@ -48,6 +48,7 @@ import org.eclipse.sirius.diagram.business.internal.experimental.sync.DDiagramSy
 import org.eclipse.sirius.diagram.business.internal.experimental.sync.DEdgeCandidate;
 import org.eclipse.sirius.diagram.business.internal.metamodel.description.extensions.IContainerMappingExt;
 import org.eclipse.sirius.diagram.business.internal.metamodel.helper.ContainerMappingHelper;
+import org.eclipse.sirius.diagram.business.internal.metamodel.helper.NodeMappingHelper;
 import org.eclipse.sirius.diagram.business.internal.metamodel.operations.DDiagramElementContainerSpecOperations;
 import org.eclipse.sirius.diagram.description.ContainerMapping;
 import org.eclipse.sirius.diagram.description.DiagramDescription;
@@ -122,11 +123,11 @@ public final class DnDTasksOperations {
                 public void execute() {
                     DNodeListElement viewNodeListElement;
                     if (droppedDiagramElement != null && mapping.equals(droppedDiagramElement.getMapping())) {
-                        // The mapping is the same so we don't create a new
-                        // DNode
+                        // The mapping is the same so we don't create a new DNode
                         viewNodeListElement = (DNodeListElement) droppedDiagramElement;
                     } else {
-                        viewNodeListElement = mapping.createListElement(droppedElement, parentDiagram);
+                        IInterpreter interpreter = SiriusPlugin.getDefault().getInterpreterRegistry().getInterpreter(droppedElement);
+                        viewNodeListElement = new NodeMappingHelper(interpreter).createListElement(mapping, droppedDiagramElement, parentDiagram);
                     }
                     ((DNodeList) target).getOwnedElements().add(viewNodeListElement);
                     if (!viewNodeListElement.equals(droppedDiagramElement)) {
@@ -144,8 +145,7 @@ public final class DnDTasksOperations {
                 public void execute() {
                     DNode dNode;
                     if (droppedDiagramElement != null && mapping.equals(droppedDiagramElement.getMapping())) {
-                        // The mapping is the same so we don't create a new
-                        // DNode
+                        // The mapping is the same so we don't create a new DNode
                         dNode = (DNode) droppedDiagramElement;
                     } else {
                         dNode = mapping.createNode(droppedElement, semanticContainer, parentDiagram);

plugins/org.eclipse.sirius.diagram.sequence/src/org/eclipse/sirius/diagram/sequence/business/internal/metamodel/description/EndOfLifeMappingSpec.java
@@ -99,15 +99,6 @@ public class EndOfLifeMappingSpec extends EndOfLifeMappingImpl implements INodeM
      * {@inheritDoc}
      */
     @Override
-    public DNodeListElement createListElement(final EObject modelElement, final DDiagram diagram) {
-        IInterpreter interpreter = SiriusPlugin.getDefault().getInterpreterRegistry().getInterpreter(modelElement);
-        return new NodeMappingHelper(interpreter).createListElement(this, modelElement, diagram);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
     public void updateListElement(final DNodeListElement listElement) {
         IInterpreter interpreter = SiriusPlugin.getDefault().getInterpreterRegistry().getInterpreter(listElement);
         new NodeMappingHelper(interpreter).updateListElement(this, listElement);

plugins/org.eclipse.sirius.diagram.sequence/src/org/eclipse/sirius/diagram/sequence/business/internal/metamodel/description/ExecutionMappingSpec.java
@@ -99,15 +99,6 @@ public class ExecutionMappingSpec extends ExecutionMappingImpl implements INodeM
      * {@inheritDoc}
      */
     @Override
-    public DNodeListElement createListElement(final EObject modelElement, final DDiagram diagram) {
-        IInterpreter interpreter = SiriusPlugin.getDefault().getInterpreterRegistry().getInterpreter(modelElement);
-        return new NodeMappingHelper(interpreter).createListElement(this, modelElement, diagram);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
     public void updateListElement(final DNodeListElement listElement) {
         IInterpreter interpreter = SiriusPlugin.getDefault().getInterpreterRegistry().getInterpreter(listElement);
         new NodeMappingHelper(interpreter).updateListElement(this, listElement);

plugins/org.eclipse.sirius.diagram.sequence/src/org/eclipse/sirius/diagram/sequence/business/internal/metamodel/description/InstanceRoleMappingSpec.java
@@ -99,15 +99,6 @@ public class InstanceRoleMappingSpec extends InstanceRoleMappingImpl implements 
      * {@inheritDoc}
      */
     @Override
-    public DNodeListElement createListElement(final EObject modelElement, final DDiagram diagram) {
-        IInterpreter interpreter = SiriusPlugin.getDefault().getInterpreterRegistry().getInterpreter(modelElement);
-        return new NodeMappingHelper(interpreter).createListElement(this, modelElement, diagram);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
     public void updateListElement(final DNodeListElement listElement) {
         IInterpreter interpreter = SiriusPlugin.getDefault().getInterpreterRegistry().getInterpreter(listElement);
         new NodeMappingHelper(interpreter).updateListElement(this, listElement);

plugins/org.eclipse.sirius.diagram.sequence/src/org/eclipse/sirius/diagram/sequence/business/internal/metamodel/description/ObservationPointMappingSpec.java
@@ -99,15 +99,6 @@ public class ObservationPointMappingSpec extends ObservationPointMappingImpl imp
      * {@inheritDoc}
      */
     @Override
-    public DNodeListElement createListElement(final EObject modelElement, final DDiagram diagram) {
-        IInterpreter interpreter = SiriusPlugin.getDefault().getInterpreterRegistry().getInterpreter(modelElement);
-        return new NodeMappingHelper(interpreter).createListElement(this, modelElement, diagram);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
     public void updateListElement(final DNodeListElement listElement) {
         IInterpreter interpreter = SiriusPlugin.getDefault().getInterpreterRegistry().getInterpreter(listElement);
         new NodeMappingHelper(interpreter).updateListElement(this, listElement);

plugins/org.eclipse.sirius.diagram.sequence/src/org/eclipse/sirius/diagram/sequence/business/internal/metamodel/description/StateMappingSpec.java
@@ -99,15 +99,6 @@ public class StateMappingSpec extends StateMappingImpl implements INodeMappingEx
      * {@inheritDoc}
      */
     @Override
-    public DNodeListElement createListElement(final EObject modelElement, final DDiagram diagram) {
-        IInterpreter interpreter = SiriusPlugin.getDefault().getInterpreterRegistry().getInterpreter(modelElement);
-        return new NodeMappingHelper(interpreter).createListElement(this, modelElement, diagram);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
     public void updateListElement(final DNodeListElement listElement) {
         IInterpreter interpreter = SiriusPlugin.getDefault().getInterpreterRegistry().getInterpreter(listElement);
         new NodeMappingHelper(interpreter).updateListElement(this, listElement);

plugins/org.eclipse.sirius.diagram/model/diagram.ecore
@@ -1368,17 +1368,6 @@
           </eAnnotations>
         </eParameters>
       </eOperations>
-      <eOperations name="createListElement" eType="#//DNodeListElement">
-        <eAnnotations source="http://www.eclipse.org/emf/2002/GenModel">
-          <details key="documentation" value="Create a list element. This method is equivalent of createNode but returns a ViewNodeListElement."/>
-        </eAnnotations>
-        <eParameters name="modelElement" eType="ecore:EClass platform:/plugin/org.eclipse.emf.ecore/model/Ecore.ecore#//EObject">
-          <eAnnotations source="http://www.eclipse.org/emf/2002/GenModel">
-            <details key="documentation" value="The semantic model element."/>
-          </eAnnotations>
-        </eParameters>
-        <eParameters name="diagram" eType="#//DDiagram"/>
-      </eOperations>
       <eOperations name="updateListElement">
         <eAnnotations source="http://www.eclipse.org/emf/2002/GenModel">
           <details key="documentation" value="Update a ViewNodeListElement."/>

plugins/org.eclipse.sirius.diagram/model/diagram.genmodel
@@ -448,10 +448,6 @@
         <genOperations ecoreOperation="diagram.ecore#//description/NodeMapping/updateNode">
           <genParameters ecoreParameter="diagram.ecore#//description/NodeMapping/updateNode/node"/>
         </genOperations>
-        <genOperations ecoreOperation="diagram.ecore#//description/NodeMapping/createListElement">
-          <genParameters ecoreParameter="diagram.ecore#//description/NodeMapping/createListElement/modelElement"/>
-          <genParameters ecoreParameter="diagram.ecore#//description/NodeMapping/createListElement/diagram"/>
-        </genOperations>
         <genOperations ecoreOperation="diagram.ecore#//description/NodeMapping/updateListElement">
           <genParameters ecoreParameter="diagram.ecore#//description/NodeMapping/updateListElement/listElement"/>
         </genOperations>

commit 8e2fb4edc1d5e5c299feaaf8b224ff235830555d

[427247] The commit 447cbd2 has introduced a regression that causes NPE
In DnDTasksOperations.createDropinForNodeTask(DragAndDropTarget,
NodeMapping, DDiagramElement, EObject, EObject, boolean), the inlining
of NodeMapping.createListElement() uses the DDiagramElement instead of
the semantic element. When doing a drag'n'drop of a semantic element
from the model explorer view, this can cause a NPE.

Bug: 427247
Change-Id: Ib1f0f813e49d52eb7f75cf42389159d15d81c848
Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>

plugins/org.eclipse.sirius.diagram/src-core/org/eclipse/sirius/diagram/business/internal/helper/task/DnDTasksOperations.java
@@ -127,7 +127,7 @@ public final class DnDTasksOperations {
                         viewNodeListElement = (DNodeListElement) droppedDiagramElement;
                     } else {
                         IInterpreter interpreter = SiriusPlugin.getDefault().getInterpreterRegistry().getInterpreter(droppedElement);
-                        viewNodeListElement = new NodeMappingHelper(interpreter).createListElement(mapping, droppedDiagramElement, parentDiagram);
+                        viewNodeListElement = new NodeMappingHelper(interpreter).createListElement(mapping, droppedElement, parentDiagram);
                     }
                     ((DNodeList) target).getOwnedElements().add(viewNodeListElement);
                     if (!viewNodeListElement.equals(droppedDiagramElement)) {