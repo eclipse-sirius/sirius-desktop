c88a05a0de170c23251afee22a447cfdd1dc2733
Pierre-Charles David <pierre-charles.david@obeo.fr>
2014-09-29 11:09:57 +0200

    [441483] Integrate the test plug-ins into the build
    Bug: 441483
    Change-Id: I6fcbf8c9e36f02692454ec117f9ae11d867e0ffb
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- build-all.sh
+++ build-all.sh
@@ -4,6 +4,7 @@ set -e
 
 readonly PLATFORM=${1:-luna}
 readonly TESTS_GOAL=${2:-package}
+readonly TESTS_SUITES=${3:-junit,swtbot-sequence,swtbot}
 readonly BASEDIR=$(pwd)
 readonly BUILD_POM="packaging/org.eclipse.sirius.parent/pom.xml"
 readonly TESTS_POM="packaging/org.eclipse.sirius.tests.parent/pom.xml"
@@ -11,4 +12,4 @@ readonly TESTS_SETTINGS="packaging/org.eclipse.sirius.tests.parent/sirius-local-
 
 mvn -Dplatform-version-name="$PLATFORM" -f "$BUILD_POM" clean package
 sed -i -e "s!@BASEDIR@!$BASEDIR!" "$TESTS_SETTINGS"
-mvn -Dplatform-version-name="$PLATFORM" -s "$TESTS_SETTINGS" -f "$TESTS_POM" clean "$TESTS_GOAL"
+mvn -Dplatform-version-name="$PLATFORM" -s "$TESTS_SETTINGS" -f "$TESTS_POM" -P"$TESTS_SUITES" clean "$TESTS_GOAL"
