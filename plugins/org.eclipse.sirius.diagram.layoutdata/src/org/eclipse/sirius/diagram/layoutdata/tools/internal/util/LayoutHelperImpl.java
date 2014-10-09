eclipse-sirius/sirius-desktop

commit 103036c5cfbc7c8a652c552dadfe4cbaa4c139e5

[439871] Avoid depending on Draw2D for a trivial geometric computation
Bug: 439871
Change-Id: If0b41bdea93af612bb1930d7b405a681f5a2fdc1
Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

plugins/org.eclipse.sirius.diagram.layoutdata/src/org/eclipse/sirius/diagram/layoutdata/tools/internal/util/LayoutHelperImpl.java
@@ -177,16 +177,16 @@ public class LayoutHelperImpl implements LayoutHelper {
      *            The expected location
      * @param realLocation
      *            The real location
-     * @param distance
+     * @param maxDistance
      *            the error margin in x and y coordinates.
      * @return true is the real location is near the expected location, false
      *         otherwise
      */
-    private boolean isAroundPoint(Point expectedLocation, Point realLocation, double distance) {
-        org.eclipse.draw2d.geometry.Point draw2dExpectedPoint = new org.eclipse.draw2d.geometry.Point(expectedLocation.getX(), expectedLocation.getY());
-        org.eclipse.draw2d.geometry.Point draw2dRealPoint = new org.eclipse.draw2d.geometry.Point(realLocation.getX(), realLocation.getY());
-
-        return draw2dExpectedPoint.getDistance2(draw2dRealPoint) <= distance * distance;
+    private boolean isAroundPoint(Point expectedLocation, Point realLocation, double maxDistance) {
+        long dx = realLocation.getX() - expectedLocation.getX();
+        long dy = realLocation.getY() - expectedLocation.getY();
+        long distance2 = dx * dx + dy * dy;
+        return distance2 < (maxDistance * maxDistance);
     }
 
     /**

plugins/org.eclipse.sirius.diagram.layoutdata/META-INF/MANIFEST.MF
@@ -16,6 +16,5 @@ Export-Package: org.eclipse.sirius.diagram.layoutdata;uses:="org.eclipse.emf.eco
  org.eclipse.sirius.diagram.layoutdata.util;uses:="org.eclipse.emf.ecore,  org.eclipse.emf.common.notify.impl,  org.eclipse.sirius.diagram.layoutdata,  org.eclipse.emf.common.notify";version="2.0.0"
 Require-Bundle: org.eclipse.core.runtime;bundle-version="3.8.0",
  org.eclipse.emf.ecore;bundle-version="2.8.3";visibility:=reexport,
- org.eclipse.draw2d;bundle-version="3.2.100",
  org.eclipse.sirius.common;bundle-version="2.0.0"
 Bundle-ActivationPolicy: lazy

commit 4051f13d9978e4a95b04fe4fd58b2197d9a1f084

[439871] Fix bug introduced by 103036c5cf
The new version of isAroundPoint() was not strictly equivalent to the
previous one: it used "<" instead of "<=" to compare the actual distance
and the maximum allowed distance.

Bug: 439871
Change-Id: I1b8da9f8f0509ae530455720e99df9a152ea4bc6
Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

plugins/org.eclipse.sirius.diagram.layoutdata/src/org/eclipse/sirius/diagram/layoutdata/tools/internal/util/LayoutHelperImpl.java
@@ -186,7 +186,7 @@ public class LayoutHelperImpl implements LayoutHelper {
         long dx = realLocation.getX() - expectedLocation.getX();
         long dy = realLocation.getY() - expectedLocation.getY();
         long distance2 = dx * dx + dy * dy;
-        return distance2 < (maxDistance * maxDistance);
+        return distance2 <= (maxDistance * maxDistance);
     }
 
     /**