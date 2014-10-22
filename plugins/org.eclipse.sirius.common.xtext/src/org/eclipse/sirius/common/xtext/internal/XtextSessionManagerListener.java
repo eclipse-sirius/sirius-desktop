bcbcd8125886073e285d2e8b26d773ac5eb61587
Pierre-Charles David <pierre-charles.david@obeo.fr>
2014-10-22 09:50:12 +0200

    [430724] Only enable the new saving policy if XText resources are present
    Instead of enabling the new saving policy for all sessions as soon as
    the Sirius/Xtext bridge plug-in is installed, enable it only when at
    least one Xtext resource is added. We do not revert to the default one
    if all Xtext resources are removed, but this should not happen very
    often.
    
    Note that with this change, even the Xtext-specific saving options are
    now only enabled if Xtext resources are present, not just the new saving
    policy.
    
    Bug: 430724
    Change-Id: Ie13112a6468243e6bce24beab1c32026dcc969a5
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.sirius.common.xtext/src/org/eclipse/sirius/common/xtext/internal/XtextSessionManagerListener.java
+++ plugins/org.eclipse.sirius.common.xtext/src/org/eclipse/sirius/common/xtext/internal/XtextSessionManagerListener.java
@@ -10,33 +10,55 @@
  *******************************************************************************/
 package org.eclipse.sirius.common.xtext.internal;
 
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.sirius.business.api.session.Session;
+import org.eclipse.sirius.business.api.session.SessionListener;
 import org.eclipse.sirius.business.api.session.SessionManagerListener.Stub;
 import org.eclipse.sirius.business.internal.session.IsModifiedSavingPolicy;
+import org.eclipse.xtext.resource.XtextResource;
+
+import com.google.common.base.Predicates;
+import com.google.common.collect.Iterators;
 
 /**
  * A session manager listener which will switch the saving policy of every
- * session which is getting created by one which has special support for Xtext
- * resources.
+ * session which uses Xtext resources.
+ * <p>
+ * Due to bug 432795 in Xtext, we need to override the default saving options
+ * for Xtext resources to avoid hitting false positive validation errors
+ * preventing the save. Furthermore when one of this error is thrown the
+ * underlying file is emptied which makes this quite critical.
+ * <p>
+ * In addition, given the structure of a typical Xtext resource set (many
+ * "library" resources, which are referenced by the session but do not reference
+ * it themselves), we use a policy with different and more efficient strategy to
+ * determine which resources need to be saved.
  * 
  * @author cbrun
- * 
  */
 public class XtextSessionManagerListener extends Stub {
-
-    /**
-     * {@inheritDoc}
-     */
     @Override
-    public void notifyAddSession(Session newSession) {
-        super.notifyAddSession(newSession);
-        /*
-         * Due to bug 432795 in Xtext, we need to override the default saving
-         * options for Xtext resources to avoid hitting false positive
-         * validation errors preventing the save. Furthermore when one of this
-         * error is thrown the underlying file is emptied which makes this quite
-         * critical.
-         */
-        newSession.setSavingPolicy(new XtextSavingPolicy(new IsModifiedSavingPolicy(newSession.getTransactionalEditingDomain())));
+    public void notify(Session updated, int notification) {
+        super.notify(updated, notification);
+        switch (notification) {
+        case SessionListener.OPENED:
+            changeSavingPolicyIfXtextIsUsed(updated);
+            break;
+
+        case SessionListener.SEMANTIC_CHANGE:
+            changeSavingPolicyIfXtextIsUsed(updated);
+            break;
+        }
+    }
+
+    private void changeSavingPolicyIfXtextIsUsed(Session session) {
+        TransactionalEditingDomain ted = session.getTransactionalEditingDomain();
+        if (ted != null && !(session.getSavingPolicy() instanceof XtextSavingPolicy) && containsXtextResources(ted)) {
+            session.setSavingPolicy(new XtextSavingPolicy(new IsModifiedSavingPolicy(ted)));
+        }
+    }
+
+    protected boolean containsXtextResources(TransactionalEditingDomain ted) {
+        return ted.getResourceSet() != null && Iterators.any(ted.getResourceSet().getResources().iterator(), Predicates.instanceOf(XtextResource.class));
     }
 }
