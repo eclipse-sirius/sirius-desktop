0d8d25916b663b6754636ef78c6886a91144a86c
Maxime Porhel <maxime.porhel@obeo.fr>
2014-11-07 11:19:44 +0100

    [453056] Test to detect invalid variable in interpreted expression
    The test checks the variables defined in our metamodels eannotations
    ("http://www.eclipse.org/sirius/interpreted/expression/variables").
    
    It allows to reveal the issue of Bug 447729 with DiagramVariablesTest
    
    Cloned from bug: 447729
    Bug: 453056
    Change-Id: I05cd0aca52cfdf24023dc7c55cb2894dbb025d4a
    Signed-off-by: Maxime Porhel <maxime.porhel@obeo.fr>
    (cherry picked from commit dea709c6bdd029eb54676b2c87087ab484f4bb7a)

--- plugins/org.eclipse.sirius.tests.junit.support/src/org/eclipse/sirius/tests/support/api/AbstractInterpretedExpressionTestCase.java
+++ plugins/org.eclipse.sirius.tests.junit.support/src/org/eclipse/sirius/tests/support/api/AbstractInterpretedExpressionTestCase.java
@@ -11,6 +11,7 @@
 package org.eclipse.sirius.tests.support.api;
 
 import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 import junit.framework.TestCase;
@@ -18,13 +19,17 @@ import junit.framework.TestCase;
 import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.sirius.viewpoint.ViewpointPackage;
 import org.eclipse.sirius.viewpoint.description.DescriptionPackage;
 import org.junit.Assert;
 
 import com.google.common.base.Predicate;
+import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
 
 /**
@@ -81,7 +86,106 @@ public abstract class AbstractInterpretedExpressionTestCase extends TestCase {
     /**
      * Test that all interpreted expression has a variable documentation.
      */
-    public void testVariablesInterpretedExpressionEAnnotation() {
+    public void testVariableTypesInterpretedExpressionEAnnotation() {
+        Multimap<EAttribute, String> wrongTypes = ArrayListMultimap.create();
+
+        for (EAttribute attr : interpretedExpressions) {
+            EAnnotation varAnnotations = attr.getEAnnotation(AbstractInterpretedExpressionTestCase.VARIABLES);
+            if (varAnnotations != null) {
+                for (String varName : varAnnotations.getDetails().keySet()) {
+                    String doc = varAnnotations.getDetails().get(varName);
+                    if (doc != null && doc.indexOf("|") != -1) {
+                        String typeName = doc.substring(0, doc.indexOf("|")).trim();
+                        String errorMessage = validateVariableType(typeName);
+                        if (errorMessage != null) {
+                            wrongTypes.put(attr, varName + ":" + typeName + " > " + errorMessage);
+                        }
+                    }
+                }
+            }
+        }
+
+        Assert.assertTrue(getMessage(wrongTypes), wrongTypes.isEmpty());
+    }
+
+    private String getMessage(Multimap<EAttribute, String> wrongTypes) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(wrongTypes.size());
+        sb.append(" variable(s) available in interpreted expressions need type correction:");
+        for (EAttribute attr : wrongTypes.keySet()) {
+            sb.append("\n . ");
+            sb.append(attr.eResource().getURIFragment(attr));
+            for (String v : wrongTypes.get(attr)) {
+                sb.append("\n   . " + v);
+            }
+        }
+        return sb.toString();
+    }
+
+    private String validateVariableType(String typeName) {
+        String errorMessage = "cannot be checked, the test method must be improved";
+        if ("ecore.EObject".equals(typeName) || "EObject".equals(typeName)) {
+            errorMessage = null;
+        } else {
+            int indexOf = typeName.indexOf(".");
+            String pName = typeName.substring(0, indexOf).trim();
+            String cName = typeName.substring(indexOf + 1).trim();
+
+            EPackage ePackage = getEPackage(pName, getAvailablePackages());
+            if (ePackage != null) {
+                EClassifier eClassifier = ePackage.getEClassifier(cName);
+                if (eClassifier == null) {
+                    errorMessage = "the EClass " + cName + " has not been found in the indicated EPackage.";
+                } else {
+                    // Valid type.
+                    errorMessage = null;
+                }
+            } else {
+                errorMessage = "the EPackage" + pName + " might not be accessible for the expression.";
+            }
+        }
+        return errorMessage;
+    }
+
+    private Collection<EPackage> getAvailablePackages() {
+        LinkedHashSet<EPackage> availablePackages = Sets.newLinkedHashSet();
+        availablePackages.add(basePackage);
+        availablePackages.add(getDialectPackage());
+        availablePackages.add(ViewpointPackage.eINSTANCE);
+        return availablePackages;
+    }
+
+    /**
+     * Allow to indicate which is the dialect package (in case of sur diagram
+     * types or dialect extension).
+     * 
+     * @return the dialect package (or core base package)
+     */
+    protected EPackage getDialectPackage() {
+        return getBasePackage();
+    }
+
+    private EPackage getEPackage(String pName, Collection<EPackage> packages) {
+        EPackage found = null;
+        for (EPackage p : packages) {
+            if (p.getName().equals(pName)) {
+                found = p;
+            } else {
+                found = getEPackage(pName, p.getESubpackages());
+            }
+
+            if (found != null) {
+                break;
+            }
+        }
+        return found;
+
+    }
+
+    /**
+     * Test that all interpreted expression variables have a known type.
+     */
+    public void testVariablesInInterpretedExpressionEAnnotation() {
         List<EAttribute> nonDocumented = Lists.newArrayList();
 
         Predicate<EAttribute> needsDocumentation = new Predicate<EAttribute>() {
