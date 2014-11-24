c70070c5931aa02004649cc246330d3ce3c5541a
Laurent Fasani <laurent.fasani@obeo.fr>
2014-11-24 14:01:51 +0100

    [428757] tool variables are not displayed in autocompletion
    variables are hard coded for the feature
    ToolPackage.Literals.ABSTRACT_TOOL_DESCRIPTION__PRECONDITION in the
    context of a Diagram.
    This hard-coding is a temporary solution. The good way could be to put
    that metadata on the diagram.ecore metamodeler as additional feature
    annotations avoiding a specific code.
    
    Note that the displayed order of the variables is not consistent between
    connection start and complete Precondition. A specification should be
    created to define the annotation display order according for example to
    where it is in the metamodel hierarchy.
    
    Bug 428757
    Change-Id: I92aa16fdac3f5e18749e717456d66e12c37f7810
    Signed-off-by: Laurent Fasani <laurent.fasani@obeo.fr>

--- plugins/org.eclipse.sirius.tests.junit/src/org/eclipse/sirius/tests/unit/api/vsm/interpreted/expression/variables/DiagramVariablesTest.java
+++ plugins/org.eclipse.sirius.tests.junit/src/org/eclipse/sirius/tests/unit/api/vsm/interpreted/expression/variables/DiagramVariablesTest.java
@@ -10,8 +10,18 @@
  *******************************************************************************/
 package org.eclipse.sirius.tests.unit.api.vsm.interpreted.expression.variables;
 
+import java.util.Set;
+
+import org.eclipse.sirius.common.tools.api.interpreter.IInterpreterContext;
 import org.eclipse.sirius.diagram.DiagramPackage;
+import org.eclipse.sirius.diagram.description.DescriptionFactory;
+import org.eclipse.sirius.diagram.description.DiagramDescription;
+import org.eclipse.sirius.diagram.description.tool.EdgeCreationDescription;
+import org.eclipse.sirius.diagram.description.tool.ToolFactory;
+import org.eclipse.sirius.diagram.description.tool.ToolSection;
 import org.eclipse.sirius.tests.support.api.AbstractInterpretedExpressionTestCase;
+import org.eclipse.sirius.tools.api.interpreter.context.SiriusInterpreterContextFactory;
+import org.eclipse.sirius.viewpoint.description.tool.ToolPackage;
 
 /**
  * Test documentation of interpreted expressions.
@@ -25,4 +35,28 @@ public class DiagramVariablesTest extends AbstractInterpretedExpressionTestCase 
         setBasePackage(DiagramPackage.eINSTANCE);
         super.setUp();
     }
+
+    public void testInterpreterContextForAbstractVariable() {
+        // Setup
+        DiagramDescription diagramDescription = DescriptionFactory.eINSTANCE.createDiagramDescription();
+        ToolSection createToolSection = ToolFactory.eINSTANCE.createToolSection();
+        EdgeCreationDescription edgeCreationDescription = ToolFactory.eINSTANCE.createEdgeCreationDescription();
+        diagramDescription.setToolSection(createToolSection);
+        createToolSection.getOwnedTools().add(edgeCreationDescription);
+
+        // Test
+        IInterpreterContext context = SiriusInterpreterContextFactory.createInterpreterContext(edgeCreationDescription, ToolPackage.Literals.ABSTRACT_TOOL_DESCRIPTION__PRECONDITION);
+
+        // Check
+        Set<String> keySet = context.getVariables().keySet();
+
+        assertTrue("The interpreter context should contains the variable preSourceView", keySet.contains("preSourceView"));
+        assertTrue("The interpreter context should contains the variable preTargetView", keySet.contains("preTargetView"));
+        assertTrue("The interpreter context should contains the variable preSource", keySet.contains("preSource"));
+        assertTrue("The interpreter context should contains the variable preTarget", keySet.contains("preTarget"));
+        assertTrue("The interpreter context should contains the variable diagram", keySet.contains("diagram"));
+        assertTrue("The interpreter context should contains the variable container", keySet.contains("container"));
+
+    }
+
 }
