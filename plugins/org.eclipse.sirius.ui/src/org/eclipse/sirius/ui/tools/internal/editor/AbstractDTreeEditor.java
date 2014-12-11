305fb1719f60ff1823e2362c9441dfc7bdb2a361
Laurent Redor <laurent.redor@obeo.fr>
2014-12-11 14:57:34 +0100

    [456055] Dispose SessionEditorInput on table and tree closing
    This avoid a memory leak caused by the INavigationHistory and
    EditorHistory that retain the IEditorInput.
    
    Bug: 456055
    Cherry-picked-from: 454887
    Change-Id: I040c8244a6356032aef9eea931c9e9d33a59c7ea
    Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>
    Signed-off-by: Maxime Porhel <maxime.porhel@obeo.fr>

--- plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/tools/internal/editor/AbstractDTreeEditor.java
+++ plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/tools/internal/editor/AbstractDTreeEditor.java
@@ -50,6 +50,7 @@ import org.eclipse.sirius.tools.api.permission.DRepresentationPermissionStatusQu
 import org.eclipse.sirius.ui.business.api.dialect.DialectEditor;
 import org.eclipse.sirius.ui.business.api.dialect.DialectEditorDialogFactory;
 import org.eclipse.sirius.ui.business.api.session.IEditingSession;
+import org.eclipse.sirius.ui.business.api.session.SessionEditorInput;
 import org.eclipse.sirius.ui.business.api.session.SessionUIManager;
 import org.eclipse.sirius.ui.business.internal.dialect.TreeEditorDialogFactory;
 import org.eclipse.sirius.ui.tools.api.properties.DTablePropertySheetpage;
@@ -550,6 +551,11 @@ public abstract class AbstractDTreeEditor extends EditorPart implements ISelecti
         super.dispose();
         if (getTableViewer() != null) {
             getTableViewer().dispose();
+        }
+        // Dispose the session editor input to keep the minimum information to
+        // be restore from the INavigationHistory and EditorHistory.
+        if (getEditorInput() instanceof SessionEditorInput) {
+            ((SessionEditorInput) getEditorInput()).dispose();
         }
     }
 
