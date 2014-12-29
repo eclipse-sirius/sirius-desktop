0f2c9f499709c4c527e8ee6d7d94e1dc18f17427
Maxime Porhel <maxime.porhel@obeo.fr>
2014-12-29 10:11:52 +0100

    [version] Bump version to 2.0.3
    Change-Id: I783a5edbab524a9c5f1c1481a9cac783e967e9b4
    Signed-off-by: Maxime Porhel <maxime.porhel@obeo.fr>

--- releng/org.eclipse.sirius.releng/publish-nightly.sh
+++ releng/org.eclipse.sirius.releng/publish-nightly.sh
@@ -31,7 +31,7 @@ provided by the Hudson build.
 set -e
 
 # The full version (should be taken as an argument)
-export VERSION="2.0.2"
+export VERSION="2.0.3"
 
 # The type of build being published
 export BUILD_TYPE="nightly"
