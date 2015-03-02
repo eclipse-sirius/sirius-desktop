eclipse-sirius/sirius-desktop

commit 51caff64adb3eb051b938c6afc4e4778d130c67a

[459783]  Improve cursor feedback when dropping an element on a diagram
Bug: 459783
Change-Id: I26542df27383985494de0c1382e0c49c73b9a49e
Signed-off-by: Nathalie Lepine <nathalie.lepine@obeo.fr>

plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/editor/DDiagramEditorImpl.java
@@ -182,6 +182,8 @@ import org.eclipse.sirius.viewpoint.DRepresentationElement;
 import org.eclipse.sirius.viewpoint.DSemanticDecorator;
 import org.eclipse.sirius.viewpoint.SiriusPlugin;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.dnd.DropTargetEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
@@ -1476,6 +1478,14 @@ public class DDiagramEditorImpl extends SiriusDiagramEditor implements DDiagramE
                 }
                 request.setEditParts(list);
                 return request;
+            }
+            
+            @Override
+            public void setCurrentEvent(DropTargetEvent currentEvent) {
+                if (currentEvent!=null) {
+                    currentEvent.detail = DND.DROP_COPY;
+                }
+                super.setCurrentEvent(currentEvent);
             }
 
             @Override

commit 423350b4f31c4195ce2d111e495f8af73e038f1a

[459783] Revert "Improve cursor feedback when dropping an element on a diagram"
This reverts commit 51caff64adb3eb051b938c6afc4e4778d130c67a, which
caused a regression because changing the nature of the DND event does
not just change the cursor but causes some drop listeners to refuse the
event. More work is needed to get the fix right.

Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/editor/DDiagramEditorImpl.java
@@ -182,8 +182,6 @@ import org.eclipse.sirius.viewpoint.DRepresentationElement;
 import org.eclipse.sirius.viewpoint.DSemanticDecorator;
 import org.eclipse.sirius.viewpoint.SiriusPlugin;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.dnd.DND;
-import org.eclipse.swt.dnd.DropTargetEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
@@ -1478,14 +1476,6 @@ public class DDiagramEditorImpl extends SiriusDiagramEditor implements DDiagramE
                 }
                 request.setEditParts(list);
                 return request;
-            }
-            
-            @Override
-            public void setCurrentEvent(DropTargetEvent currentEvent) {
-                if (currentEvent!=null) {
-                    currentEvent.detail = DND.DROP_COPY;
-                }
-                super.setCurrentEvent(currentEvent);
             }
 
             @Override