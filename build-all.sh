c577e9961ec893b9611e3954ac172d3c509e5d87
Pierre-Charles David <pierre-charles.david@obeo.fr>
2015-03-08 16:02:31 +0100

    [build] Improve and document build script
    Reset the settings file to its previous content after the build is done.
    
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- build-all.sh
+++ build-all.sh
@@ -10,8 +10,14 @@ readonly BUILD_POM="packaging/org.eclipse.sirius.parent/pom.xml"
 readonly TESTS_POM="packaging/org.eclipse.sirius.tests.parent/pom.xml"
 readonly TESTS_SETTINGS="packaging/org.eclipse.sirius.tests.parent/sirius-local-settings.xml"
 
+# Build Sirius core using the normal Target Platform definition
 mvn -Dplatform-version-name="$PLATFORM" -f "$BUILD_POM" clean package
-sed -i -e "s!@BASEDIR@!$BASEDIR!" "$TESTS_SETTINGS"
 
-    export SWT_GTK3=0
-mvn -Dplatform-version-name="$PLATFORM" -s "$TESTS_SETTINGS" -f "$TESTS_POM" -P"$TESTS_SUITES" clean "$TESTS_GOAL"
+# Build (and optionally execute) the Sirius test suites, using an
+# adjusted Target Platform which uses the locally build Sirius core
+# instead of the latest published nightly.
+readonly ORIGINAL_SETTINGS=$(mktemp)
+cp "$TESTS_SETTINGS" "$ORIGINAL_SETTINGS"
+sed -i -e "s!@BASEDIR@!$BASEDIR!" "$TESTS_SETTINGS"
+SWT_GTK3=0 mvn -Dplatform-version-name="$PLATFORM" -s "$TESTS_SETTINGS" -f "$TESTS_POM" -P"$TESTS_SUITES" clean "$TESTS_GOAL"
+mv "$ORIGINAL_SETTINGS" "$TESTS_SETTINGS"
