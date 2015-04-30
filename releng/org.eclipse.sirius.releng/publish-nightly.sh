c9d821584e1d0458adb2d3c96168ed55d740b8b7
Laurent Redor <laurent.redor@obeo.fr>
2015-04-30 10:23:25 +0200

    [build] Bump all 2013 or 2014 Copyright of Sirius features to 2015
    Change-Id: Idb4bc9886ec6069c71a84249209a27fb092c7117
    Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>

--- releng/org.eclipse.sirius.releng/publish-nightly.sh
+++ releng/org.eclipse.sirius.releng/publish-nightly.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # ====================================================================
-# Copyright (c) 2013 Obeo
+# Copyright (c) 2013, 2015 Obeo
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
