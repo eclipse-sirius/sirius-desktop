cd558cb06d7807291a78def287f13647e37676ab
Laurent Fasani <laurent.fasani@obeo.fr>
2015-05-04 12:09:55 +0200

    [456771] Add UI plugin that contains extensions definition
    Definition of extensions in org.eclipse.sirius.xxx.ui plugins are moved
    into new plugins named org.eclipse.sirius.xxx.ui.ext.
    That allows customer to define its own feature and choose only the
    functionalities he wants among perspective, views, newWizard, menus and
    contextual menus, editors and preference pages.
    
    Change-Id: Icc165d3fdcf2522a309af7ff553e2490b814f5fe
    Signed-off-by: Laurent Fasani <laurent.fasani@obeo.fr>

---
+++ plugins/org.eclipse.sirius.common.ui.ext/src/org/eclipse/sirius/common/ui/ext/Activator.java
@@ -0,0 +1,24 @@
+package org.eclipse.sirius.common.ui.ext;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+    private static BundleContext context;
+
+    static BundleContext getContext() {
+        return context;
+    }
+
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        Activator.context = bundleContext;
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        Activator.context = null;
+    }
+
+}
