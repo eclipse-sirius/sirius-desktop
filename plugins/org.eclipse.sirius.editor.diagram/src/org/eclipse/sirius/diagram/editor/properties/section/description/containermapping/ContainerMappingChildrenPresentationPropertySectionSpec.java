d24d766476264a68bf31d60d7bb4473f7a7172af
Maxime Porhel <maxime.porhel@obeo.fr>
2015-05-11 12:04:05 +0200

    [436359] Enable compartiments in VSM editor
    Enable the compartiments in the VSM editor. 
    Disabled stacks container children preentation option in property
    section when the element is a region: a ContainerMapping cannot be a
    Region and a RegionContainer.
    
    Bug: 436359
    Change-Id: I9b4d4cd963e60cf164fba1b7447069810cf1692e
    Signed-off-by: Belqassim Djafer <belqassim.djafer@obeo.fr>
    Signed-off-by: Maxime Porhel <maxime.porhel@obeo.fr>

--- plugins/org.eclipse.sirius.editor.diagram/src/org/eclipse/sirius/diagram/editor/properties/section/description/containermapping/ContainerMappingChildrenPresentationPropertySectionSpec.java
+++ plugins/org.eclipse.sirius.editor.diagram/src/org/eclipse/sirius/diagram/editor/properties/section/description/containermapping/ContainerMappingChildrenPresentationPropertySectionSpec.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 THALES GLOBAL SERVICES.
+ * Copyright (c) 2013, 2015 THALES GLOBAL SERVICES and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,13 +10,11 @@
  *******************************************************************************/
 package org.eclipse.sirius.diagram.editor.properties.section.description.containermapping;
 
-import java.util.Collections;
-import java.util.List;
-
 import org.eclipse.sirius.diagram.ContainerLayout;
+import org.eclipse.sirius.diagram.business.internal.query.ContainerMappingExperimentalQuery;
+import org.eclipse.sirius.diagram.description.ContainerMapping;
 import org.eclipse.sirius.diagram.editor.properties.sections.description.containermapping.ContainerMappingChildrenPresentationPropertySection;
-
-import com.google.common.collect.Lists;
+import org.eclipse.swt.widgets.Button;
 
 /**
  * A section for the childrenPresentation property of a ContainerMapping object.
@@ -26,12 +24,36 @@ import com.google.common.collect.Lists;
  */
 public class ContainerMappingChildrenPresentationPropertySectionSpec extends ContainerMappingChildrenPresentationPropertySection {
 
-    private final List<?> values = Collections.unmodifiableList(Lists.newArrayList(ContainerLayout.FREE_FORM, ContainerLayout.LIST));
+    @Override
+    protected void makeWrittable() {
+        super.makeWrittable();
 
-    /**
-     * @see org.eclipse.sirius.editor.properties.sections.AbstractRadioButtonPropertySection#getEnumerationFeatureValues()
-     */
-    protected List<?> getChoiceOfValues() {
-        return values;
+        if (button != null && eObject instanceof ContainerMapping) {
+            boolean stackButtonEnabled = shouldEnableCompartiments((ContainerMapping) eObject);
+
+            String hStackButtonText = getText(ContainerLayout.HORIZONTAL_STACK);
+            String vStackButtonText = getText(ContainerLayout.VERTICAL_STACK);
+            for (Button b : button) {
+                if (hStackButtonText.equals(b.getText()) || vStackButtonText.equals(b.getText())) {
+                    b.setEnabled(stackButtonEnabled);
+                }
+            }
+        }
+    }
+
+    private boolean shouldEnableCompartiments(ContainerMapping containerMapping) {
+        ContainerMappingExperimentalQuery query = new ContainerMappingExperimentalQuery(containerMapping);
+        
+        boolean enableCompartiments = !query.isRegion();
+        if (enableCompartiments) {
+            for (ContainerMapping subContainer : containerMapping.getAllContainerMappings()) {
+                ContainerMappingExperimentalQuery subQuery = new ContainerMappingExperimentalQuery(subContainer);
+                enableCompartiments = enableCompartiments && !subQuery.isRegionContainer();
+            }
+        }
+        if (enableCompartiments) {
+            enableCompartiments = enableCompartiments && containerMapping.getAllNodeMappings().isEmpty();
+        }
+        return enableCompartiments;
     }
 }
