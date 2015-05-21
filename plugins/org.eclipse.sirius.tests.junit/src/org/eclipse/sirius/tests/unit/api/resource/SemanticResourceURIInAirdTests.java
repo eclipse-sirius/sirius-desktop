b468cc1c294dc25b85633a9b05317d3de1554ce9
Laurent Fasani <laurent.fasani@obeo.fr>
2015-05-21 15:32:30 +0200

    [456351] Make SemanticResourceURIInAirdTests more reliable
    Explicitly add semantic resources rather than using genericSetUp.
    Add explicit save.
    Add test on resourceDescriptor URI
    
    Bug: 456351
    Change-Id: I3bce6ffb60188d3763528154d0f3fe328647433e
    Signed-off-by: Laurent Fasani <laurent.fasani@obeo.fr>

--- plugins/org.eclipse.sirius.tests.junit/src/org/eclipse/sirius/tests/unit/api/resource/SemanticResourceURIInAirdTests.java
+++ plugins/org.eclipse.sirius.tests.junit/src/org/eclipse/sirius/tests/unit/api/resource/SemanticResourceURIInAirdTests.java
@@ -14,14 +14,18 @@ import java.io.File;
 import java.util.Collections;
 import java.util.List;
 
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.sirius.business.api.query.AirDResouceQuery;
+import org.eclipse.sirius.business.api.resource.ResourceDescriptor;
+import org.eclipse.sirius.business.api.session.resource.AirdResource;
 import org.eclipse.sirius.diagram.tools.api.command.IDiagramCommandFactory;
 import org.eclipse.sirius.tests.SiriusTestsPlugin;
 import org.eclipse.sirius.tests.support.api.SiriusTestCase;
 import org.eclipse.sirius.tests.support.internal.helper.AirdFileParser;
-import org.eclipse.sirius.ui.business.api.preferences.SiriusUIPreferencesKeys;
-
-import com.google.common.collect.Lists;
+import org.eclipse.sirius.tools.api.command.semantic.AddSemanticResourceCommand;
+import org.eclipse.sirius.viewpoint.DAnalysis;
 
 /**
  * Ensures that DAnalysis.semanticResources is
@@ -46,11 +50,11 @@ public class SemanticResourceURIInAirdTests extends SiriusTestCase {
 
     private static final String MODEL_FILE_NAME_LIB2 = "LibrarySemRes2.ecore";
 
-    private static final String OTHER_FOLDER_IN_WS = "SiriusLibrary";
+    private static final String OTHER_PROJECT_IN_WS = "SiriusLibrary";
 
-    private static final String RESOURCE_URI_IN_OTHER_PROJECT = "platform:/resource/" + OTHER_FOLDER_IN_WS + "/" + MODEL_FILE_NAME_LIB;
+    private static final String RESOURCE_URI_IN_OTHER_PROJECT = "platform:/resource/" + OTHER_PROJECT_IN_WS + "/" + MODEL_FILE_NAME_LIB;
 
-    private static final String RESOURCE_URI_IN_OTHER_PROJECT2 = "platform:/resource/" + OTHER_FOLDER_IN_WS + "/" + MODEL_FILE_NAME_LIB2;
+    private static final String RESOURCE_URI_IN_OTHER_PROJECT2 = "platform:/resource/" + OTHER_PROJECT_IN_WS + "/" + MODEL_FILE_NAME_LIB2;
 
     @Override
     protected IDiagramCommandFactory getCommandFactory() {
@@ -60,8 +64,10 @@ public class SemanticResourceURIInAirdTests extends SiriusTestCase {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        changeSiriusUIPreference(SiriusUIPreferencesKeys.PREF_SAVE_WHEN_NO_EDITOR.name(), true);
-        changeSiriusUIPreference(SiriusUIPreferencesKeys.PREF_RELOAD_ON_LAST_EDITOR_CLOSE.name(), true);
+        copyFilesToTestProject(SiriusTestsPlugin.PLUGIN_ID, REPRESENTATIONS_FILE_PATH, REPRESENTATIONS_FILE_NAME, MODEL_FILE_NAME, MODEL_FILE_NAME2);
+        copyFiles(SiriusTestsPlugin.PLUGIN_ID, REPRESENTATIONS_FILE_PATH, OTHER_PROJECT_IN_WS + File.separator, MODEL_FILE_NAME_LIB, MODEL_FILE_NAME_LIB2);
+
+        genericSetUp(Collections.<URI> emptyList(), Collections.<URI> emptyList(), true, toURI(TEMPORARY_PROJECT_NAME + "/" + REPRESENTATIONS_FILE_NAME, ResourceURIType.RESOURCE_PLATFORM_URI));
     }
 
     /**
@@ -70,26 +76,40 @@ public class SemanticResourceURIInAirdTests extends SiriusTestCase {
      * 
      * @throws Exception
      */
+    /**
+     * @throws Exception
+     */
     public void testSemanticResourcesTagInAird() throws Exception {
-        copyFilesToTestProject(SiriusTestsPlugin.PLUGIN_ID, REPRESENTATIONS_FILE_PATH, REPRESENTATIONS_FILE_NAME, MODEL_FILE_NAME, MODEL_FILE_NAME2);
-        copyFiles(SiriusTestsPlugin.PLUGIN_ID, REPRESENTATIONS_FILE_PATH, OTHER_FOLDER_IN_WS + File.separator, MODEL_FILE_NAME_LIB, MODEL_FILE_NAME_LIB2);
+        // Check that representationsWithSemanticResourceTag.aird content is
+        // correct after open
+        List<String> semanticResourcesTagContent = AirdFileParser.parseSemanticResourcesTag(TEMPORARY_PROJECT_NAME);
+        assertEquals("Bad semantic resource tag number in aird file", 2, semanticResourcesTagContent.size());
+        assertEquals("Bad semantic resource tag in aird file", MODEL_FILE_NAME, semanticResourcesTagContent.get(0));
+        assertEquals("Bad semantic resource tag in aird file", RESOURCE_URI_IN_OTHER_PROJECT, semanticResourcesTagContent.get(1));
 
-        List<URI> semanticModelURIs = Lists.newArrayList(toURI(TEMPORARY_PROJECT_NAME + "/" + MODEL_FILE_NAME2, ResourceURIType.RESOURCE_PLATFORM_URI),
-                toURI(OTHER_FOLDER_IN_WS + "/" + MODEL_FILE_NAME_LIB2, ResourceURIType.RESOURCE_PLATFORM_URI));
-        // The representationsWithSemanticResourceTag.aird already contains
-        // resourceDescriptor to MODEL_FILE_NAME(local) and
-        // MODEL_FILE_NAME_LIB(in other project than aird)
-        // The genericSetUp method will add the two other semantic resources
-        // MODEL_FILE_NAME2(local) and MODEL_FILE_NAME_LIB2(in other project
-        // than aird)
-        genericSetUp(semanticModelURIs, Collections.<URI> emptyList(), true, toURI(TEMPORARY_PROJECT_NAME + "/" + REPRESENTATIONS_FILE_NAME, ResourceURIType.RESOURCE_PLATFORM_URI));
+        // Add two other semantic resources : MODEL_FILE_NAME2(local) and
+        // MODEL_FILE_NAME_LIB2(in other project than aird)
+        Command addSemanticResourceCmd = new AddSemanticResourceCommand(session, toURI(TEMPORARY_PROJECT_NAME + "/" + MODEL_FILE_NAME2, ResourceURIType.RESOURCE_PLATFORM_URI),
+                new NullProgressMonitor());
+        session.getTransactionalEditingDomain().getCommandStack().execute(addSemanticResourceCmd);
+
+        addSemanticResourceCmd = new AddSemanticResourceCommand(session, toURI(OTHER_PROJECT_IN_WS + "/" + MODEL_FILE_NAME_LIB2, ResourceURIType.RESOURCE_PLATFORM_URI), new NullProgressMonitor());
+        session.getTransactionalEditingDomain().getCommandStack().execute(addSemanticResourceCmd);
+
+        session.save(new NullProgressMonitor());
 
         // Check that the serialization is properly done
-        List<String> semanticResourcesTagContent = AirdFileParser.parseSemanticResourcesTag(TEMPORARY_PROJECT_NAME);
+        semanticResourcesTagContent = AirdFileParser.parseSemanticResourcesTag(TEMPORARY_PROJECT_NAME);
         assertEquals("Bad semantic resource tag number in aird file", 4, semanticResourcesTagContent.size());
         assertEquals("Bad semantic resource tag in aird file", MODEL_FILE_NAME, semanticResourcesTagContent.get(0));
         assertEquals("Bad semantic resource tag in aird file", RESOURCE_URI_IN_OTHER_PROJECT, semanticResourcesTagContent.get(1));
         assertEquals("Bad semantic resource tag in aird file", MODEL_FILE_NAME2, semanticResourcesTagContent.get(2));
         assertEquals("Bad semantic resource tag in aird file", RESOURCE_URI_IN_OTHER_PROJECT2, semanticResourcesTagContent.get(3));
+
+        // Check that all ResourceDescriptor URI are in absolute path
+        DAnalysis dAnalysis = new AirDResouceQuery((AirdResource) session.getSessionResource()).getDAnalysis().get();
+        for (ResourceDescriptor rd : dAnalysis.getSemanticResources()) {
+            assertTrue("Resource descriptor " + rd + " does not contain a absolute URI", rd.getResourceURI().isPlatformResource());
+        }
     }
 }
