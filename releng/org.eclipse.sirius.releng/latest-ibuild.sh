9de3888af3663aee0262c970e33a6ba1e142d33d
Pierre-Charles David <pierre-charles.david@obeo.fr>
2015-08-26 16:29:51 +0200

    [build] Update/fix de canary target platforms
    Change-Id: I3e6032ff51fe6429552a5a5712716593bf75255c
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- releng/org.eclipse.sirius.releng/latest-ibuild.sh
+++ releng/org.eclipse.sirius.releng/latest-ibuild.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-BASE="http://download.eclipse.org/eclipse/updates/4.5-I-builds"
+BASE="http://download.eclipse.org/eclipse/updates/4.6-I-builds"
 TEMPFILE=$(mktemp --suffix=.jar) || exit
 curl -s "$BASE/compositeContent.jar" > "$TEMPFILE"
 LATEST=$(unzip -p "$TEMPFILE" compositeContent.xml | grep "<child location='I" | sort -r | head -1 | sed -e "s/\s*<child location='//" -e "s|'/>\s*||")
