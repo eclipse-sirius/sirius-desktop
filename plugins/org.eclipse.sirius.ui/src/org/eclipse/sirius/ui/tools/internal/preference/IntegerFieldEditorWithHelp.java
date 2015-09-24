c7964b27d1a15569b96cf4e1cc85bcbf3ac5f33c
Pierre-Charles David <pierre-charles.david@obeo.fr>
2015-09-24 14:24:36 +0200

    [ip] Add/fix missing/broken copyright headers
    Change-Id: I76d092927e6606147b9769f12785c8746dd032d9
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/tools/internal/preference/IntegerFieldEditorWithHelp.java
+++ plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/tools/internal/preference/IntegerFieldEditorWithHelp.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2013 THALES GLOBAL SERVICES and others
- * All rights reserved.
+ * Copyright (c) 2007, 2015 THALES GLOBAL SERVICES and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *      Obeo - Initial API and implementation
- *      IBM Corporation - The implementation of a constructor that come from
- *      org.eclipse.jface.preference.IntegerFieldEditor
+ *    Obeo - initial API and implementation
+ *    IBM Corporation - The implementation of a constructor that come from
+ *                      org.eclipse.jface.preference.IntegerFieldEditor
  *******************************************************************************/
 package org.eclipse.sirius.ui.tools.internal.preference;
 
