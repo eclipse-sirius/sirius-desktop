0e233f42b9d417fb22366595d320bf4745ce1a1f
cbrun <cedric.brun@obeo.fr>
2015-09-29 16:47:52 +0200

    [470599]Revert "Use an index for xRefs Setting to pick addUnique faster"
    This reverts commit 1495c2d9258553dfa6564bcfe1597f112bb1aab1 to get back
    to the original state before applying the fix proposed in the EMF
    project.
    
    
    Bug: 470599
    Change-Id: I070a2a81e5f5d8dc2eda8384f2c8f595c8fea5dc
    Signed-off-by: Cedric Brun <cedric.brun@obeo.fr>

--- plugins/org.eclipse.sirius.common/src/org/eclipse/sirius/common/tools/api/util/SiriusCrossReferenceAdapterImpl.java
+++ plugins/org.eclipse.sirius.common/src/org/eclipse/sirius/common/tools/api/util/SiriusCrossReferenceAdapterImpl.java
@@ -10,11 +10,7 @@
  *******************************************************************************/
 package org.eclipse.sirius.common.tools.api.util;
 
-import java.util.Collection;
-
-import org.eclipse.emf.ecore.EStructuralFeature.Setting;
 import org.eclipse.emf.ecore.util.ECrossReferenceAdapter;
-import org.eclipse.sirius.common.tools.internal.ecore.IndexedSettingsEList;
 
 /**
  * Specific {@link ECrossReferenceAdapter} which resolve proxy ability can be
@@ -52,15 +48,5 @@ public class SiriusCrossReferenceAdapterImpl extends ECrossReferenceAdapter impl
             return super.resolve();
         }
         return false;
-    }
-    
-    @Override
-    protected InverseCrossReferencer createInverseCrossReferencer() {
-        return new InverseCrossReferencer() {
-            @Override
-            protected Collection<Setting> newCollection() {
-                return new IndexedSettingsEList();
-            }
-        };
     }
 }
--- plugins/org.eclipse.sirius/src/org/eclipse/sirius/business/internal/session/danalysis/LocalResourceCollector.java
+++ plugins/org.eclipse.sirius/src/org/eclipse/sirius/business/internal/session/danalysis/LocalResourceCollector.java
@@ -27,7 +27,6 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.sirius.business.api.query.ResourceQuery;
 import org.eclipse.sirius.common.tools.api.util.SiriusCrossReferenceAdapterImpl;
-import org.eclipse.sirius.common.tools.internal.ecore.IndexedSettingsEList;
 
 /**
  * A {@link IResourceCollector} for local {@link Resource}.
@@ -224,11 +223,6 @@ public class LocalResourceCollector extends SiriusCrossReferenceAdapterImpl impl
                     }
                 }
             }
-        }
-
-        @Override
-        protected Collection<EStructuralFeature.Setting> newCollection() {
-            return new IndexedSettingsEList();
         }
 
     }
