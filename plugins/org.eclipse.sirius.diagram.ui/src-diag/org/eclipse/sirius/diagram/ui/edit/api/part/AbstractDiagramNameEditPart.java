eclipse-sirius/sirius-desktop

commit e7d07c250cebf1f413fcd0d9c3ad36fecaddd399

[443518] Use DialectServices2.refreshImpactedElements() to refresh DDE
The goal of this commit is to use the new implementation, as for table
and tree in bug 444101, to refresh diagram elements when the refresh
mode is manual.

* The method IDiagramElementEditPart.getEAdapterSemanticElements() has
been removed (and all its implementations).
* The DiagramDialectServices now implements DialectServices2, and method
refreshImpactedElements(DRepresentation, Collection<Notification>,
IProgressMonitor).
* The EObjectQuery has now a new contructor with a
ECrossReferenceAdapter parameter. This can be usefull if there is many
call to inverse references methods for the same Session. This avoids to
compute ECrossReferenceAdapter each time. It also proposes a new method
getInverseReferences(Set<EReference>).

Bug: 443518
Bug: 444101
Change-Id: Ic6306cd8e3f51d98507e1cdacc67e5179f9a0094
Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>

plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/edit/api/part/AbstractDiagramNameEditPart.java
@@ -21,7 +21,6 @@ import org.eclipse.gef.Request;
 import org.eclipse.gef.editpolicies.ResizableEditPolicy;
 import org.eclipse.gmf.runtime.diagram.core.listener.DiagramEventBroker;
 import org.eclipse.gmf.runtime.diagram.core.listener.NotificationListener;
-import org.eclipse.gmf.runtime.diagram.core.listener.NotificationPreCommitListener;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.LabelEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
 import org.eclipse.gmf.runtime.notation.View;
@@ -135,15 +134,6 @@ public abstract class AbstractDiagramNameEditPart extends LabelEditPart implemen
      * @see org.eclipse.sirius.diagram.edit.api.part.IDiagramElementEditPart#getEAdapterDiagramElement()
      */
     public NotificationListener getEAdapterDiagramElement() {
-        return null;
-    }
-
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.diagram.edit.api.part.IDiagramElementEditPart#getEAdapterSemanticElements()
-     */
-    public NotificationPreCommitListener getEAdapterSemanticElements() {
         return null;
     }
 

plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/edit/internal/part/DiagramElementEditPartOperation.java
@@ -18,7 +18,6 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Label;
-import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
@@ -60,7 +59,6 @@ import org.eclipse.sirius.diagram.ui.tools.api.figure.StyledFigure;
 import org.eclipse.sirius.diagram.ui.tools.api.graphical.edit.styles.IStyleConfigurationRegistry;
 import org.eclipse.sirius.diagram.ui.tools.api.graphical.edit.styles.StyleConfiguration;
 import org.eclipse.sirius.diagram.ui.tools.api.part.IDiagramDialectGraphicalViewer;
-import org.eclipse.sirius.diagram.ui.tools.internal.commands.SemanticChangedCommand;
 import org.eclipse.sirius.ecore.extender.business.api.permission.IPermissionAuthority;
 import org.eclipse.sirius.ecore.extender.business.api.permission.PermissionAuthorityRegistry;
 import org.eclipse.sirius.ui.tools.api.color.VisualBindingManager;
@@ -156,24 +154,6 @@ public final class DiagramElementEditPartOperation {
             return ((DSemanticDecorator) semanticHost).getTarget();
         }
         return null;
-    }
-
-    /**
-     * Creates a listener for semantic elements.
-     * 
-     * @param self
-     *            the edit part.
-     * @return the listener to install on semantic elements.
-     */
-    public static NotificationPreCommitListener createEAdpaterSemanticElements(final IDiagramElementEditPart self) {
-
-        return new NotificationPreCommitListener() {
-
-            public Command transactionAboutToCommit(final Notification msg) {
-                return new SemanticChangedCommand(self.getEditingDomain(), self, msg);
-            }
-
-        };
     }
 
     /**
@@ -395,9 +375,6 @@ public final class DiagramElementEditPartOperation {
         addNavigateDecoratorRefresher(self, broker);
         while (iterSemanticElements.hasNext()) {
             final EObject semantic = iterSemanticElements.next();
-            if (self.getEAdapterSemanticElements() != null) {
-                broker.addNotificationListener(semantic, self.getEAdapterSemanticElements());
-            }
             if (self.getEditModeListener() != null) {
                 broker.addNotificationListener(semantic, self.getEditModeListener());
             }
@@ -479,7 +456,6 @@ public final class DiagramElementEditPartOperation {
                 removeNavigateDecoratorRefresher(self, broker);
                 while (iterSemanticElements.hasNext()) {
                     final EObject semantic = iterSemanticElements.next();
-                    DiagramElementEditPartOperation.removeListener(broker, semantic, self.getEAdapterSemanticElements());
                     DiagramElementEditPartOperation.removeListener(broker, semantic, self.getEditModeListener());
                     // remove this try/catch once the offline mode
                     // will be supported

plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/edit/api/part/AbstractBorderedDiagramElementEditPart.java
@@ -16,7 +16,6 @@ import org.eclipse.draw2d.IFigure;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gef.editparts.ZoomManager;
 import org.eclipse.gmf.runtime.diagram.core.listener.NotificationListener;
-import org.eclipse.gmf.runtime.diagram.core.listener.NotificationPreCommitListener;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.AbstractBorderedShapeEditPart;
 import org.eclipse.gmf.runtime.draw2d.ui.figures.IBorderItemLocator;
 import org.eclipse.gmf.runtime.notation.View;
@@ -43,9 +42,6 @@ public abstract class AbstractBorderedDiagramElementEditPart extends AbstractBor
 
     /** Listens the diagram element. */
     private NotificationListener adapterDiagramElement;
-
-    /** Listens the semantic elements. */
-    private NotificationPreCommitListener adapterSemanticElements;
 
     /** listen to semantic elements container */
     private NotificationListener editModeListener = new EditStatusUpdater(this);
@@ -92,18 +88,6 @@ public abstract class AbstractBorderedDiagramElementEditPart extends AbstractBor
             this.adapterDiagramElement = DiagramElementEditPartOperation.createEApdaterDiagramElement(this);
         }
         return this.adapterDiagramElement;
-    }
-
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.diagram.edit.api.part.IDiagramElementEditPart#getEAdapterSemanticElements()
-     */
-    public NotificationPreCommitListener getEAdapterSemanticElements() {
-        if (this.adapterSemanticElements == null) {
-            this.adapterSemanticElements = DiagramElementEditPartOperation.createEAdpaterSemanticElements(this);
-        }
-        return this.adapterSemanticElements;
     }
 
     /**

plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/edit/api/part/AbstractDiagramBorderNodeEditPart.java
@@ -34,7 +34,6 @@ import org.eclipse.gef.requests.SelectionRequest;
 import org.eclipse.gmf.runtime.common.core.command.CompositeCommand;
 import org.eclipse.gmf.runtime.common.core.util.Proxy;
 import org.eclipse.gmf.runtime.diagram.core.listener.NotificationListener;
-import org.eclipse.gmf.runtime.diagram.core.listener.NotificationPreCommitListener;
 import org.eclipse.gmf.runtime.diagram.ui.commands.CommandProxy;
 import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
 import org.eclipse.gmf.runtime.diagram.ui.commands.SetBoundsCommand;
@@ -86,9 +85,6 @@ public abstract class AbstractDiagramBorderNodeEditPart extends BorderedBorderIt
 
     /** Listens the diagram element. */
     private NotificationListener adapterDiagramElement;
-
-    /** Listens the semantic elements. */
-    private NotificationPreCommitListener adapterSemanticElements;
 
     private NotificationListener editModeListener = new EditStatusUpdater(this);
 
@@ -169,18 +165,6 @@ public abstract class AbstractDiagramBorderNodeEditPart extends BorderedBorderIt
             this.adapterDiagramElement = DiagramElementEditPartOperation.createEApdaterDiagramElement(this);
         }
         return this.adapterDiagramElement;
-    }
-
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.diagram.edit.api.part.IDiagramElementEditPart#getEAdapterSemanticElements()
-     */
-    public NotificationPreCommitListener getEAdapterSemanticElements() {
-        if (this.adapterSemanticElements == null) {
-            this.adapterSemanticElements = DiagramElementEditPartOperation.createEAdpaterSemanticElements(this);
-        }
-        return this.adapterSemanticElements;
     }
 
     /**

plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/edit/api/part/AbstractDiagramEdgeEditPart.java
@@ -94,9 +94,6 @@ public abstract class AbstractDiagramEdgeEditPart extends ConnectionNodeEditPart
     /** Listens the diagram element. */
     private NotificationListener adapterDiagramElement;
 
-    /** Listens the semantic elements. */
-    private NotificationPreCommitListener adapterSemanticElements;
-
     /** Listens the routing style. */
     private NotificationPreCommitListener adapterRoutingStyle;
 
@@ -306,14 +303,6 @@ public abstract class AbstractDiagramEdgeEditPart extends ConnectionNodeEditPart
             this.adapterDiagramElement = DiagramElementEditPartOperation.createEApdaterDiagramElement(this);
         }
         return this.adapterDiagramElement;
-    }
-
-    @Override
-    public NotificationPreCommitListener getEAdapterSemanticElements() {
-        if (this.adapterSemanticElements == null) {
-            this.adapterSemanticElements = DiagramElementEditPartOperation.createEAdpaterSemanticElements(this);
-        }
-        return this.adapterSemanticElements;
     }
 
     @Override

plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/edit/api/part/IDiagramElementEditPart.java
@@ -14,7 +14,6 @@ import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.diagram.core.listener.NotificationListener;
-import org.eclipse.gmf.runtime.diagram.core.listener.NotificationPreCommitListener;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.sirius.diagram.DDiagramElement;
 import org.eclipse.sirius.diagram.ui.tools.api.permission.EditPartAuthorityListener;
@@ -44,14 +43,6 @@ public interface IDiagramElementEditPart extends IGraphicalEditPart, ISiriusEdit
      *         represented by this edit part.
      */
     EObject resolveTargetSemanticElement();
-
-    /**
-     * Returns the listener to install on semantic elements.
-     * 
-     * @return the listener to install on semantic elements, <code>null</code>
-     *         if there is none.
-     */
-    NotificationPreCommitListener getEAdapterSemanticElements();
 
     /**
      * Returns the adapter to install on the diagram element.

commit 0070ebeeac67a8732336a4ba11a8dcb34e35a1ab

[475685] Have icon and label refreshed on semantic change
- Manage the case of XXXItemProvider.getImage()/getText() customization.
Indeed in some customization, for example on
uml::NamedElement.visibility change,
XXXItemProvider.getImage()/getText() returns an image with a decoration
different according to uml::NamedElement.visibility attribute value.
- This fix a regression from e7d07c250cebf1f413fcd0d9c3ad36fecaddd399 :
[443518] Use DialectServices2.refreshImpactedElements() to refresh DDE.

Bug: 475685
Change-Id: I75b9b86e22f017a66df2d5165f0ad79bf602c791
Signed-off-by: Esteban Dugueperoux <esteban.dugueperoux@obeo.fr>

plugins/org.eclipse.sirius.diagram.ui/META-INF/MANIFEST.MF
@@ -79,6 +79,7 @@ Export-Package: org.eclipse.sirius.diagram.description.concern.provider;version=
  org.eclipse.sirius.diagram.ui.internal.edit.commands;x-internal:=true;version="3.0.0",
  org.eclipse.sirius.diagram.ui.internal.edit.handles;x-internal:=true;version="3.1.0",
  org.eclipse.sirius.diagram.ui.internal.edit.parts;x-internal:=true;version="3.1.0",
+ org.eclipse.sirius.diagram.ui.internal.edit.parts.refresh;x-internal:=true;version="3.1.0",
  org.eclipse.sirius.diagram.ui.internal.edit.parts.locator;x-internal:=true;version="3.1.0",
  org.eclipse.sirius.diagram.ui.internal.edit.policies;x-internal:=true;version="3.0.0",
   org.eclipse.sirius.diagram.ui.internal.operation;x-internal:=true;version="3.0.0",

plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/edit/api/part/AbstractDiagramNameEditPart.java
@@ -36,6 +36,7 @@ import org.eclipse.sirius.diagram.ui.edit.internal.part.DiagramElementEditPartOp
 import org.eclipse.sirius.diagram.ui.graphical.edit.policies.LabelDeletionEditPolicy;
 import org.eclipse.sirius.diagram.ui.graphical.edit.policies.LabelSemanticEditPolicy;
 import org.eclipse.sirius.diagram.ui.graphical.edit.policies.ToolBasedLabelDirectEditPolicy;
+import org.eclipse.sirius.diagram.ui.internal.edit.parts.refresh.LabelAndIconRefresher;
 import org.eclipse.sirius.diagram.ui.tools.api.permission.EditPartAuthorityListener;
 import org.eclipse.sirius.diagram.ui.tools.api.requests.RequestConstants;
 import org.eclipse.sirius.viewpoint.DMappingBased;
@@ -48,6 +49,8 @@ import org.eclipse.swt.graphics.Image;
  */
 public abstract class AbstractDiagramNameEditPart extends LabelEditPart implements IDiagramNameEditPart {
 
+    private LabelAndIconRefresher labelAndIconRefresher;
+
     /**
      * Creates a new <code>AbstractDiagramNameEditPart</code>.
      * 
@@ -56,6 +59,12 @@ public abstract class AbstractDiagramNameEditPart extends LabelEditPart implemen
      */
     public AbstractDiagramNameEditPart(final View view) {
         super(view);
+    }
+
+    @Override
+    public void activate() {
+        super.activate();
+        labelAndIconRefresher = new LabelAndIconRefresher(this);
     }
 
     /**
@@ -310,6 +319,13 @@ public abstract class AbstractDiagramNameEditPart extends LabelEditPart implemen
             // (and the nullity of the returned adapter is checked).
             return null;
         }
+    }
+
+    @Override
+    public void deactivate() {
+        labelAndIconRefresher.dispose();
+        labelAndIconRefresher = null;
+        super.deactivate();
     }
 
 }

plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/edit/api/part/AbstractNotSelectableShapeNodeEditPart.java
@@ -12,6 +12,7 @@ package org.eclipse.sirius.diagram.ui.edit.api.part;
 
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeNodeEditPart;
 import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.sirius.diagram.ui.internal.edit.parts.refresh.LabelAndIconRefresher;
 
 /**
  * <p>
@@ -28,6 +29,8 @@ import org.eclipse.gmf.runtime.notation.View;
 // Select Object not clearly update
 public abstract class AbstractNotSelectableShapeNodeEditPart extends ShapeNodeEditPart {
 
+    private LabelAndIconRefresher labelAndIconRefresher;
+
     /**
      * Creates a new AbstractUnselectableShapeNodeEditPart.
      * 
@@ -38,14 +41,23 @@ public abstract class AbstractNotSelectableShapeNodeEditPart extends ShapeNodeEd
         super(view);
     }
 
-    /**
-     * 
-     * {@inheritDoc}
-     */
+    @Override
+    public void activate() {
+        super.activate();
+        labelAndIconRefresher = new LabelAndIconRefresher(this);
+    }
+
     @Override
     public boolean isSelectable() {
         // We simply always return false so that this EditPart cannot be
         // selected manually or automatically
         return false;
+    }
+
+    @Override
+    public void deactivate() {
+        labelAndIconRefresher.dispose();
+        labelAndIconRefresher = null;
+        super.deactivate();
     }
 }

plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/edit/internal/part/DiagramElementEditPartOperation.java
@@ -52,6 +52,7 @@ import org.eclipse.sirius.diagram.ui.business.internal.edit.helpers.LabelAlignme
 import org.eclipse.sirius.diagram.ui.business.internal.query.StyleConfigurationQuery;
 import org.eclipse.sirius.diagram.ui.edit.api.part.IDiagramElementEditPart;
 import org.eclipse.sirius.diagram.ui.edit.api.part.IStyleEditPart;
+import org.eclipse.sirius.diagram.ui.internal.edit.parts.AbstractGeneratedDiagramNameEditPart;
 import org.eclipse.sirius.diagram.ui.provider.DiagramUIPlugin;
 import org.eclipse.sirius.diagram.ui.provider.Messages;
 import org.eclipse.sirius.diagram.ui.tools.api.figure.SiriusWrapLabel;
@@ -666,6 +667,35 @@ public final class DiagramElementEditPartOperation {
                     ConstrainedToolbarLayout ctl = (ConstrainedToolbarLayout) layoutManager;
                     ctl.setMinorAlignment(LabelAlignmentHelper.getAsCTLMinorAlignment(alignment));
                 }
+            }
+        }
+    }
+
+    /**
+     * Refresh the label and icon.
+     * 
+     * @param diagramElementEditPart
+     *            the {@link IDiagramElementEditPart}
+     * @param dDiagramElement
+     *            the {@link DDiagramElement} having style definition
+     * @param siriusWrapLabel
+     *            the figure owning label and icon to refresh
+     */
+    public static void refreshLabelAndIcon(IDiagramElementEditPart diagramElementEditPart, DDiagramElement dDiagramElement, SiriusWrapLabel siriusWrapLabel) {
+        if (siriusWrapLabel != null && siriusWrapLabel.getParent() != null && dDiagramElement != null) {
+            DiagramElementEditPartOperation.refreshFont(diagramElementEditPart, dDiagramElement, siriusWrapLabel);
+            StyleConfiguration styleConfiguration = IStyleConfigurationRegistry.INSTANCE.getStyleConfiguration(dDiagramElement.getDiagramElementMapping(), dDiagramElement.getStyle());
+            Image labelIcon = new StyleConfigurationQuery(styleConfiguration).getLabelIcon(dDiagramElement, diagramElementEditPart);
+            String label = null;
+            if (diagramElementEditPart instanceof AbstractGeneratedDiagramNameEditPart) {
+                label = ((AbstractGeneratedDiagramNameEditPart) diagramElementEditPart).getLabelText();
+            } else {
+                label = dDiagramElement.getName();
+            }
+            siriusWrapLabel.setText(label);
+            siriusWrapLabel.setIcon(labelIcon);
+            if (dDiagramElement instanceof DNode) {
+                styleConfiguration.adaptNodeLabel((DNode) dDiagramElement, siriusWrapLabel);
             }
         }
     }

plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/internal/edit/parts/AbstractGeneratedDiagramNameEditPart.java
@@ -37,7 +37,6 @@ import org.eclipse.gmf.runtime.diagram.ui.tools.TextDirectEditManager;
 import org.eclipse.gmf.runtime.emf.core.util.EObjectAdapter;
 import org.eclipse.gmf.runtime.emf.type.core.IElementType;
 import org.eclipse.gmf.runtime.emf.ui.services.parser.ISemanticParser;
-import org.eclipse.gmf.runtime.notation.FontStyle;
 import org.eclipse.gmf.runtime.notation.NotationPackage;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jface.text.contentassist.IContentAssistProcessor;
@@ -93,7 +92,7 @@ public abstract class AbstractGeneratedDiagramNameEditPart extends AbstractDiagr
     /**
      * @was-generated
      */
-    protected String getLabelText() {
+    public String getLabelText() {
         String text = null;
         final EObject parserElement = getParserElement();
         if (parserElement != null && getParser() != null) {

plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/internal/edit/parts/refresh/LabelAndIconRefresher.java
@@ -1,0 +1,135 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.diagram.ui.internal.edit.parts.refresh;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gmf.runtime.diagram.core.listener.DiagramEventBroker;
+import org.eclipse.gmf.runtime.diagram.core.listener.NotificationListener;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
+import org.eclipse.sirius.diagram.DDiagramElement;
+import org.eclipse.sirius.diagram.ui.edit.api.part.AbstractDiagramNameEditPart;
+import org.eclipse.sirius.diagram.ui.edit.api.part.AbstractNotSelectableShapeNodeEditPart;
+import org.eclipse.sirius.diagram.ui.edit.api.part.IAbstractDiagramNodeEditPart;
+import org.eclipse.sirius.diagram.ui.edit.api.part.IDiagramElementEditPart;
+import org.eclipse.sirius.diagram.ui.edit.internal.part.DiagramElementEditPartOperation;
+import org.eclipse.sirius.diagram.ui.tools.api.figure.SiriusWrapLabel;
+import org.eclipse.sirius.viewpoint.ViewpointPackage;
+
+/**
+ * Icon refresher on semantic changes to manage when
+ * XXXItemProvider.getImage()/getText() is customized to return different result
+ * on semantic change
+ * 
+ * @author <a href="mailto:esteban.dugueperoux@obeo.fr">Esteban Dugueperoux</a>
+ */
+public class LabelAndIconRefresher implements NotificationListener {
+
+    private IGraphicalEditPart graphicalEditPart;
+
+    public LabelAndIconRefresher(IGraphicalEditPart graphicalEditPart) {
+        this.graphicalEditPart = graphicalEditPart;
+        DiagramEventBroker broker = DiagramEventBroker.getInstance(graphicalEditPart.getEditingDomain());
+        DDiagramElement dDiagramElement = getDDiagramElement(graphicalEditPart);
+        if (broker != null && dDiagramElement != null) {
+            for (EObject semanticElement : dDiagramElement.getSemanticElements()) {
+                broker.addNotificationListener(semanticElement, this);
+            }
+            broker.addNotificationListener(dDiagramElement, ViewpointPackage.Literals.DREPRESENTATION_ELEMENT__SEMANTIC_ELEMENTS, this);
+        }
+    }
+
+    private DDiagramElement getDDiagramElement(IGraphicalEditPart graphicalEditPart) {
+        DDiagramElement dDiagramElement = null;
+        if (graphicalEditPart instanceof IDiagramElementEditPart) {
+            dDiagramElement = ((IDiagramElementEditPart) graphicalEditPart).resolveDiagramElement();
+        } else if (graphicalEditPart.getParent() instanceof IDiagramElementEditPart) {
+            dDiagramElement = ((IDiagramElementEditPart) graphicalEditPart.getParent()).resolveDiagramElement();
+        }
+        return dDiagramElement;
+    }
+
+    @Override
+    public void notifyChanged(Notification notification) {
+        if (graphicalEditPart != null && graphicalEditPart.isActive()) {
+            DDiagramElement dDiagramElement = getDDiagramElement(graphicalEditPart);
+            if (dDiagramElement != null && dDiagramElement.getSemanticElements().contains(notification.getNotifier())) {
+                refreshLabelAndIcon(dDiagramElement);
+            } else if (ViewpointPackage.Literals.DREPRESENTATION_ELEMENT__SEMANTIC_ELEMENTS == notification.getFeature()) {
+                // Manage change on DRepresentationElement.semanticElements
+                // reference
+                DiagramEventBroker broker = DiagramEventBroker.getInstance(graphicalEditPart.getEditingDomain());
+                if (notification.getEventType() == Notification.ADD || notification.getEventType() == Notification.ADD_MANY) {
+                    Object newValue = notification.getNewValue();
+                    if (newValue instanceof Collection<?>) {
+                        for (Object newVal : (Collection<?>) newValue) {
+                            if (newVal instanceof EObject) {
+                                broker.addNotificationListener((EObject) newVal, this);
+                            }
+                        }
+                    } else if (newValue instanceof EObject) {
+                        broker.addNotificationListener((EObject) newValue, this);
+                    }
+                } else if (notification.getEventType() == Notification.REMOVE || notification.getEventType() == Notification.REMOVE_MANY) {
+                    Object oldValue = notification.getOldValue();
+                    if (oldValue instanceof Collection<?>) {
+                        for (Object oldVal : (Collection<?>) oldValue) {
+                            if (oldVal instanceof EObject) {
+                                broker.removeNotificationListener((EObject) oldVal, this);
+                            }
+                        }
+                    } else if (oldValue instanceof EObject) {
+                        broker.removeNotificationListener((EObject) oldValue, this);
+                    }
+                }
+            }
+        }
+    }
+
+    private void refreshLabelAndIcon(DDiagramElement dDiagramElement) {
+        SiriusWrapLabel siriusWrapLabel = null;
+        IDiagramElementEditPart diagramElementEditPart = null;
+        if (graphicalEditPart instanceof AbstractDiagramNameEditPart) {
+            if (graphicalEditPart.getFigure() instanceof SiriusWrapLabel) {
+                siriusWrapLabel = (SiriusWrapLabel) graphicalEditPart.getFigure();
+                diagramElementEditPart = (AbstractDiagramNameEditPart) graphicalEditPart;
+            }
+        } else if (graphicalEditPart instanceof AbstractNotSelectableShapeNodeEditPart) {
+            EditPart parent = graphicalEditPart.getParent();
+            if (parent instanceof IAbstractDiagramNodeEditPart) {
+                diagramElementEditPart = (IAbstractDiagramNodeEditPart) parent;
+                siriusWrapLabel = ((IAbstractDiagramNodeEditPart) diagramElementEditPart).getNodeLabel();
+            }
+        }
+        if (diagramElementEditPart != null) {
+            DiagramElementEditPartOperation.refreshLabelAndIcon(diagramElementEditPart, dDiagramElement, siriusWrapLabel);
+        }
+    }
+
+    public void dispose() {
+        DiagramEventBroker broker = DiagramEventBroker.getInstance(graphicalEditPart.getEditingDomain());
+        if (broker != null) {
+            DDiagramElement dDiagramElement = getDDiagramElement(graphicalEditPart);
+            if (dDiagramElement != null) {
+                broker.removeNotificationListener(dDiagramElement, ViewpointPackage.Literals.DREPRESENTATION_ELEMENT__SEMANTIC_ELEMENTS, this);
+                for (EObject semanticElement : dDiagramElement.getSemanticElements()) {
+                    broker.removeNotificationListener(semanticElement, this);
+                }
+            }
+
+        }
+        graphicalEditPart = null;
+    }
+
+}

plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/graphical/edit/styles/SquareStyleConfiguration.java
@@ -14,6 +14,7 @@ import org.eclipse.draw2d.ConnectionAnchor;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.geometry.PrecisionPoint;
 import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gmf.runtime.diagram.ui.internal.figures.BorderItemContainerFigure;
 import org.eclipse.gmf.runtime.gef.ui.figures.SlidableAnchor;
 import org.eclipse.sirius.diagram.DNode;
 import org.eclipse.sirius.diagram.ResizeKind;
@@ -66,7 +67,11 @@ class SquareStyleConfiguration extends SimpleStyleConfiguration {
             final int height = (int) (origin.height - (origin.height * Y1 / 100 + origin.height * Y2 / 100));
             final Rectangle bounds = new Rectangle(x, y, width, height);
             nodeLabel.setBounds(bounds);
-            nodeLabel.getParent().setConstraint(nodeLabel, bounds);
+            // Don't set a Rectangle as constraint for BorderItemContainerFigure
+            // as a IBorderItemLocator is expected for constraint consumer
+            if (!(nodeLabel.getParent() instanceof BorderItemContainerFigure)) {
+                nodeLabel.getParent().setConstraint(nodeLabel, bounds);
+            }
         }
     }
 
@@ -123,6 +128,7 @@ class SquareStyleConfiguration extends SimpleStyleConfiguration {
          * @see org.eclipse.sirius.transversal.figure.anchor.AnchorProvider#createAnchor(org.eclipse.sirius.diagram.ui.tools.api.figure.common.ui.tool.api.graphical.figure.AirDefaultSizeNodeFigure,
          *      org.eclipse.draw2d.geometry.PrecisionPoint)
          */
+        @Override
         public ConnectionAnchor createAnchor(final AirDefaultSizeNodeFigure figure, final PrecisionPoint p) {
             if (p == null) {
                 return createDefaultAnchor(figure);
@@ -133,6 +139,7 @@ class SquareStyleConfiguration extends SimpleStyleConfiguration {
         /**
          * @see org.eclipse.sirius.transversal.figure.anchor.AnchorProvider#createDefaultAnchor(org.eclipse.sirius.diagram.ui.tools.api.figure.common.ui.tool.api.graphical.figure.AirDefaultSizeNodeFigure)
          */
+        @Override
         public ConnectionAnchor createDefaultAnchor(final AirDefaultSizeNodeFigure figure) {
             return new SquareStyleSlidableAnchor(figure);
         }