de6a08e7c0af0de76bc4b990a065470e6de26b97
Esteban Dugueperoux <esteban.dugueperoux@obeo.fr>
2015-11-09 09:32:37 +0100

    [481685] SemanticPropertySection should dispose its fields
    Bug: 481685
    Change-Id: I0f940f6f9fb13e62fbb620531325b9d09ffd86f0
    Signed-off-by: Esteban Dugueperoux <esteban.dugueperoux@obeo.fr>

--- plugins/org.eclipse.sirius.table.ui/src/org/eclipse/sirius/table/ui/tools/internal/properties/section/semantic/SemanticPropertySection.java
+++ plugins/org.eclipse.sirius.table.ui/src/org/eclipse/sirius/table/ui/tools/internal/properties/section/semantic/SemanticPropertySection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2011 THALES GLOBAL SERVICES.
+ * Copyright (c) 2007, 2015 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -35,9 +35,7 @@ import org.eclipse.ui.views.properties.IPropertySource;
  */
 public class SemanticPropertySection extends AbstractDTablePropertySection {
 
-    /**
-     * {@inheritDoc}
-     */
+    @Override
     public IPropertySource getPropertySource(final Object object) {
         IPropertySource propSrc = null;
 
