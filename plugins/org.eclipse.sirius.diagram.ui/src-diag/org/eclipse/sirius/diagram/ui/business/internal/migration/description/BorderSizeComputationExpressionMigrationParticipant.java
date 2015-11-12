881e92f6c06a0429d223da3bb32eaf01c3a37938
Maxime Porhel <maxime.porhel@obeo.fr>
2015-11-12 11:03:20 +0100

    [481836] Complete and correct border size migration
    The VSM migration was done only on the first level of container
    mappings. 
    Now we also recursively handle the sub container mappings. 
    
    A manual modification might be required for VSM already migrated to
    Sirius 3.1.0, 3.1.1 or 3.1.2 (see release notes for more details).
    
    Bug: 481836
    Change-Id: Id636c875b541503a9e5c3edcd28ef31b0a1a10ce
    Signed-off-by: Maxime Porhel <maxime.porhel@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/business/internal/migration/description/BorderSizeComputationExpressionMigrationParticipant.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/business/internal/migration/description/BorderSizeComputationExpressionMigrationParticipant.java
@@ -38,7 +38,11 @@ public class BorderSizeComputationExpressionMigrationParticipant extends Abstrac
     /**
      * The version for which this migration is added.
      */
-    private static final Version MIGRATION_VERSION = new Version("10.1.0.201507101000"); //$NON-NLS-1$
+    private static final Version MIGRATION_VERSION = new Version("11.0.0.201511131800"); //$NON-NLS-1$
+
+    private static final Version INITIAL_MIGRATION_VERSION = new Version("10.1.0.201507101000"); //$NON-NLS-1$
+
+    private static final Version ALREADY_MIGRATED_VERSION = new Version("10.1.3.201511131800"); //$NON-NLS-1$
 
     @Override
     public Version getMigrationVersion() {
@@ -50,35 +54,76 @@ public class BorderSizeComputationExpressionMigrationParticipant extends Abstrac
         // Set the borderSizeComputationExpression to "1" (to replace 0 default
         // value) for style of container mapping, before the bug 465211 0 is
         // considered as 1.
+
+        // Replace all borderSize of Container equals to 0 by 1 and replace all
+        // borderSizeComputationExpression of Container equals to "0" by "1". If
+        // the expression is not "0" (default value), we let the borderSize
+        // unchanged.
         if (loadedVersion.compareTo(MIGRATION_VERSION) < 0) {
-            for (Viewpoint viewpoint : group.getOwnedViewpoints()) {
-                for (DiagramDescription diagramDescription : Iterables.filter(viewpoint.getOwnedRepresentations(), DiagramDescription.class)) {
-                    migrateStyleOfContainerMappingInLayer(diagramDescription.getAllLayers());
-                    migrateStyleOfContainerMapping(diagramDescription.getAllContainerMappings());
+            // The Sirius 3.1.3 maintenance version already contains the
+            // complete migration.
+            // Full migration is required for version in [0.0.0, 3.1.0[
+            // Manual complementary modification might be required for version
+            // in [3.1.0, 3.1.3[
+
+            if (loadedVersion.compareTo(INITIAL_MIGRATION_VERSION) < 0) {
+                // Full migration
+                migrateBorderSizeComputationExpressions(group);
+            } else if (loadedVersion.compareTo(ALREADY_MIGRATED_VERSION) < 0) {
+                // Do nothing:
+                // first level of container mappings have been migrated.
+                // sub container mappings were with the previous version of the
+                // migration but in Sirius 3.1.0, specifier might have
+                // explicitly added styles with a 0 pix border: the migration
+                // cannot take the decision to do the migration or not.
+            } else {
+                // Files saved with 3.1.3, are already migrated.
+            }
+        }
+    }
+
+    private void migrateBorderSizeComputationExpressions(Group group) {
+        for (Viewpoint viewpoint : group.getOwnedViewpoints()) {
+            for (DiagramDescription diagramDescription : Iterables.filter(viewpoint.getOwnedRepresentations(), DiagramDescription.class)) {
+                // Migrate container mappings from default and additional layers
+                for (Layer layer : diagramDescription.getAllLayers()) {
+                    // Migrate container mappings contained by the layer (no
+                    // need to migrate reused mappings, they will be migrated by
+                    // the migration of their containing resource).
+                    migrateBorderSizeComputationExpressions(layer.getContainerMappings());
                 }
-                for (DiagramExtensionDescription diagramExtensionDescription : Iterables.filter(viewpoint.getOwnedRepresentationExtensions(), DiagramExtensionDescription.class)) {
-                    migrateStyleOfContainerMappingInLayer(diagramExtensionDescription.getLayers());
+
+                // Migrate container mappings contained by the diagram
+                // description (old without layer mode)
+                migrateBorderSizeComputationExpressions(diagramDescription.getContainerMappings());
+            }
+            for (DiagramExtensionDescription diagramExtensionDescription : Iterables.filter(viewpoint.getOwnedRepresentationExtensions(), DiagramExtensionDescription.class)) {
+                for (Layer layer : diagramExtensionDescription.getLayers()) {
+                    migrateBorderSizeComputationExpressions(layer.getContainerMappings());
                 }
             }
         }
     }
 
-    private void migrateStyleOfContainerMappingInLayer(List<? extends Layer> layers) {
-        for (Layer layer : layers) {
-            migrateStyleOfContainerMapping(layer.getContainerMappings());
-        }
-    }
-
-    private void migrateStyleOfContainerMapping(List<ContainerMapping> containerMappings) {
+    /**
+     * Recursively migrate the given containers and their sub containers.
+     * @param containerMappings the container mappings to migrate.
+     */
+    private void migrateBorderSizeComputationExpressions(List<ContainerMapping> containerMappings) {
         for (ContainerMapping containerMapping : containerMappings) {
-            migrateStyleOfContainerMapping(containerMapping.getStyle());
+
+            // Migrate the styles of the current container mapping.
+            migrateBorderSizeComputationExpression(containerMapping.getStyle());
             for (ConditionalContainerStyleDescription conditionalStyleDescription : containerMapping.getConditionnalStyles()) {
-                migrateStyleOfContainerMapping(conditionalStyleDescription.getStyle());
+                migrateBorderSizeComputationExpression(conditionalStyleDescription.getStyle());
             }
+
+            // Migrate the sub container mappings.
+            migrateBorderSizeComputationExpressions(containerMapping.getSubContainerMappings());
         }
     }
 
-    private void migrateStyleOfContainerMapping(ContainerStyleDescription styleDescription) {
+    private void migrateBorderSizeComputationExpression(ContainerStyleDescription styleDescription) {
         if (styleDescription != null && "0".equals(styleDescription.getBorderSizeComputationExpression())) { //$NON-NLS-1$
             styleDescription.setBorderSizeComputationExpression("1"); //$NON-NLS-1$
         }
