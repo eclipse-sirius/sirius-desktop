c69639bc01e2332cbb4dabf04cec53aa483b7c67
Pierre-Charles David <pierre-charles.david@obeo.fr>
2015-12-04 16:11:06 +0100

    [482528] Extract model conversion into a separate class
    Add ViewDescriptionConverter to contain the conversion logic from the
    high-level description found in a Sirius VSM into the lower-level model
    used by the EEF runtime.
    
    Bug: 482528
    Change-Id: I528d1f53f8c4779b6f257c12064987dc0fea4305
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- incubation/org.eclipse.sirius.ui.properties/src/org/eclipse/sirius/ui/properties/internal/SiriusTabDescriptorProvider.java
+++ incubation/org.eclipse.sirius.ui.properties/src/org/eclipse/sirius/ui/properties/internal/SiriusTabDescriptorProvider.java
@@ -17,12 +17,7 @@ import java.util.List;
 import java.util.Set;
 
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.eef.EEFContainerDescription;
-import org.eclipse.eef.EEFGroupDescription;
-import org.eclipse.eef.EEFPageDescription;
-import org.eclipse.eef.EEFTextDescription;
 import org.eclipse.eef.EEFViewDescription;
-import org.eclipse.eef.EefFactory;
 import org.eclipse.eef.core.api.EEFExpressionUtils;
 import org.eclipse.eef.core.api.EEFPage;
 import org.eclipse.eef.core.api.EEFVariableManagerFactory;
@@ -32,7 +27,6 @@ import org.eclipse.eef.core.api.IVariableManager;
 import org.eclipse.eef.ide.ui.internal.properties.EEFTabDescriptor;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ConnectionEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
 import org.eclipse.jface.viewers.ISelection;
@@ -41,16 +35,10 @@ import org.eclipse.sirius.business.api.query.EObjectQuery;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.common.interpreter.api.IInterpreterProvider;
 import org.eclipse.sirius.ext.emf.AllContents;
-import org.eclipse.sirius.properties.ContainerDescription;
-import org.eclipse.sirius.properties.GroupDescription;
-import org.eclipse.sirius.properties.PageDescription;
 import org.eclipse.sirius.properties.PropertiesPackage;
-import org.eclipse.sirius.properties.TextDescription;
 import org.eclipse.sirius.properties.ViewExtensionDescription;
-import org.eclipse.sirius.properties.WidgetDescription;
 import org.eclipse.sirius.viewpoint.DSemanticDecorator;
 import org.eclipse.sirius.viewpoint.description.Viewpoint;
-import org.eclipse.sirius.viewpoint.description.tool.InitialOperation;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.views.properties.tabbed.ITabDescriptor;
 import org.eclipse.ui.views.properties.tabbed.ITabDescriptorProvider;
@@ -104,7 +92,7 @@ public class SiriusTabDescriptorProvider implements ITabDescriptorProvider {
             // FIXME We take only the firts one
             if (descriptions.size() > 0) {
                 ViewExtensionDescription viewExtensionDescription = descriptions.get(0);
-                EEFViewDescription eefViewDescription = this.convert(viewExtensionDescription);
+                EEFViewDescription eefViewDescription = new ViewDescriptionConverter(viewExtensionDescription).convert();
 
                 IVariableManager variableManager = new EEFVariableManagerFactory().createVariableManager();
                 variableManager.put(EEFExpressionUtils.SELF, semanticElement);
@@ -127,56 +115,4 @@ public class SiriusTabDescriptorProvider implements ITabDescriptorProvider {
 
         return new ITabDescriptor[0];
     }
-
-    private EEFViewDescription convert(ViewExtensionDescription viewExtensionDescription) {
-        EEFViewDescription eefViewDescription = EefFactory.eINSTANCE.createEEFViewDescription();
-        eefViewDescription.setIdentifier(viewExtensionDescription.getIdentifier());
-        eefViewDescription.setLabelExpression(viewExtensionDescription.getLabelExpression());
-
-        List<GroupDescription> groups = viewExtensionDescription.getGroups();
-        for (GroupDescription groupDescription : groups) {
-            EEFGroupDescription eefGroupDescription = EefFactory.eINSTANCE.createEEFGroupDescription();
-            eefViewDescription.getGroups().add(eefGroupDescription);
-
-            eefGroupDescription.setIdentifier(groupDescription.getIdentifier());
-            eefGroupDescription.setSemanticCandidateExpression(groupDescription.getSemanticCandidateExpression());
-            eefGroupDescription.setLabelExpression(groupDescription.getLabelExpression());
-
-            EEFContainerDescription eefContainerDescription = EefFactory.eINSTANCE.createEEFContainerDescription();
-            eefGroupDescription.setContainer(eefContainerDescription);
-
-            ContainerDescription containerDescription = groupDescription.getContainer();
-            List<WidgetDescription> widgets = containerDescription.getWidgets();
-            for (WidgetDescription widgetDescription : widgets) {
-                if (widgetDescription instanceof TextDescription) {
-                    TextDescription textDescription = (TextDescription) widgetDescription;
-
-                    EEFTextDescription eefTextDescription = EefFactory.eINSTANCE.createEEFTextDescription();
-                    eefContainerDescription.getWidgets().add(eefTextDescription);
-
-                    eefTextDescription.setIdentifier(textDescription.getIdentifier());
-                    eefTextDescription.setLabelExpression(textDescription.getLabelExpression());
-                    eefTextDescription.setValueExpression(textDescription.getValueExpression());
-
-                    InitialOperation initialOperation = textDescription.getInitialOperation();
-                    eefTextDescription.setEditExpression("aql:self.run(selection, '" + EcoreUtil.getURI(initialOperation).toString() + "')");
-                }
-            }
-        }
-
-        List<PageDescription> pages = viewExtensionDescription.getPages();
-        for (PageDescription pageDescription : pages) {
-            EEFPageDescription eefPageDescription = EefFactory.eINSTANCE.createEEFPageDescription();
-            eefViewDescription.getPages().add(eefPageDescription);
-
-            eefPageDescription.setIdentifier(pageDescription.getIdentifier());
-            eefPageDescription.setLabelExpression(pageDescription.getLabelExpression());
-
-            // FIXME OMG THIS IS HORRIBLE!!!ELEVEN!!!
-            eefPageDescription.getGroups().add(eefViewDescription.getGroups().get(0));
-        }
-
-        return eefViewDescription;
-    }
-
 }
---
+++ incubation/org.eclipse.sirius.ui.properties/src/org/eclipse/sirius/ui/properties/internal/ViewDescriptionConverter.java
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.ui.properties.internal;
+
+import java.util.List;
+
+import org.eclipse.eef.EEFContainerDescription;
+import org.eclipse.eef.EEFGroupDescription;
+import org.eclipse.eef.EEFPageDescription;
+import org.eclipse.eef.EEFTextDescription;
+import org.eclipse.eef.EEFViewDescription;
+import org.eclipse.eef.EefFactory;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.sirius.properties.ContainerDescription;
+import org.eclipse.sirius.properties.GroupDescription;
+import org.eclipse.sirius.properties.PageDescription;
+import org.eclipse.sirius.properties.TextDescription;
+import org.eclipse.sirius.properties.ViewExtensionDescription;
+import org.eclipse.sirius.properties.WidgetDescription;
+import org.eclipse.sirius.viewpoint.description.tool.InitialOperation;
+
+/**
+ * Interprets the high-level property views description defined in a Sirius VSM
+ * into a lower-level EEFViewDescription suitable for the EEF runtime.
+ */
+public class ViewDescriptionConverter {
+    private final ViewExtensionDescription description;
+    
+    public ViewDescriptionConverter(ViewExtensionDescription description) {
+        this.description = description;
+    }
+
+    public EEFViewDescription convert() {
+        return convert(description);
+    }
+
+    private EEFViewDescription convert(ViewExtensionDescription viewExtensionDescription) {
+        EEFViewDescription eefViewDescription = EefFactory.eINSTANCE.createEEFViewDescription();
+        eefViewDescription.setIdentifier(viewExtensionDescription.getIdentifier());
+        eefViewDescription.setLabelExpression(viewExtensionDescription.getLabelExpression());
+
+        List<GroupDescription> groups = viewExtensionDescription.getGroups();
+        for (GroupDescription groupDescription : groups) {
+            EEFGroupDescription eefGroupDescription = EefFactory.eINSTANCE.createEEFGroupDescription();
+            eefViewDescription.getGroups().add(eefGroupDescription);
+
+            eefGroupDescription.setIdentifier(groupDescription.getIdentifier());
+            eefGroupDescription.setSemanticCandidateExpression(groupDescription.getSemanticCandidateExpression());
+            eefGroupDescription.setLabelExpression(groupDescription.getLabelExpression());
+
+            EEFContainerDescription eefContainerDescription = EefFactory.eINSTANCE.createEEFContainerDescription();
+            eefGroupDescription.setContainer(eefContainerDescription);
+
+            ContainerDescription containerDescription = groupDescription.getContainer();
+            List<WidgetDescription> widgets = containerDescription.getWidgets();
+            for (WidgetDescription widgetDescription : widgets) {
+                if (widgetDescription instanceof TextDescription) {
+                    TextDescription textDescription = (TextDescription) widgetDescription;
+
+                    EEFTextDescription eefTextDescription = EefFactory.eINSTANCE.createEEFTextDescription();
+                    eefContainerDescription.getWidgets().add(eefTextDescription);
+
+                    eefTextDescription.setIdentifier(textDescription.getIdentifier());
+                    eefTextDescription.setLabelExpression(textDescription.getLabelExpression());
+                    eefTextDescription.setValueExpression(textDescription.getValueExpression());
+
+                    InitialOperation initialOperation = textDescription.getInitialOperation();
+                    eefTextDescription.setEditExpression("aql:self.run(selection, '" + EcoreUtil.getURI(initialOperation).toString() + "')");
+                }
+            }
+        }
+
+        List<PageDescription> pages = viewExtensionDescription.getPages();
+        for (PageDescription pageDescription : pages) {
+            EEFPageDescription eefPageDescription = EefFactory.eINSTANCE.createEEFPageDescription();
+            eefViewDescription.getPages().add(eefPageDescription);
+
+            eefPageDescription.setIdentifier(pageDescription.getIdentifier());
+            eefPageDescription.setLabelExpression(pageDescription.getLabelExpression());
+
+            // FIXME OMG THIS IS HORRIBLE!!!ELEVEN!!!
+            eefPageDescription.getGroups().add(eefViewDescription.getGroups().get(0));
+        }
+
+        return eefViewDescription;
+    }
+}
