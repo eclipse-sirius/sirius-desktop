01e69425dfef92bddf2b47813fa12558c9b46812
Laurent Fasani <laurent.fasani@obeo.fr>
2016-01-08 11:57:45 +0100

    [483574] Add release notes and documentation
    TypedVariableValueDialog has been moved to internal.
    
    Bug: 483574
    Change-Id: If6e71e9eb845cf3498b66004a8ff43e74b093c8b
    Signed-off-by: Laurent Fasani <laurent.fasani@obeo.fr>

--- plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/tools/internal/selection/TypedVariableValueDialog.java
+++ plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/tools/internal/selection/TypedVariableValueDialog.java
@@ -8,7 +8,7 @@
  * Contributors:
  *    Obeo - initial API and implementation
  *******************************************************************************/
-package org.eclipse.sirius.ui.tools.api.selection;
+package org.eclipse.sirius.ui.tools.internal.selection;
 
 import java.util.ArrayList;
 import java.util.List;
