ee02b956c2a16987f1003a0e5a2ec02be516bc6c
Pierre-Charles David <pierre-charles.david@obeo.fr>
2016-01-18 16:47:22 +0100

    [482993] Add standard coercions to IEvaluationResult
    Bug: 482993
    Change-Id: I197ba71ece4ad89b4ecded0a398345bffa22ab47
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- incubation/org.eclipse.sirius.common.interpreter/src/org/eclipse/sirius/common/interpreter/api/EvaluationResult.java
+++ incubation/org.eclipse.sirius.common.interpreter/src/org/eclipse/sirius/common/interpreter/api/EvaluationResult.java
@@ -10,8 +10,16 @@
  *******************************************************************************/
 package org.eclipse.sirius.common.interpreter.api;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.ecore.EObject;
+
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 
 /**
  * Default implementaiton of {@link IEvaluationResult}, suitable for most cases.
@@ -64,9 +72,33 @@ public class EvaluationResult implements IEvaluationResult {
     public Diagnostic getDiagnostic() {
         return status;
     }
-    
+
     @Override
     public boolean success() {
         return Diagnostic.OK == status.getSeverity() && rawValue != null;
+    }
+
+    @Override
+    public Collection<EObject> asEObjects() {
+        final Collection<EObject> result;
+        if (rawValue instanceof Collection<?>) {
+            result = Lists.newArrayList(Iterables.filter((Collection<?>) rawValue, EObject.class));
+        } else if (rawValue instanceof EObject) {
+            result = Collections.singleton((EObject) rawValue);
+        } else if (rawValue != null && rawValue.getClass().isArray()) {
+            result = Lists.newArrayList(Iterables.filter(Lists.newArrayList((Object[]) rawValue), EObject.class));
+        } else {
+            result = Collections.emptySet();
+        }
+        return result;
+    }
+
+    @Override
+    public String asString() {
+        if (rawValue != null) {
+            return String.valueOf(rawValue);
+        } else {
+            return "";
+        }
     }
 }
--- incubation/org.eclipse.sirius.common.interpreter/src/org/eclipse/sirius/common/interpreter/api/IEvaluationResult.java
+++ incubation/org.eclipse.sirius.common.interpreter/src/org/eclipse/sirius/common/interpreter/api/IEvaluationResult.java
@@ -10,7 +10,10 @@
  *******************************************************************************/
 package org.eclipse.sirius.common.interpreter.api;
 
+import java.util.Collection;
+
 import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.ecore.EObject;
 
 /**
  * The evaluation result.
@@ -24,6 +27,22 @@ public interface IEvaluationResult {
      * @return The value returned by the expression
      */
     Object getValue();
+
+    /**
+     * Coerces the value as a collection of EObject if possible.
+     * 
+     * @return a collection of {@link EObject}, which will be empty if the raw
+     *         value could not be coerced.
+     */
+    Collection<EObject> asEObjects();
+
+    /**
+     * Coerces the value as a string.
+     * 
+     * @return a string representation of the value, which will be empty if the
+     *         raw value could not be coerced meaningfully.
+     */
+    String asString();
 
     /**
      * The diagnostic of the evaluation.
