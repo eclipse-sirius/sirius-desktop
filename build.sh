01f8f387b443ff89a75d47e9d90a128972586c4f
cbrun <cedric.brun@obeo.fr>
2016-01-29 15:52:49 +0100

    [478878] Merge the code and Test builds
    Include the tests and their configuration into the base Maven build.
    Remove the "test specific" targets platforms and update the "build"
    target platform to include UML2.
    
    The test specific build org.eclipse.sirius.tests.parent is not removed
    but is made empty in order to be able to have Jenkins/hudson jobs which
    can indiferently launch one layout or the other.
    
    Bug: 478878
    Change-Id: I9472ab5ee6a5d631cf054d5f809609d7567bec3d
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+readonly PLATFORM=${1:-mars}
+readonly GOAL=${2:-package}
+readonly TESTS_SUITES=${3:-junit,swtbot-sequence,swtbot}
+readonly BUILD_POM="packaging/org.eclipse.sirius.parent/pom.xml"
+readonly USE_P2_MIRRORS="true"
+
+SWT_GTK3=0 mvn -Dplatform-version-name="$PLATFORM" -Declipse.p2.mirrors="$USE_P2_MIRRORS" -f "$BUILD_POM" -P full,headless,incubation,"$TESTS_SUITES" clean "$GOAL"
