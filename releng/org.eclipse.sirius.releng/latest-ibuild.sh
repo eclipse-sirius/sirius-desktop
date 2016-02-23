608c580b53cc795bd62b9d472b3dcbb7a4ce7201
Pierre-Charles David <pierre-charles.david@obeo.fr>
2016-02-23 11:11:35 +0100

    [build] Improve the canary build
    - Use a fixed URL to get the latest platform nightly instead of relying
      on a separate latest-ibuild.sh script (which is removed) and some sed
      magic in the Hudson job configuration (removed too).
    - Fix the version of SWTBot we target to 2.1.1, as the most recent
      versions have a lot of API breaks compared to the one we use.
    
    Change-Id: Ie555fb27af412998885d582e9e7565d84b442f70
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- releng/org.eclipse.sirius.releng/latest-ibuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-BASE="http://download.eclipse.org/eclipse/updates/4.6-I-builds"
-TEMPFILE=$(mktemp --suffix=.jar) || exit
-curl -s "$BASE/compositeContent.jar" > "$TEMPFILE"
-LATEST=$(unzip -p "$TEMPFILE" compositeContent.xml | grep "<child location='I" | sort -r | head -1 | sed -e "s/\s*<child location='//" -e "s|'/>\s*||")
-echo "$BASE/$LATEST"
-rm "$TEMPFILE"
