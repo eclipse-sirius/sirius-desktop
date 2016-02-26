058c6dd1776c10a7585ea61fe62191932b1accd5
Pierre-Charles David <pierre-charles.david@obeo.fr>
2016-02-26 15:50:14 +0100

    [cleanup] Remove duplicated test in SWTbot suite
    Change-Id: Id66af41318ed13f6ee2a36add947e7bafdeabe91
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.sirius.tests.swtbot/src/org/eclipse/sirius/tests/swtbot/suite/AllTestSuite.java
+++ plugins/org.eclipse.sirius.tests.swtbot/src/org/eclipse/sirius/tests/swtbot/suite/AllTestSuite.java
@@ -268,7 +268,6 @@ public class AllTestSuite extends TestCase {
         suite.addTestSuite(SpecificLayoutBendpointsOnReopeningTest.class);
         suite.addTestSuite(TreeFoldingTest.class);
         suite.addTestSuite(DoubleClickToolNavigationOperationTest.class);
-        suite.addTestSuite(DoubleClickToolNavigationOperationTest.class);
         suite.addTestSuite(BackgroundColorFigureUpdateTests.class);
         suite.addTestSuite(BackgroundStyleUpdateTest.class);
         suite.addTestSuite(BorderSizeComputationExpressionTest.class);
