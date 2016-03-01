d0ae6abdf7d24a8190259c167c97a5dbdfdf79d3
Pierre-Charles David <pierre-charles.david@obeo.fr>
2016-03-01 11:56:22 +0100

    [cleanup] Remove useless internal class FeedbackLayer
    Change-Id: Ida5247c2bdb310601025192a2e5fbacf877ec7f9
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/graphical/edit/part/DDiagramRootEditPart.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/graphical/edit/part/DDiagramRootEditPart.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2011 THALES GLOBAL SERVICES.
+ * Copyright (c) 2007, 2016 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,9 +12,12 @@ package org.eclipse.sirius.diagram.ui.tools.internal.graphical.edit.part;
 
 import org.eclipse.draw2d.FreeformLayer;
 import org.eclipse.draw2d.FreeformLayeredPane;
+import org.eclipse.draw2d.Layer;
 import org.eclipse.draw2d.LayeredPane;
 import org.eclipse.draw2d.ScalableFreeformLayeredPane;
 import org.eclipse.gef.AutoexposeHelper;
+import org.eclipse.gef.LayerConstants;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramRootEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.figures.BorderItemsAwareFreeFormLayer;
 import org.eclipse.gmf.runtime.diagram.ui.render.editparts.RenderedDiagramRootEditPart;
 import org.eclipse.gmf.runtime.notation.MeasurementUnit;
@@ -22,14 +25,14 @@ import org.eclipse.sirius.diagram.ui.tools.internal.figure.DConnectionLayerEx;
 
 /**
  * Installs the tree designer forest router and a scalable feedback layer.
- * 
+ *
  * @author ymortier
  */
 public class DDiagramRootEditPart extends RenderedDiagramRootEditPart {
 
     /**
      * Constructor.
-     * 
+     *
      * @param mu
      *            the measurement unit
      */
@@ -40,40 +43,38 @@ public class DDiagramRootEditPart extends RenderedDiagramRootEditPart {
     /**
      * Installs the
      * {@link org.eclipse.sirius.diagram.ui.tools.internal.routers.DForestRouter}
-     * . {@inheritDoc}
-     * 
-     * @see org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramRootEditPart#createPrintableLayers()
+     * .
+     * <p>
+     * {@inheritDoc}
      */
     @Override
     protected LayeredPane createPrintableLayers() {
         final FreeformLayeredPane layeredPane = new FreeformLayeredPane();
-        layeredPane.add(new BorderItemsAwareFreeFormLayer(), PRIMARY_LAYER);
-        layeredPane.add(new DConnectionLayerEx(), CONNECTION_LAYER);
-        layeredPane.add(new FreeformLayer(), DECORATION_PRINTABLE_LAYER);
+        layeredPane.add(new BorderItemsAwareFreeFormLayer(), LayerConstants.PRIMARY_LAYER);
+        layeredPane.add(new DConnectionLayerEx(), LayerConstants.CONNECTION_LAYER);
+        layeredPane.add(new FreeformLayer(), DiagramRootEditPart.DECORATION_PRINTABLE_LAYER);
         return layeredPane;
     }
 
     /**
      * Overridden to have a SCALED_FEEDBACK_LAYER.
-     * 
+     * <p>
      * {@inheritDoc}
      */
     @Override
     protected ScalableFreeformLayeredPane createScaledLayers() {
         ScalableFreeformLayeredPane layers = super.createScaledLayers();
-        layers.add(new org.eclipse.sirius.diagram.ui.tools.internal.figure.FeedbackLayer(), SCALED_FEEDBACK_LAYER);
+        Layer feedbackLayer = new FreeformLayer();
+        feedbackLayer.setEnabled(false);
+        layers.add(feedbackLayer, LayerConstants.SCALED_FEEDBACK_LAYER);
         return layers;
     }
 
-    /**
-     * 
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.gef.editparts.FreeformGraphicalRootEditPart#getAdapter(java.lang.Class)
-     */
+    @Override
     public Object getAdapter(@SuppressWarnings("rawtypes") Class key) {
-        if (key == AutoexposeHelper.class)
+        if (key == AutoexposeHelper.class) {
             return new SiriusScroller(this, SiriusScroller.VIEWPOINT_SCROLLER_INSETS);
+        }
         return super.getAdapter(key);
     }
 
