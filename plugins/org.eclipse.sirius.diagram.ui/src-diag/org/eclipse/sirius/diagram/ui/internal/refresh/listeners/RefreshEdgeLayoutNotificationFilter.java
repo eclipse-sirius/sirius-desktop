3bbe6132bb525e572a9064fa78de2a35f85f05e2
Laurent Redor <laurent.redor@obeo.fr>
2016-03-10 11:52:56 +0100

    [489183] Fix the detected regressions
    * Move all code concerning EdgeLayoutUpdaterModelChangeTrigger in this
    class (instead of in the associated notification filter class
    RefreshEdgeLayoutNotificationFilter).
    * Consider the figure in case of one notification
    * Keep all notifications (not only REFRESH_FEATURES) for the
    EdgeLayoutUpdaterModelChangeTrigger to be able to detect if other
    notifications are consequences of the first one.
    
    Bug: 489183
    Change-Id: I763254bf15a9b13b80c8b18cbf5666b1cf8a004e
    Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/internal/refresh/listeners/RefreshEdgeLayoutNotificationFilter.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/internal/refresh/listeners/RefreshEdgeLayoutNotificationFilter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************        
- * Copyright (c) 2014 THALES GLOBAL SERVICES.       
+ * Copyright (c) 2014, 2016 THALES GLOBAL SERVICES.       
  * All rights reserved. This program and the accompanying materials     
  * are made available under the terms of the Eclipse Public License v1.0        
  * which accompanies this distribution, and is available at     
@@ -10,40 +10,17 @@
  *******************************************************************************/
 package org.eclipse.sirius.diagram.ui.internal.refresh.listeners;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-
 import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.gmf.runtime.notation.NotationPackage;
 import org.eclipse.sirius.business.api.dialect.DRepresentationNotificationFilter;
 import org.eclipse.sirius.diagram.DDiagram;
-import org.eclipse.sirius.diagram.DiagramPackage;
-import org.eclipse.sirius.viewpoint.ViewpointPackage;
 
 /**
- * A NotificationFilter to restrict the scope of RefreshEdgeLayoutChangeTrigger.
+ * A NotificationFilter to restrict the scope of RefreshEdgeLayoutChangeTrigger
+ * (all notifications of the current diagram that have a new value).
  * 
  * @author Florian Barbin
  */
 public class RefreshEdgeLayoutNotificationFilter extends DRepresentationNotificationFilter {
-
-    private static final Set<EStructuralFeature> INTERESTING_FEATURES = new HashSet<EStructuralFeature>();
-
-    static {
-        INTERESTING_FEATURES.add(DiagramPackage.Literals.DEDGE__OWNED_STYLE);
-        INTERESTING_FEATURES.add(DiagramPackage.Literals.EDGE_STYLE__CENTERED);
-        INTERESTING_FEATURES.add(NotationPackage.Literals.ROUTING_STYLE__ROUTING);
-        INTERESTING_FEATURES.add(NotationPackage.Literals.DIAGRAM__PERSISTED_EDGES);
-    }
-
-    private static final Set<EStructuralFeature> INDIRECT_INTERESTING_FEATURES = new HashSet<EStructuralFeature>();
-
-    static {
-        INDIRECT_INTERESTING_FEATURES.add(ViewpointPackage.Literals.CUSTOMIZABLE__CUSTOM_FEATURES);
-        INDIRECT_INTERESTING_FEATURES.add(DiagramPackage.Literals.EDGE_STYLE__ROUTING_STYLE);
-    }
 
     public RefreshEdgeLayoutNotificationFilter(DDiagram dDiagram) {
         super(dDiagram);
@@ -51,35 +28,6 @@ public class RefreshEdgeLayoutNotificationFilter extends DRepresentationNotifica
 
     @Override
     public boolean matches(Notification notification) {
-        boolean matches = super.matches(notification) && notification.getNewValue() != null && INTERESTING_FEATURES.contains(notification.getFeature());
-        return matches;
+        return super.matches(notification) && notification.getNewValue() != null;
     }
-
-    /**
-     * Test whether the other notifications concern the given one. For instance,
-     * in case of a manual modification of the GMF routing style, we also update
-     * the Sirius style and we add the routing style within the custom features.
-     * This method aims to check whether we are in the case of an individual
-     * modification or a global one.
-     * 
-     * @param notification
-     *            the notification for which we are notified.
-     * @param notifications
-     *            the whole notification list.
-     * @return true if the notifications list contains only notifications
-     *         induced by the first one.
-     */
-    public boolean otherNotificationsAreIndirectlyConcerned(Notification notification, Collection<Notification> notifications) {
-        boolean otherNotificationsAreIndirectlyConcerned = false;
-        if (NotationPackage.Literals.ROUTING_STYLE__ROUTING == notification.getFeature() || DiagramPackage.Literals.EDGE_STYLE__CENTERED == notification.getFeature()) {
-            for (Notification currentNotification : notifications) {
-                if (currentNotification != notification && !INDIRECT_INTERESTING_FEATURES.contains(currentNotification.getFeature())) {
-                    otherNotificationsAreIndirectlyConcerned = true;
-                    break;
-                }
-            }
-        }
-        return otherNotificationsAreIndirectlyConcerned;
-    }
-
 }
