b38268bcbf4c1ddb41a168255d7593f1c88455ca
Florian Barbin <florian.barbin@obeo.fr>
2016-03-29 14:50:50 +0200

    [485832] Fixes SWTBot test
    Bug: 485832
    Change-Id: I5ca2b88271b137897c9604fd4de2bc60c7b0921f
    Signed-off-by: Florian Barbin <florian.barbin@obeo.fr>

--- plugins/org.eclipse.sirius.tests.swtbot/src/org/eclipse/sirius/tests/swtbot/SecurityExceptionPopupTest.java
+++ plugins/org.eclipse.sirius.tests.swtbot/src/org/eclipse/sirius/tests/swtbot/SecurityExceptionPopupTest.java
@@ -14,11 +14,9 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.sirius.tests.SiriusTestsPlugin;
 import org.eclipse.sirius.tests.swtbot.support.api.AbstractSiriusSwtBotGefTestCase;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swtbot.eclipse.finder.matchers.WidgetMatcherFactory;
-import org.eclipse.swtbot.eclipse.finder.waits.Conditions;
-import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
-import org.eclipse.swtbot.swt.finder.waits.WaitForObjectCondition;
+import org.eclipse.sirius.tests.swtbot.support.internal.conditions.ShellChangedCondition;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.eclipse.swtbot.swt.finder.widgets.TimeoutException;
 
 /**
  * This test checks that the securityException opens a popup only in the case of
@@ -54,19 +52,19 @@ public class SecurityExceptionPopupTest extends AbstractSiriusSwtBotGefTestCase 
         } else {
             exception = new SecurityException();
         }
-
+        ShellChangedCondition shellChangedCondition = new ShellChangedCondition(bot.activeShell());
         SiriusTestsPlugin.getDefault().getLog().log(new Status(IStatus.ERROR, SiriusTestsPlugin.PLUGIN_ID, "exception occurs", exception));
 
         // Step 2 - Check whether the dialog is opened
         // or not (if there is no message in the exception, the dialog
         // should not be opened).
 
-        Shell shell = null;
+        SWTBotShell shell = null;
         try {
-            WaitForObjectCondition<Shell> waitForShell = Conditions.waitForShell(WidgetMatcherFactory.<Shell> withText("Permission Issue"));
-            bot.waitUntilWidgetAppears(waitForShell);
-            shell = waitForShell.getAllMatches().get(0);
-        } catch (WidgetNotFoundException e) {
+            bot.waitUntil(shellChangedCondition);
+            shell = bot.activeShell();
+            assertEquals("Permission Issue", shell.getText());
+        } catch (TimeoutException e) {
             // We throw the exception only in the case where the
             // dialog is expected (a SecurityException with a
             // message).
@@ -76,8 +74,7 @@ public class SecurityExceptionPopupTest extends AbstractSiriusSwtBotGefTestCase 
         }
         errors.clear();
         if (shell != null) {
-            // We close the shell and clear the error log.
-            bot.closeAllShells();
+            shell.close();
             // If the exception was logged without message, we
             // shouldn't have found a shell.
             if (!withMessage) {
