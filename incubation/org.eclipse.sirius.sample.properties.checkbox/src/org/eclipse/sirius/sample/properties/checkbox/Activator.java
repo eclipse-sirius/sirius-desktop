d3a2cc35d2d2bff440f1e3e5a0e8fb1553a89f42
Pierre-Charles David <pierre-charles.david@obeo.fr>
2016-04-01 10:54:39 +0200

    [490594] Improve packaging of the properties view support
    - Replace the monolithic oes.incubation.properties feature with two
      finer-grained ones, isolating the specification environment support
      from the runtime.
    
      Despite the incubation status of the work, the new features' names do
      not include an "incubation" segment to avoid breaking updates when
      this becomes ready for prime time.
    
    - Fix the various plug-ins packaging: add missing files and copyright
      information, update version to 4.0.0 to match the rest of Sirius (even
      though this particular component is new and still in incubation), make
      sure build.properties & co. are correct, etc.
    
    - Also fix the samples projects (which had inconsistent Java package
      names and mostly missing or incorrect metadata due to quick
      copy/pasting) and add all of them to the build via a new feature.
    
    Bug: 490594
    Change-Id: I80b91ac4322370a2320376fb81c466b02a8cd77d
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

---
+++ incubation/org.eclipse.sirius.sample.properties.checkbox/src/org/eclipse/sirius/sample/properties/checkbox/Activator.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.sample.properties.checkbox;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.sirius.business.api.componentization.ViewpointRegistry;
+import org.eclipse.sirius.viewpoint.description.Viewpoint;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+    // The plug-in ID
+    public static final String PLUGIN_ID = "org.eclipse.sirius.sample.properties.checkbox";
+
+    // The shared instance
+    private static Activator plugin;
+
+    private static Set<Viewpoint> viewpoints;
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        Activator.plugin = this;
+        Activator.viewpoints = new HashSet<Viewpoint>();
+        Activator.viewpoints.addAll(ViewpointRegistry.getInstance().registerFromPlugin(Activator.PLUGIN_ID + "/description/project.odesign"));
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        Activator.plugin = null;
+        if (Activator.viewpoints != null) {
+            for (final Viewpoint viewpoint : Activator.viewpoints) {
+                ViewpointRegistry.getInstance().disposeFromPlugin(viewpoint);
+            }
+            Activator.viewpoints.clear();
+            Activator.viewpoints = null;
+        }
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     *
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return Activator.plugin;
+    }
+}
