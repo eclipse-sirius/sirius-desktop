ad8c2a23533e04f6744495453307f36028b8470d
Laurent Redor <laurent.redor@obeo.fr>
2016-04-18 16:15:20 +0200

    [491913] Copy original classes from GMF
    Preparation commit: Duplicate original GMF classes needed for this
    bugzilla. This commit allows to have an initial state of these classes
    before modifying them for this bugzilla.
    
    Bug: 491913
    Change-Id: I7a255313c42bab04dbd13f3f9ed0e1f2560917ec
    Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>

---
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/render/SiriusDiagramSVGGenerator.java
@@ -0,0 +1,185 @@
+/******************************************************************************
+ * Copyright (c) 2004, 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    IBM Corporation - initial API and implementation 
+ ****************************************************************************/
+
+package org.eclipse.sirius.diagram.ui.tools.internal.render;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.draw2d.Graphics;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gmf.runtime.common.core.util.Log;
+import org.eclipse.gmf.runtime.common.core.util.Trace;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.l10n.SharedImages;
+import org.eclipse.gmf.runtime.diagram.ui.render.clipboard.DiagramGenerator;
+import org.eclipse.gmf.runtime.diagram.ui.render.internal.DiagramUIRenderDebugOptions;
+import org.eclipse.gmf.runtime.diagram.ui.render.internal.DiagramUIRenderPlugin;
+import org.eclipse.gmf.runtime.draw2d.ui.render.RenderedImage;
+import org.eclipse.gmf.runtime.draw2d.ui.render.awt.internal.image.ImageConverter;
+import org.eclipse.gmf.runtime.draw2d.ui.render.awt.internal.svg.export.GraphicsSVG;
+import org.eclipse.gmf.runtime.draw2d.ui.render.factory.RenderedImageFactory;
+import org.eclipse.gmf.runtime.draw2d.ui.render.internal.RenderedImageDescriptor;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.w3c.dom.Element;
+
+/**
+ * Supports generation of an SVG DOM for a diagram or a subset of editparts on a
+ * diagram.
+ * 
+ * @author jschofie / sshaw
+ */
+// CHECKSTYLE:OFF
+public class SiriusDiagramSVGGenerator extends DiagramGenerator {
+
+    private RenderedImage renderedImage = null;
+
+    private Element svgRoot = null;
+
+    private Rectangle viewBox = null;
+
+    /**
+     * Creates a new instance.
+     * 
+     * @param diagramEditPart
+     *            the diagram editpart
+     */
+    public SiriusDiagramSVGGenerator(DiagramEditPart diagramEditPart) {
+        super(diagramEditPart);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see
+     * org.eclipse.gmf.runtime.diagram.ui.render.clipboard.DiagramGenerator#
+     * setUpGraphics(int, int)
+     */
+    protected Graphics setUpGraphics(int width, int height) {
+        viewBox = new Rectangle(0, 0, width, height);
+        return GraphicsSVG.getInstance(viewBox);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see
+     * org.eclipse.gmf.runtime.diagram.ui.render.clipboard.DiagramGenerator#
+     * getImageDescriptor(org.eclipse.draw2d.Graphics)
+     */
+    protected ImageDescriptor getImageDescriptor(Graphics g) {
+        try {
+            GraphicsSVG svgG = (GraphicsSVG) g;
+            // Get the root element (the svg element)
+            svgRoot = svgG.getRoot();
+
+            ByteArrayOutputStream os = new ByteArrayOutputStream(5000); // 5K
+                                                                        // buffer
+            stream(os);
+            os.close();
+
+            setRenderedImage(RenderedImageFactory.getInstance(os.toByteArray()));
+
+            return RenderedImageDescriptor.createFromRenderedImage(getRenderedImage());
+        } catch (IOException ex) {
+            Log.error(DiagramUIRenderPlugin.getInstance(), IStatus.ERROR, ex.getMessage(), ex);
+        }
+
+        return null;
+    }
+
+    /**
+     * Writes the SVG Model out to a file.
+     * 
+     * @param outputStream
+     *            output stream to store the SVG Model
+     */
+    public void stream(OutputStream outputStream) {
+        try {
+
+            // Define the view box
+            svgRoot.setAttributeNS(null, "viewBox", //$NON-NLS-1$
+                    String.valueOf(viewBox.x) + " " + //$NON-NLS-1$
+                            String.valueOf(viewBox.y) + " " + //$NON-NLS-1$
+                            String.valueOf(viewBox.width) + " " + //$NON-NLS-1$
+                            String.valueOf(viewBox.height));
+
+            // Write the document to the stream
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml"); //$NON-NLS-1$
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8"); //$NON-NLS-1$
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes"); //$NON-NLS-1$
+
+            DOMSource source = new DOMSource(svgRoot);
+            StreamResult result = new StreamResult(outputStream);
+            transformer.transform(source, result);
+        } catch (Exception ex) {
+            Log.error(DiagramUIRenderPlugin.getInstance(), IStatus.ERROR, ex.getMessage(), ex);
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see
+     * org.eclipse.gmf.runtime.diagram.ui.internal.clipboard.DiagramGenerator#
+     * createAWTImageForParts(java.util.List)
+     */
+    public Image createAWTImageForParts(List editparts, org.eclipse.swt.graphics.Rectangle sourceRect) {
+        createSWTImageDescriptorForParts(editparts, sourceRect);
+        if (getRenderedImage() != null) {
+            try {
+                BufferedImage bufImg = (BufferedImage) getRenderedImage().getAdapter(BufferedImage.class);
+                if (bufImg == null)
+                    bufImg = ImageConverter.convert(getRenderedImage().getSWTImage());
+                return bufImg;
+            } catch (Error e) {
+                // log the Error but allow execution to continue
+                Trace.catching(DiagramUIRenderPlugin.getInstance(), DiagramUIRenderDebugOptions.EXCEPTIONS_THROWING, getClass(), "createAWTImageForParts() failed to generate image", //$NON-NLS-1$
+                        e);
+                return ImageConverter.convert(SharedImages.get(SharedImages.IMG_ERROR));
+
+            } catch (Exception ex) {
+                // log the Exception but allow execution to continue
+                Trace.catching(DiagramUIRenderPlugin.getInstance(), DiagramUIRenderDebugOptions.EXCEPTIONS_THROWING, getClass(), "createAWTImageForParts() failed to generate image", //$NON-NLS-1$
+                        ex);
+                return ImageConverter.convert(SharedImages.get(SharedImages.IMG_ERROR));
+            }
+        }
+
+        return ImageConverter.convert(SharedImages.get(SharedImages.IMG_ERROR));
+    }
+
+    /**
+     * @return Returns the rendered image created by previous call to
+     *         createSWTImageDescriptorForParts
+     */
+    public RenderedImage getRenderedImage() {
+        return renderedImage;
+    }
+
+    /**
+     * @param svgImage
+     *            The svgImage to set.
+     */
+    private void setRenderedImage(RenderedImage renderedImage) {
+        this.renderedImage = renderedImage;
+    }
+}
+// CHECKSTYLE:ON
---
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/render/SiriusGraphicsSVG.java
@@ -0,0 +1,180 @@
+/******************************************************************************
+ * Copyright (c) 2004, 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    IBM Corporation - initial API and implementation 
+ ****************************************************************************/
+package org.eclipse.sirius.diagram.ui.tools.internal.render;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import org.apache.batik.dom.svg.SVGDOMImplementation;
+import org.apache.batik.dom.svg.SVGOMDocument;
+import org.apache.batik.dom.util.DOMUtilities;
+import org.apache.batik.svggen.DOMTreeManager;
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.apache.batik.util.SVGConstants;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gmf.runtime.draw2d.ui.render.RenderInfo;
+import org.eclipse.gmf.runtime.draw2d.ui.render.RenderedImage;
+import org.eclipse.gmf.runtime.draw2d.ui.render.awt.internal.graphics.GraphicsToGraphics2DAdaptor;
+import org.eclipse.gmf.runtime.draw2d.ui.render.awt.internal.svg.SVGColorConverter;
+import org.eclipse.gmf.runtime.draw2d.ui.render.awt.internal.svg.SVGImage;
+import org.eclipse.gmf.runtime.draw2d.ui.render.internal.DrawableRenderedImage;
+import org.eclipse.gmf.runtime.draw2d.ui.render.internal.RenderingListener;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * Objects of this class can be used with draw2d to create an SVG DOM.
+ * 
+ * @author jschofie / sshaw
+ */
+// CHECKSTYLE:OFF
+public class SiriusGraphicsSVG extends GraphicsToGraphics2DAdaptor implements DrawableRenderedImage {
+
+    private Document doc;
+
+    /**
+     * Static initializer that will return an instance of
+     * <code>GraphicsSVG</code>
+     * 
+     * @param viewPort
+     *            the <code>Rectangle</code> area that is to be rendered.
+     * @return a new <code>GraphicsSVG</code> object.
+     */
+    public static SiriusGraphicsSVG getInstance(Rectangle viewPort) {
+        SVGGraphics2D svgGraphics;
+
+        // Get the DOM implementation and create the document
+        DOMImplementation impl = SVGDOMImplementation.getDOMImplementation();
+        String svgNameSpace = SVGDOMImplementation.SVG_NAMESPACE_URI;
+        Document doc = impl.createDocument(svgNameSpace, "svg", null); //$NON-NLS-1$
+
+        // Create the SVG Graphics Object
+        svgGraphics = new SVGGraphics2D(doc);
+
+        // Set the precision level to avoid NPEs (issue with Batik 1.5)
+        svgGraphics.getGeneratorContext().setPrecision(3);
+
+        // Set the Width and Height Attributes on the Root Element
+        svgGraphics.setSVGCanvasSize(new Dimension(viewPort.width, viewPort.height));
+
+        return new SiriusGraphicsSVG(svgGraphics, doc, svgNameSpace, viewPort);
+    }
+
+    /**
+     * @return <code>SVGGraphics2D</code> object
+     */
+    public SVGGraphics2D getSVGGraphics2D() {
+        return (SVGGraphics2D) getGraphics2D();
+    }
+
+    /**
+     * @param graphics
+     * @param doc
+     * @param svgNameSpace
+     * @param viewPort
+     */
+    private SiriusGraphicsSVG(SVGGraphics2D graphics, Document doc, String svgNameSpace, Rectangle viewPort) {
+
+        this(graphics, doc, svgNameSpace, new org.eclipse.swt.graphics.Rectangle(viewPort.x, viewPort.y, viewPort.width, viewPort.height));
+    }
+
+    /**
+     * @param graphics
+     * @param doc
+     * @param svgNameSpace
+     * @param viewPort
+     */
+    private SiriusGraphicsSVG(SVGGraphics2D graphics, Document doc, String svgNameSpace, org.eclipse.swt.graphics.Rectangle viewPort) {
+
+        super(graphics, viewPort);
+        this.doc = doc;
+        paintNotCompatibleStringsAsBitmaps = false;
+    }
+
+    /**
+     * Method used to get the SVG DOM from the Graphics
+     * 
+     * @return SVG document
+     */
+    public Document getDocument() {
+        return doc;
+    }
+
+    /**
+     * Method used to get the SVG Root element from the document
+     * 
+     * @return DOM Root element
+     */
+    public Element getRoot() {
+        return getSVGGraphics2D().getRoot();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.gmf.runtime.draw2d.ui.render.awt.internal.graphics.
+     * GraphicsToGraphics2DAdaptor#drawRenderedImage(org.eclipse.gmf.runtime.
+     * draw2d.ui.render.RenderedImage, org.eclipse.draw2d.geometry.Rectangle,
+     * org.eclipse.gmf.runtime.draw2d.ui.render.RenderingListener)
+     */
+    @Override
+    public RenderedImage drawRenderedImage(RenderedImage srcImage, Rectangle rect, RenderingListener listener) {
+
+        // Check for a change in the state
+        checkState();
+
+        // Get the Tree Manager
+        DOMTreeManager treeManager = getSVGGraphics2D().getDOMTreeManager();
+
+        Point trans = getTranslationOffset();
+        // Get the Root element of the SVG document to export
+        if (srcImage instanceof SVGImage) {
+            Document document = ((SVGImage) srcImage).getDocument();
+
+            DOMImplementation impl = SVGDOMImplementation.getDOMImplementation();
+            document = DOMUtilities.deepCloneDocument(document, impl);
+
+            if (document instanceof SVGOMDocument) {
+                RenderInfo info = srcImage.getRenderInfo();
+                if (info != null && info.getBackgroundColor() != null && info.getForegroundColor() != null) {
+                    SVGColorConverter.getInstance().replaceDocumentColors((SVGOMDocument) document,
+                            new Color(info.getBackgroundColor().red, info.getBackgroundColor().green, info.getBackgroundColor().blue),
+                            new Color(info.getForegroundColor().red, info.getForegroundColor().green, info.getForegroundColor().blue));
+
+                }
+            }
+            Element root = document.getDocumentElement();
+
+            // Create a "deep" copy of the document
+            Element toAppend = (Element) doc.importNode(root, true);
+
+            // Modify the X Attribute
+            toAppend.setAttributeNS(null, SVGConstants.SVG_X_ATTRIBUTE, String.valueOf(rect.x + trans.x));
+
+            // Modify the Y Attribute
+            toAppend.setAttributeNS(null, SVGConstants.SVG_Y_ATTRIBUTE, String.valueOf(rect.y + trans.y));
+
+            // Modify the Width Attribute
+            toAppend.setAttributeNS(null, SVGConstants.SVG_WIDTH_ATTRIBUTE, String.valueOf(rect.width));
+
+            // Modify the Height Attribute
+            toAppend.setAttributeNS(null, SVGConstants.SVG_HEIGHT_ATTRIBUTE, String.valueOf(rect.height));
+
+            treeManager.appendGroup(toAppend, null);
+            return srcImage;
+        } else {
+            return super.drawRenderedImage(srcImage, rect, listener);
+        }
+    }
+
+}
+// CHECKSTYLE:ON
