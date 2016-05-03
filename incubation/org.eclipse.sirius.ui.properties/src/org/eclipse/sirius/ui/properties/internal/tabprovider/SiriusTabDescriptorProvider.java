eclipse-sirius/sirius-desktop

commit 50ced5e59c1a74b54fb7b03b5f4c2f43a275ffbe

[482528] Use the session's ModelAccessor to test for domain classes
Bug: 482528
Change-Id: I4ccba7ad4f57e241494a7abb10c0b1b86c1f8a48
Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

incubation/org.eclipse.sirius.ui.properties/src/org/eclipse/sirius/ui/properties/internal/tabprovider/SiriusTabDescriptorProvider.java
@@ -20,6 +20,7 @@ import org.eclipse.eef.core.api.EEFExpressionUtils;
 import org.eclipse.eef.core.api.EEFPage;
 import org.eclipse.eef.core.api.EEFView;
 import org.eclipse.eef.core.api.EEFViewFactory;
+import org.eclipse.eef.core.api.IEEFDomainClassTester;
 import org.eclipse.eef.ide.ui.properties.api.EEFTabDescriptor;
 import org.eclipse.eef.properties.ui.api.IEEFTabDescriptor;
 import org.eclipse.eef.properties.ui.api.IEEFTabDescriptorProvider;
@@ -137,7 +138,12 @@ public class SiriusTabDescriptorProvider implements IEEFTabDescriptorProvider {
         variableManager.put(EEFExpressionUtils.SELF, input.getSemanticElement());
         variableManager.put(EEFExpressionUtils.INPUT, input);
         TransactionalEditingDomainContextAdapter eca = new TransactionalEditingDomainContextAdapter(session.getTransactionalEditingDomain());
-        EEFView eefView = new EEFViewFactory().createEEFView(viewDescription, variableManager, new SiriusInterpreter(session), eca, input);
+        EEFView eefView = new EEFViewFactory().createEEFView(viewDescription, variableManager, new SiriusInterpreter(session), eca, new IEEFDomainClassTester() {
+            @Override
+            public boolean eInstanceOf(EObject eObject, String domainClass) {
+                return session.getModelAccessor().eInstanceOf(eObject, domainClass);
+            }
+        }, input);
         return eefView;
     }
 

commit f24f997e9d57899ba09c40a76b804559d0bd9b22

[482528] Let the EEF runtime use its default DomainClass tester for new
This reverts commit 50ced5e59c1a74b54fb7b03b5f4c2f43a275ffbe which broke
the whole feature.

Bug: 482528
Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

incubation/org.eclipse.sirius.ui.properties/src/org/eclipse/sirius/ui/properties/internal/tabprovider/SiriusTabDescriptorProvider.java
@@ -20,7 +20,6 @@ import org.eclipse.eef.core.api.EEFExpressionUtils;
 import org.eclipse.eef.core.api.EEFPage;
 import org.eclipse.eef.core.api.EEFView;
 import org.eclipse.eef.core.api.EEFViewFactory;
-import org.eclipse.eef.core.api.IEEFDomainClassTester;
 import org.eclipse.eef.ide.ui.properties.api.EEFTabDescriptor;
 import org.eclipse.eef.properties.ui.api.IEEFTabDescriptor;
 import org.eclipse.eef.properties.ui.api.IEEFTabDescriptorProvider;
@@ -138,12 +137,7 @@ public class SiriusTabDescriptorProvider implements IEEFTabDescriptorProvider {
         variableManager.put(EEFExpressionUtils.SELF, input.getSemanticElement());
         variableManager.put(EEFExpressionUtils.INPUT, input);
         TransactionalEditingDomainContextAdapter eca = new TransactionalEditingDomainContextAdapter(session.getTransactionalEditingDomain());
-        EEFView eefView = new EEFViewFactory().createEEFView(viewDescription, variableManager, new SiriusInterpreter(session), eca, new IEEFDomainClassTester() {
-            @Override
-            public boolean eInstanceOf(EObject eObject, String domainClass) {
-                return session.getModelAccessor().eInstanceOf(eObject, domainClass);
-            }
-        }, input);
+        EEFView eefView = new EEFViewFactory().createEEFView(viewDescription, variableManager, new SiriusInterpreter(session), eca, input);
         return eefView;
     }
 