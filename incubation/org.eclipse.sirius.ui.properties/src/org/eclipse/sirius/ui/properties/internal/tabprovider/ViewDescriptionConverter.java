eclipse-sirius/sirius-desktop

commit e5e1a5b15c0818dde9d619aa46f54b3aa218d9b6

[482528] Rename LabelDescription.bodyExpression into valueExpression
bug: 482528
Change-Id: I78179e9f7e1f17f091309ee21a946edfd497e767
Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

incubation/org.eclipse.sirius.ui.properties/src/org/eclipse/sirius/ui/properties/internal/tabprovider/ViewDescriptionConverter.java
@@ -735,7 +735,7 @@ public class ViewDescriptionConverter {
         EEFLabelDescription eefLabelDescription = EefFactory.eINSTANCE.createEEFLabelDescription();
 
         eefLabelDescription.setIdentifier(labelDescription.getIdentifier());
-        eefLabelDescription.setBodyExpression(labelDescription.getBodyExpression());
+        eefLabelDescription.setLabelExpression(labelDescription.getLabelExpression());
         LabelWidgetStyle bodyStyle = labelDescription.getStyle();
         if (bodyStyle != null) {
             eefLabelDescription.setStyle((EEFLabelStyle) createEEFWidgetStyle(bodyStyle));

incubation/org.eclipse.sirius.editor.properties/plugin.xml
@@ -376,9 +376,9 @@
         <input type="org.eclipse.sirius.properties.CheckboxDescription"/>
       </propertySection>
   	  <propertySection
-               class="org.eclipse.sirius.properties.editor.properties.sections.properties.labeldescription.LabelDescriptionBodyExpressionPropertySection"
-               id="properties.section.labelDescription.BodyExpression"
-               filter="org.eclipse.sirius.properties.editor.properties.filters.properties.labeldescription.LabelDescriptionBodyExpressionFilter"
+               class="org.eclipse.sirius.properties.editor.properties.sections.properties.labeldescription.LabelDescriptionValueExpressionPropertySection"
+               id="properties.section.labelDescription.ValueExpression"
+               filter="org.eclipse.sirius.properties.editor.properties.filters.properties.labeldescription.LabelDescriptionValueExpressionFilter"
                 tab="viewpoint.tab.general">
         <input type="org.eclipse.sirius.properties.LabelDescription"/>
       </propertySection>

incubation/org.eclipse.sirius.editor.properties/src-gen/org/eclipse/sirius/properties/editor/properties/filters/properties/labeldescription/LabelDescriptionValueExpressionFilter.java
@@ -18,16 +18,16 @@ import org.eclipse.sirius.properties.PropertiesPackage;
 // End of user code specific imports
 
 /**
- * A filter for the bodyExpression property section.
+ * A filter for the valueExpression property section.
  */
-public class LabelDescriptionBodyExpressionFilter extends ViewpointPropertyFilter {
+public class LabelDescriptionValueExpressionFilter extends ViewpointPropertyFilter {
 
     /**
      * {@inheritDoc}
      */
     @Override
     protected EStructuralFeature getFeature() {
-        return PropertiesPackage.eINSTANCE.getLabelDescription_BodyExpression();
+        return PropertiesPackage.eINSTANCE.getLabelDescription_ValueExpression();
     }
 
     /**

incubation/org.eclipse.sirius.editor.properties/src-gen/org/eclipse/sirius/properties/editor/properties/sections/properties/labeldescription/LabelDescriptionValueExpressionPropertySection.java
@@ -25,16 +25,16 @@ import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 // End of user code imports
 
 /**
- * A section for the bodyExpression property of a LabelDescription object.
+ * A section for the valueExpression property of a LabelDescription object.
  */
-public class LabelDescriptionBodyExpressionPropertySection extends AbstractTextWithButtonPropertySection implements ContentProposalClient {
+public class LabelDescriptionValueExpressionPropertySection extends AbstractTextWithButtonPropertySection implements ContentProposalClient {
 
     /**
      * @see org.eclipse.sirius.properties.editor.properties.sections.AbstractTextWithButtonPropertySection#getDefaultLabelText()
      */
     @Override
     protected String getDefaultLabelText() {
-        return "BodyExpression"; //$NON-NLS-1$
+        return "ValueExpression"; //$NON-NLS-1$
     }
 
     /**
@@ -55,7 +55,7 @@ public class LabelDescriptionBodyExpressionPropertySection extends AbstractTextW
      */
     @Override
     public EAttribute getFeature() {
-        return PropertiesPackage.eINSTANCE.getLabelDescription_BodyExpression();
+        return PropertiesPackage.eINSTANCE.getLabelDescription_ValueExpression();
     }
 
     /**
@@ -98,7 +98,7 @@ public class LabelDescriptionBodyExpressionPropertySection extends AbstractTextW
         return new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent e) {
-                TextWithContentProposalDialog dialog = new TextWithContentProposalDialog(composite.getShell(), LabelDescriptionBodyExpressionPropertySection.this, text.getText());
+                TextWithContentProposalDialog dialog = new TextWithContentProposalDialog(composite.getShell(), LabelDescriptionValueExpressionPropertySection.this, text.getText());
                 dialog.open();
                 text.setText(dialog.getResult());
                 handleTextModified();

incubation/org.eclipse.sirius.properties.edit/plugin.properties
@@ -74,7 +74,6 @@ _UI_DynamicMappingIf_type = If
 _UI_DynamicMappingFor_ifs_feature = Ifs
 _UI_DynamicMappingIf_predicateExpression_feature = Predicate Expression
 _UI_DynamicMappingIf_widget_feature = Widget
-_UI_LabelDescription_bodyExpression_feature = Body Expression
 _UI_WidgetDescription_helpExpression_feature = Help Expression
 _UI_OperationDescription_type = Operation
 _UI_CustomDescription_type = Custom Widget
@@ -206,3 +205,4 @@ _UI_GroupStyle_expandedByDefault_feature = Expanded by Default
 _UI_GroupConditionalStyle_type = Conditional Style
 _UI_GroupConditionalStyle_style_feature = Style
 _UI_GroupDescription_conditionalStyles_feature = Conditional Styles
+_UI_LabelDescription_valueExpression_feature = Value Expression

incubation/org.eclipse.sirius.properties.edit/src-gen/org/eclipse/sirius/properties/provider/LabelDescriptionItemProvider.java
@@ -55,22 +55,22 @@ public class LabelDescriptionItemProvider extends WidgetDescriptionItemProvider 
         if (itemPropertyDescriptors == null) {
             super.getPropertyDescriptors(object);
 
-            addBodyExpressionPropertyDescriptor(object);
+            addValueExpressionPropertyDescriptor(object);
         }
         return itemPropertyDescriptors;
     }
 
     /**
-     * This adds a property descriptor for the Body Expression feature. <!--
+     * This adds a property descriptor for the Value Expression feature. <!--
      * begin-user-doc --> <!-- end-user-doc -->
      *
      * @generated
      */
-    protected void addBodyExpressionPropertyDescriptor(Object object) {
+    protected void addValueExpressionPropertyDescriptor(Object object) {
         itemPropertyDescriptors
-                .add(createItemPropertyDescriptor(((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(), getResourceLocator(), getString("_UI_LabelDescription_bodyExpression_feature"), //$NON-NLS-1$
-                        getString("_UI_PropertyDescriptor_description", "_UI_LabelDescription_bodyExpression_feature", "_UI_LabelDescription_type"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-                        PropertiesPackage.Literals.LABEL_DESCRIPTION__BODY_EXPRESSION, true, false, false, ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
+                .add(createItemPropertyDescriptor(((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(), getResourceLocator(), getString("_UI_LabelDescription_valueExpression_feature"), //$NON-NLS-1$
+                        getString("_UI_PropertyDescriptor_description", "_UI_LabelDescription_valueExpression_feature", "_UI_LabelDescription_type"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                        PropertiesPackage.Literals.LABEL_DESCRIPTION__VALUE_EXPRESSION, true, false, false, ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
     }
 
     /**
@@ -80,7 +80,7 @@ public class LabelDescriptionItemProvider extends WidgetDescriptionItemProvider 
      * {@link org.eclipse.emf.edit.command.RemoveCommand} or
      * {@link org.eclipse.emf.edit.command.MoveCommand} in
      * {@link #createCommand}. <!-- begin-user-doc --> <!-- end-user-doc -->
-     * 
+     *
      * @generated
      */
     @Override
@@ -95,7 +95,7 @@ public class LabelDescriptionItemProvider extends WidgetDescriptionItemProvider 
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
-     * 
+     *
      * @generated
      */
     @Override
@@ -110,7 +110,7 @@ public class LabelDescriptionItemProvider extends WidgetDescriptionItemProvider 
     /**
      * This returns LabelDescription.gif. <!-- begin-user-doc --> <!--
      * end-user-doc -->
-     * 
+     *
      * @generated
      */
     @Override
@@ -121,7 +121,7 @@ public class LabelDescriptionItemProvider extends WidgetDescriptionItemProvider 
     /**
      * This returns the label text for the adapted class. <!-- begin-user-doc
      * --> <!-- end-user-doc -->
-     * 
+     *
      * @generated
      */
     @Override
@@ -136,7 +136,7 @@ public class LabelDescriptionItemProvider extends WidgetDescriptionItemProvider 
      * update any cached children and by creating a viewer notification, which
      * it passes to {@link #fireNotifyChanged}. <!-- begin-user-doc --> <!--
      * end-user-doc -->
-     * 
+     *
      * @generated
      */
     @Override
@@ -144,7 +144,7 @@ public class LabelDescriptionItemProvider extends WidgetDescriptionItemProvider 
         updateChildren(notification);
 
         switch (notification.getFeatureID(LabelDescription.class)) {
-        case PropertiesPackage.LABEL_DESCRIPTION__BODY_EXPRESSION:
+        case PropertiesPackage.LABEL_DESCRIPTION__VALUE_EXPRESSION:
             fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
             return;
         case PropertiesPackage.LABEL_DESCRIPTION__STYLE:

incubation/org.eclipse.sirius.properties/model/properties.ecore
@@ -107,7 +107,7 @@
         eType="#//ButtonWidgetConditionalStyle" containment="true"/>
   </eClassifiers>
   <eClassifiers xsi:type="ecore:EClass" name="LabelDescription" eSuperTypes="#//WidgetDescription">
-    <eStructuralFeatures xsi:type="ecore:EAttribute" name="bodyExpression" eType="ecore:EDataType ../../org.eclipse.sirius/model/viewpoint.ecore#//description/InterpretedExpression"/>
+    <eStructuralFeatures xsi:type="ecore:EAttribute" name="valueExpression" eType="ecore:EDataType ../../org.eclipse.sirius/model/viewpoint.ecore#//description/InterpretedExpression"/>
     <eStructuralFeatures xsi:type="ecore:EReference" name="style" eType="#//LabelWidgetStyle"
         containment="true"/>
     <eStructuralFeatures xsi:type="ecore:EReference" name="conditionalStyles" upperBound="-1"

incubation/org.eclipse.sirius.properties/model/properties.genmodel
@@ -96,7 +96,7 @@
       <genFeatures property="None" children="true" createChild="true" ecoreFeature="ecore:EReference properties.ecore#//ButtonDescription/conditionalStyles"/>
     </genClasses>
     <genClasses ecoreClass="properties.ecore#//LabelDescription">
-      <genFeatures createChild="false" ecoreFeature="ecore:EAttribute properties.ecore#//LabelDescription/bodyExpression"/>
+      <genFeatures createChild="false" ecoreFeature="ecore:EAttribute properties.ecore#//LabelDescription/valueExpression"/>
       <genFeatures property="None" children="true" createChild="true" ecoreFeature="ecore:EReference properties.ecore#//LabelDescription/style"/>
       <genFeatures property="None" children="true" createChild="true" ecoreFeature="ecore:EReference properties.ecore#//LabelDescription/conditionalStyles"/>
     </genClasses>

commit b0c93ffbf0ce45e5704175036e9e1b973ab1685f

[482528] Fix label value expression
Commit e5e1a5b15c mistakenly replaced bodyExpression with
labelExpression instead of valueExpression in VDC.

Bug: 482528
Change-Id: I806d854740a7bb75ad708491d071f07a48eeb470
Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

incubation/org.eclipse.sirius.ui.properties/src/org/eclipse/sirius/ui/properties/internal/tabprovider/ViewDescriptionConverter.java
@@ -735,7 +735,7 @@ public class ViewDescriptionConverter {
         EEFLabelDescription eefLabelDescription = EefFactory.eINSTANCE.createEEFLabelDescription();
 
         eefLabelDescription.setIdentifier(labelDescription.getIdentifier());
-        eefLabelDescription.setLabelExpression(labelDescription.getLabelExpression());
+        eefLabelDescription.setValueExpression(labelDescription.getValueExpression());
         LabelWidgetStyle bodyStyle = labelDescription.getStyle();
         if (bodyStyle != null) {
             eefLabelDescription.setStyle((EEFLabelStyle) createEEFWidgetStyle(bodyStyle));