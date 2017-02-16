f33039720526ed6ce66c41217e2c2a7bdce53843
Pierre-Charles David <pierre-charles.david@obeo.fr>
2017-02-16 15:20:33 +0100

    [cleanup] Remove usage of JFace's ListenerList
    This class has been deprecated for a while and is being removed in
    Oxygen (see bug #436448).
    
    Change-Id: I34b011274cb9b94675149dd64bcdc9ddb69e127c
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.sirius.common.ui/src/org/eclipse/sirius/common/ui/tools/api/wizard/CheckboxTreeAndListGroup.java
+++ plugins/org.eclipse.sirius.common.ui/src/org/eclipse/sirius/common/ui/tools/api/wizard/CheckboxTreeAndListGroup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * Copyright (c) 2000, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -19,8 +19,8 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import org.eclipse.core.runtime.ListenerList;
 import org.eclipse.core.runtime.SafeRunner;
-import org.eclipse.jface.util.ListenerList;
 import org.eclipse.jface.util.SafeRunnable;
 import org.eclipse.jface.viewers.CheckStateChangedEvent;
 import org.eclipse.jface.viewers.CheckboxTableViewer;
