cd9a3932085a50f8f1936a2c8651222e06e3f01d
Laurent Redor <laurent.redor@obeo.fr>
2017-02-20 11:30:32 +0100

    [512497] Remove unused GMF decorators (bookmark and unresolvedView)
    With this commit, these decorators are now only instantiated but never
    used during refresh. It is not possible to not instantiate them.
    
    Bug: 512497
    Cherry-picked-from: 512419
    Change-Id: I39fa78dc77192d85e916e2bdd34a6f89f17ac4c8
    Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>

---
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/internal/providers/SiriusBookmarkAndUnresolvedViewDecoratorProvider.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright (c) 2017 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.diagram.ui.internal.providers;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gmf.runtime.common.core.service.AbstractProvider;
+import org.eclipse.gmf.runtime.common.core.service.IOperation;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.IPrimaryEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.services.decorator.CreateDecoratorsOperation;
+import org.eclipse.gmf.runtime.diagram.ui.services.decorator.IDecoratorKeys;
+import org.eclipse.gmf.runtime.diagram.ui.services.decorator.IDecoratorProvider;
+import org.eclipse.gmf.runtime.diagram.ui.services.decorator.IDecoratorTarget;
+import org.eclipse.gmf.runtime.notation.View;
+
+/**
+ * The goal of this class is to "disable" (replace by something that do
+ * nothing), the decorators
+ * {@link org.eclipse.gmf.runtime.diagram.ui.services.decorator.IDecoratorKeys.BOOKMARK}
+ * and
+ * {@link org.eclipse.gmf.runtime.diagram.ui.services.decorator.IDecoratorKeys.UNRESOLVED_VIEW}
+ * provided by
+ * {@link org.eclipse.gmf.runtime.diagram.ui.providers.internal.DiagramDecoratorProvider}
+ * on Sirius diagram element edit parts and not used by Sirius. Even if they are
+ * not used, they have performance cost during refresh of edit parts. This class
+ * does not avoid the instantiation of BookmarkDecorator and
+ * UnresolvedViewDecorator but it avoids to have cost during each refresh. It is
+ * not possible without "modifying" GMF to avoid the instantiation.
+ * 
+ * @author <a href="mailto:laurent.redor@obeo.fr">Laurent Redor</a>
+ */
+@SuppressWarnings("restriction")
+public class SiriusBookmarkAndUnresolvedViewDecoratorProvider extends AbstractProvider implements IDecoratorProvider {
+
+    @Override
+    public boolean provides(IOperation operation) {
+        Assert.isNotNull(operation);
+
+        if (!(operation instanceof CreateDecoratorsOperation)) {
+            return false;
+        }
+
+        IDecoratorTarget decoratorTarget = ((CreateDecoratorsOperation) operation).getDecoratorTarget();
+        return decoratorTarget.getAdapter(View.class) != null;
+    }
+
+    @Override
+    public void createDecorators(IDecoratorTarget decoratorTarget) {
+        EditPart ep = (EditPart) decoratorTarget.getAdapter(EditPart.class);
+        if (ep instanceof IPrimaryEditPart) {
+            // Replace decorator installed by GMF by another one that do nothing
+            decoratorTarget.installDecorator(IDecoratorKeys.BOOKMARK, new SiriusBookmarkDecorator(decoratorTarget));
+            Object model = ep.getModel();
+            if (!(model instanceof View))
+                return;
+            if (((View) model).getElement() != null) {
+                // Replace decorator installed by GMF by another one that do
+                // nothing
+                decoratorTarget.installDecorator(IDecoratorKeys.UNRESOLVED_VIEW, new SiriusUnresolvedViewDecorator(decoratorTarget));
+            }
+        }
+    }
+}
---
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/internal/providers/SiriusBookmarkDecorator.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2017 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.diagram.ui.internal.providers;
+
+import org.eclipse.gmf.runtime.diagram.ui.services.decorator.AbstractDecorator;
+import org.eclipse.gmf.runtime.diagram.ui.services.decorator.IDecoratorTarget;
+
+/**
+ * Decorator that do nothing (just replace GMF
+ * {@link org.eclipse.gmf.runtime.diagram.ui.providers.internal.BookmarkDecorator}
+ * ).
+ * 
+ * @author <a href="mailto:laurent.redor@obeo.fr">Laurent Redor</a>
+ */
+@SuppressWarnings("restriction")
+public class SiriusBookmarkDecorator extends AbstractDecorator {
+
+    /**
+     * Default constructor.
+     * 
+     * @param decoratorTarget
+     *            the object to be decorated
+     */
+    public SiriusBookmarkDecorator(IDecoratorTarget decoratorTarget) {
+        super(decoratorTarget);
+    }
+
+    @Override
+    public void activate() {
+        // Do nothing
+    }
+
+    @Override
+    public void refresh() {
+        // Do nothing
+    }
+}
