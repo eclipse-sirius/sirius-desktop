8583155a93bc9f06503b990897dc0ec2aec57ef1
Laurent Redor <laurent.redor@obeo.fr>
2017-03-03 14:46:59 +0100

    [513069] Update equals method of SessionSaveable
    Also consider all subclass of SessionSaveable instead of just the same
    getClass() for both sessionSaveable.
    
    Bug: 513069
    Cherry-picked-from: 513061
    Change-Id: I255a602d500a2388251ae23d97320c6e3e5a1183
    Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>
    (cherry picked from commit 872c7c2d944fdd0852d8cb40d14614c2a534c8e2)

--- plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/business/internal/session/SessionSaveable.java
+++ plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/business/internal/session/SessionSaveable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2015 THALES GLOBAL SERVICES.
+ * Copyright (c) 2011, 2017 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -51,7 +51,7 @@ public class SessionSaveable extends Saveable {
     public void doSave(IProgressMonitor monitor) {
         new SaveSessionRunnable(session).run(monitor);
     }
-    
+
     @Override
     public IJobRunnable doSave(IProgressMonitor monitor, IShellProvider shellProvider) throws CoreException {
         return new SaveSessionRunnable(session);
@@ -135,7 +135,7 @@ public class SessionSaveable extends Saveable {
         if (obj == null) {
             return false;
         }
-        if (getClass() != obj.getClass()) {
+        if (!(obj instanceof SessionSaveable)) {
             return false;
         }
         final SessionSaveable other = (SessionSaveable) obj;
