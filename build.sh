bec086f921d0ef2a130a4a26f39003d802403e70
Pierre-Charles David <pierre-charles.david@obeo.fr>
2017-03-21 09:44:48 +0100

    [build] Update target platforms
    * Make build.sh default to oxygen.
    * Use the Orbit repo from Neon.3.
    * Fix references to EMF Services & GMF Runtime builds in canary TP.
    * Consume nightly versions of EEF, Accele and AQL now that M6 is done.
    
    Change-Id: Ifd2fbe55b4cf92fd0e83235bfdd091bd081d0c37
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-readonly PLATFORM=${1:-neon}
+readonly PLATFORM=${1:-oxygen}
 readonly GOAL=${2:-package}
 readonly TESTS_SUITES=${3:-junit,swtbot-sequence,swtbot}
 readonly BUILD_POM="packaging/org.eclipse.sirius.parent/pom.xml"
