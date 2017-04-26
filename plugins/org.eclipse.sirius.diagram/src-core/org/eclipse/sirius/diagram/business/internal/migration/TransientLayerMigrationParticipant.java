d763fe20907663542ced77240f47ada4fcf33582
Laurent Fasani <laurent.fasani@obeo.fr>
2017-04-26 18:06:19 +0200

    [506259] Add migration participant for transient layer
     * Remove DDiagram.activatedLayers and DDiagramElement.decorations if
    they come from a layer that is now considered as transient.
    
    Bug: 506259
    Change-Id: Ib265fae905fdb04e4528ac166d3e845748253a5e
    Signed-off-by: Laurent Fasani <laurent.fasani@obeo.fr>

---
+++ plugins/org.eclipse.sirius.diagram/src-core/org/eclipse/sirius/diagram/business/internal/migration/TransientLayerMigrationParticipant.java
@@ -0,0 +1,74 @@
+/*******************************************************************************
+ * Copyright (c) 2017 THALES GLOBAL SERVICES..
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.diagram.business.internal.migration;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.business.api.migration.AbstractRepresentationsFileMigrationParticipant;
+import org.eclipse.sirius.diagram.DDiagram;
+import org.eclipse.sirius.diagram.business.internal.metamodel.helper.LayerHelper;
+import org.eclipse.sirius.diagram.description.Layer;
+import org.eclipse.sirius.viewpoint.DAnalysis;
+import org.eclipse.sirius.viewpoint.Decoration;
+import org.osgi.framework.Version;
+
+/**
+ * Remove layers from DDiagram.activatedLayers that are now considered as transient and remove
+ * DDiagramElement.decorations if there descriptions come from a layer that is now considered as transient.
+ * 
+ * @author lfasani
+ *
+ */
+public class TransientLayerMigrationParticipant extends AbstractRepresentationsFileMigrationParticipant {
+
+    /**
+     * Migration version.
+     */
+    public static final Version MIGRATION_VERSION = new Version("12.0.0.201704271200"); //$NON-NLS-1$
+
+    @Override
+    public Version getMigrationVersion() {
+        return MIGRATION_VERSION;
+    }
+
+    @Override
+    protected void postLoad(DAnalysis dAnalysis, Version loadedVersion) {
+        if (loadedVersion.compareTo(MIGRATION_VERSION) < 0) {
+            TreeIterator<EObject> allContents = dAnalysis.eResource().getAllContents();
+            while (allContents.hasNext()) {
+                EObject eObject = allContents.next();
+                if (eObject instanceof DDiagram) {
+                    DDiagram diagram = (DDiagram) eObject;
+                    List<Layer> activatedLayersToRemove = diagram.getActivatedLayers().stream().filter(LayerHelper::isTransientLayer).collect(Collectors.toList());
+                    if (!activatedLayersToRemove.isEmpty()) {
+
+                        diagram.getDiagramElements().stream().forEach(element -> {
+                            Iterator<Decoration> decosIterator = element.getDecorations().iterator();
+                            while (decosIterator.hasNext()) {
+                                Decoration deco = decosIterator.next();
+                                Layer parentLayer = LayerHelper.getParentLayer(deco.getDescription());
+                                if (activatedLayersToRemove.contains(parentLayer)) {
+                                    decosIterator.remove();
+                                }
+                            }
+                        });
+                        diagram.getActivatedLayers().removeAll(activatedLayersToRemove);
+                        allContents.prune();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ plugins/org.eclipse.sirius.tests.junit/src/org/eclipse/sirius/tests/unit/diagram/migration/TransientLayerMigrationTest.java
@@ -0,0 +1,154 @@
+/*******************************************************************************
+ * Copyright (c) 2017 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.tests.unit.diagram.migration;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ExtensibleURIConverterImpl;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.sirius.business.internal.migration.RepresentationsFileMigrationService;
+import org.eclipse.sirius.diagram.business.internal.migration.TransientLayerMigrationParticipant;
+import org.eclipse.sirius.ecore.extender.tool.api.ModelUtils;
+import org.eclipse.sirius.tests.SiriusTestsPlugin;
+import org.eclipse.sirius.tests.support.api.SiriusTestCase;
+import org.eclipse.sirius.tools.api.command.ICommandFactory;
+import org.eclipse.sirius.viewpoint.DAnalysis;
+import org.eclipse.sirius.viewpoint.ViewpointPackage;
+import org.osgi.framework.Version;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * Test class to test that, in the case the layer is now considered as
+ * transient, the removal of the layer activation and the removal of decorations
+ * on DDiagramElements.
+ * 
+ * @author lfasani
+ */
+public class TransientLayerMigrationTest extends SiriusTestCase {
+
+    private static final String PATH = "/data/unit/migration/do_not_migrate/transientLayer/";
+
+    private static final String SESSION_RESOURCE_NAME = "transientLayer.aird";
+
+    private static final String SEMANTIC_MODEL_FILENAME = "transientLayer.ecore";
+
+    private static final String VSM_RESOURCE_NAME = "transientLayer.odesign";
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        genericSetUp();
+        copyFilesToTestProject(SiriusTestsPlugin.PLUGIN_ID, PATH, SEMANTIC_MODEL_FILENAME, SESSION_RESOURCE_NAME, VSM_RESOURCE_NAME);
+    }
+
+    /**
+     * Test that the data were not migrated on the repo. It allows to check the
+     * effect of the migration in the other test.
+     */
+    public void testMigrationIsNeededOnData() {
+        Version dRepresentationContainerRemovalVersion = TransientLayerMigrationParticipant.MIGRATION_VERSION;
+
+        // Check that the representation file migration is needed.
+        URI uri = URI.createPlatformPluginURI(SiriusTestsPlugin.PLUGIN_ID + PATH + SESSION_RESOURCE_NAME, true);
+        Version loadedVersion = checkRepresentationFileMigrationStatus(uri, true);
+        assertTrue("The transient layer migration must be required on test data.", dRepresentationContainerRemovalVersion.compareTo(loadedVersion) > 0);
+
+        // check the file contains decorations
+        checkTransientLayerMigration(uri, 2, 3);
+    }
+
+    private void checkTransientLayerMigration(URI uri, int expectedNbDecoration, int expectedNbActivatedLayer) {
+        int nbDeco[] = new int[1];
+        int nbActivatedLayer[] = new int[1];
+        ExtensibleURIConverterImpl uriConverterImpl = new ExtensibleURIConverterImpl();
+        InputStream inputStream = null;
+        try {
+            inputStream = uriConverterImpl.createInputStream(uri);
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            saxParser.parse(inputStream, new DefaultHandler() {
+
+                @Override
+                public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+                    if (qName.equals("decorations") && attributes.getValue("xmi:type").equals(ViewpointPackage.eINSTANCE.getNsPrefix() + ":" + "Decoration")) {
+                        nbDeco[0] = nbDeco[0] + 1;
+                    } else if (qName.equals("activatedLayers")) {
+                        nbActivatedLayer[0] = nbActivatedLayer[0] + 1;
+                    }
+                }
+            });
+        } catch (Exception e) {
+            failCheckData();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (final IOException e) {
+                    failCheckData();
+                }
+            }
+        }
+        assertEquals("Bad decoration number", expectedNbDecoration, nbDeco[0]);
+        assertEquals("Bad activated layer number", expectedNbActivatedLayer, nbActivatedLayer[0]);
+    }
+
+    private void failCheckData() {
+        fail("Check the test data, we should not fail here.");
+    }
+
+    /**
+     * Check the migration is done and does not produce errors during VSM load.
+     */
+    public void testDRepresentationContainerRemovalMigrationDone() {
+        ResourceSet set = new ResourceSetImpl();
+        DAnalysis analysis = null;
+        try {
+            analysis = (DAnalysis) ModelUtils.load(URI.createPlatformResourceURI(TEMPORARY_PROJECT_NAME + "/" + SESSION_RESOURCE_NAME, true), set);
+        } catch (IOException e) {
+            failCheckData();
+        }
+
+        // Check that the migration was done.
+        assertNotNull("Check the representation file test data.", analysis);
+
+        // The version will change on save, so migration service will still
+        // indicates that the migration is needed.
+        String version = analysis.getVersion();
+        assertTrue("Before save, the migration framework will return true even if the migration has been done during load.",
+                RepresentationsFileMigrationService.getInstance().isMigrationNeeded(Version.parseVersion(version)));
+
+        try {
+            analysis.eResource().save(Collections.emptyMap());
+        } catch (IOException e) {
+            failCheckData();
+        }
+        // save should update the version.
+        version = analysis.getVersion();
+        assertFalse("The version tag should now be set telling that the migration was done.", RepresentationsFileMigrationService.getInstance().isMigrationNeeded(Version.parseVersion(version)));
+        checkTransientLayerMigration(analysis.eResource().getURI(), 1, 2);
+        assertFalse(getErrorLoggersMessage(), doesAnErrorOccurs());
+    }
+
+    @Override
+    protected ICommandFactory getCommandFactory() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
