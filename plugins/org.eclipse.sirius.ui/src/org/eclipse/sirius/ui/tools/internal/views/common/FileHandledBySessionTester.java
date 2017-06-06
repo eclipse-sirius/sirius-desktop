pcdavid/org.eclipse.sirius

[510040] Make available close action on all IFile tree viewer items
The close session action is now available in right click context menu on
all aird tree viewer items when they point at an IFile and when not in
model explorer view.

Bug: 510040
Change-Id: Ic04f1e9c269fd0da82df40ef300e8ff718946bff
Signed-off-by: pguilet <pierre.guilet@obeo.fr>

plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/tools/internal/views/common/FileHandledBySessionTester.java
@@ -11,24 +11,35 @@
 package org.eclipse.sirius.ui.tools.internal.views.common;
 
 import java.util.Collections;
+import java.util.List;
 
 import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.sirius.business.api.modelingproject.ModelingProject;
 import org.eclipse.sirius.business.api.query.FileQuery;
+import org.eclipse.sirius.business.api.session.Session;
+import org.eclipse.sirius.ui.tools.internal.views.modelexplorer.ModelExplorerView;
+import org.eclipse.ui.PlatformUI;
+
+import com.google.common.collect.Lists;
 
 /**
  * Property tester to check that an {@link IFile} is:
  * <UL>
  * <LI>in a modeling project,</LI>
  * <LI>or a session resource file,</LI>
- * <LI>or the semantic resource of a transient session (no session file, the
- * session data is in memory).</LI>
+ * <LI>or is an aird file loaded in a session</LI>
+ * <LI>or the semantic resource of a transient session (no session file, the session data is in memory).</LI>
  * </UL>
  * 
  * @author mporhel
  */
 public class FileHandledBySessionTester extends PropertyTester {
+    /**
+     * Property verifying that a given element in an IFile representing an aird that has a corresponding session
+     * loading.
+     */
+    private static final String SESSION_OPENED_PROPERTY = "sessionOpened"; //$NON-NLS-1$
 
     /**
      * Constructor.
@@ -39,18 +50,49 @@ public class FileHandledBySessionTester extends PropertyTester {
     /**
      * {@inheritDoc}
      */
+    @Override
     public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
-        boolean result = false;
-        if (receiver instanceof IFile) {
-            IFile receiverFile = (IFile) receiver;
+        if (SESSION_OPENED_PROPERTY.equals(property)) {
+            return isAirdLoaded(receiver);
+        } else {
+            boolean result = false;
+            if (receiver instanceof IFile) {
+                IFile receiverFile = (IFile) receiver;
 
-            // Modeling project should show expansion arrows during session load
-            if (ModelingProject.hasModelingProjectNature(receiverFile.getProject())) {
-                result = true;
-            } else {
-                result = new FileQuery(receiverFile.getFileExtension()).isSessionResourceFile() || !FileSessionFinder.getSelectedSessions(Collections.singletonList(receiverFile)).isEmpty();
+                // Modeling project should show expansion arrows during session load
+                if (ModelingProject.hasModelingProjectNature(receiverFile.getProject())) {
+                    result = true;
+                } else {
+                    result = new FileQuery(receiverFile.getFileExtension()).isSessionResourceFile() || !FileSessionFinder.getSelectedSessions(Collections.singletonList(receiverFile)).isEmpty();
+                }
+            }
+            return result;
+        }
+    }
+
+    /**
+     * Returns true if the given element in an IFile representing an aird that has a corresponding session loading or if
+     * it detected as an aird resource.
+     * 
+     * @param element
+     *            element to test.
+     * @return true if the given element in an IFile representing an aird that has a corresponding session loading or if
+     *         it detected as an aird resource. False otherwise.
+     */
+    private boolean isAirdLoaded(Object element) {
+        boolean useSessionLoadedImage = false;
+        if (!ModelExplorerView.class.getTypeName().equals(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePart().getClass().getTypeName()) && element instanceof IFile) {
+            List<Session> relatedSessions = FileSessionFinder.getRelatedSessions(Lists.newArrayList((IFile) element), false, false);
+            for (Session session : relatedSessions) {
+                if (session.isOpen()) {
+                    useSessionLoadedImage = true;
+                    break;
+                }
+            }
+            if (!useSessionLoadedImage) {
+                useSessionLoadedImage = new FileQuery((IFile) element).isSessionResourceFile();
             }
         }
-        return result;
+        return useSessionLoadedImage;
     }
 }

eclipse-sirius/sirius-desktop

plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/tools/internal/views/common/FileHandledBySessionTester.java
@@ -11,24 +11,35 @@
 package org.eclipse.sirius.ui.tools.internal.views.common;
 
 import java.util.Collections;
+import java.util.List;
 
 import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.sirius.business.api.modelingproject.ModelingProject;
 import org.eclipse.sirius.business.api.query.FileQuery;
+import org.eclipse.sirius.business.api.session.Session;
+import org.eclipse.sirius.ui.tools.internal.views.modelexplorer.ModelExplorerView;
+import org.eclipse.ui.PlatformUI;
+
+import com.google.common.collect.Lists;
 
 /**
  * Property tester to check that an {@link IFile} is:
  * <UL>
  * <LI>in a modeling project,</LI>
  * <LI>or a session resource file,</LI>
- * <LI>or the semantic resource of a transient session (no session file, the
- * session data is in memory).</LI>
+ * <LI>or is an aird file loaded in a session</LI>
+ * <LI>or the semantic resource of a transient session (no session file, the session data is in memory).</LI>
  * </UL>
  * 
  * @author mporhel
  */
 public class FileHandledBySessionTester extends PropertyTester {
+    /**
+     * Property verifying that a given element in an IFile representing an aird that has a corresponding session
+     * loading.
+     */
+    private static final String SESSION_FILE_PROPERTY = "isSessionFile"; //$NON-NLS-1$
 
     /**
      * Constructor.
@@ -39,18 +50,43 @@ public class FileHandledBySessionTester extends PropertyTester {
     /**
      * {@inheritDoc}
      */
+    @Override
     public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
-        boolean result = false;
-        if (receiver instanceof IFile) {
-            IFile receiverFile = (IFile) receiver;
+        if (SESSION_FILE_PROPERTY.equals(property)) {
+            return isSessionFile(receiver);
+        } else {
+            boolean result = false;
+            if (receiver instanceof IFile) {
+                IFile receiverFile = (IFile) receiver;
 
-            // Modeling project should show expansion arrows during session load
-            if (ModelingProject.hasModelingProjectNature(receiverFile.getProject())) {
-                result = true;
+                // Modeling project should show expansion arrows during session load
+                if (ModelingProject.hasModelingProjectNature(receiverFile.getProject())) {
+                    result = true;
+                } else {
+                    result = new FileQuery(receiverFile.getFileExtension()).isSessionResourceFile() || !FileSessionFinder.getSelectedSessions(Collections.singletonList(receiverFile)).isEmpty();
+                }
+            }
+            return result;
+        }
+    }
+
+    /**
+     * Returns true if the given element in an IFile representing a session file.
+     * 
+     * @param element
+     *            element to test.
+     * @return true if the given element in an IFile representing a session file. False otherwise.
+     */
+    private boolean isSessionFile(Object element) {
+        boolean isSessionResourceFile = false;
+        if (!ModelExplorerView.class.getTypeName().equals(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePart().getClass().getTypeName()) && element instanceof IFile) {
+            List<Session> relatedSessions = FileSessionFinder.getRelatedSessions(Lists.newArrayList((IFile) element), false, false);
+            if (!relatedSessions.isEmpty()) {
+                isSessionResourceFile = true;
             } else {
-                result = new FileQuery(receiverFile.getFileExtension()).isSessionResourceFile() || !FileSessionFinder.getSelectedSessions(Collections.singletonList(receiverFile)).isEmpty();
+                isSessionResourceFile = new FileQuery((IFile) element).isSessionResourceFile();
             }
         }
-        return result;
+        return isSessionResourceFile;
     }
 }