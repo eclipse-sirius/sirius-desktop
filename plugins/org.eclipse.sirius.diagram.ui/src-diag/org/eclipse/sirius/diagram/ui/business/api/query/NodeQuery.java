69c1fb1fbcd980be8282cc8ac84ce8b3f4996202
Laurent Redor <laurent.redor@obeo.fr>
2017-08-17 16:14:35 +0200

    [520881] Fix bug in NodeQuery
    Without this fix, test
    org.eclipse.sirius.tests.swtbot.BorderedNodeCreationTest.testBNC_OnNodeInContainer()
    is KO (for example).
    This commit fixes the remaining failures.
    
    Bug: 520881
    Change-Id: I14d01a4296af919a32277c38d475132da069c881
    Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/business/api/query/NodeQuery.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/business/api/query/NodeQuery.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2016 THALES GLOBAL SERVICES.
+ * Copyright (c) 2013, 2017 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -287,7 +287,7 @@ public class NodeQuery {
      * @return The rectangle used for handles
      */
     public Rectangle getHandleBounds() {
-        Rectangle bounds = GMFHelper.getAbsoluteBounds(node);
+        Rectangle bounds = GMFHelper.getAbsoluteBounds(node, true);
         if (node.getElement() instanceof DDiagramElement) {
             DDiagramElement dDiagramElement = (DDiagramElement) node.getElement();
             // All container styles have a specific insets.
