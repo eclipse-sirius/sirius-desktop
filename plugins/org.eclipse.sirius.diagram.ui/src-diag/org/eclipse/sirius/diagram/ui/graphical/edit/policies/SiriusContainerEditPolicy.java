534f71bb6e90acd18b7703bf7b3654f80b8ad4d3
Florian Barbin <florian.barbin@obeo.fr>
2017-08-31 17:22:52 +0200

    [520633] Modify SnapCommand to snap sub diagram elements
    * The SnapCommand snapped only the first hierarchy of selected edit
    parts. Now we recursively snap all container sub element.
    * To avoid conflicts, border nodes locations are computed one after the
    other.
    
    Bug: 520633
    Change-Id: I75c9bafbef9278e55de0eb2c363af41c5c8d575e
    Signed-off-by: Florian Barbin <florian.barbin@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/graphical/edit/policies/SiriusContainerEditPolicy.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/graphical/edit/policies/SiriusContainerEditPolicy.java
@@ -12,22 +12,38 @@
 
 package org.eclipse.sirius.diagram.ui.graphical.edit.policies;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.Request;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.commands.UnexecutableCommand;
+import org.eclipse.gef.requests.GroupRequest;
+import org.eclipse.gmf.runtime.common.core.util.ObjectAdapter;
+import org.eclipse.gmf.runtime.common.core.util.StringStatics;
 import org.eclipse.gmf.runtime.diagram.ui.actions.ActionIds;
+import org.eclipse.gmf.runtime.diagram.ui.commands.ArrangeCommand;
+import org.eclipse.gmf.runtime.diagram.ui.commands.CommandProxy;
+import org.eclipse.gmf.runtime.diagram.ui.commands.DeferredLayoutCommand;
 import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.ContainerEditPolicy;
+import org.eclipse.gmf.runtime.diagram.ui.internal.properties.WorkspaceViewerProperties;
+import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramGraphicalViewer;
 import org.eclipse.gmf.runtime.diagram.ui.requests.ArrangeRequest;
+import org.eclipse.gmf.runtime.diagram.ui.requests.RequestConstants;
+import org.eclipse.gmf.runtime.diagram.ui.services.layout.LayoutType;
+import org.eclipse.gmf.runtime.emf.commands.core.command.CompositeTransactionalCommand;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.sirius.diagram.ui.edit.api.part.AbstractDiagramElementContainerEditPart;
 import org.eclipse.sirius.diagram.ui.internal.edit.commands.CenterEdgeLayoutCommand;
 import org.eclipse.sirius.diagram.ui.internal.edit.commands.DistributeCommand;
 import org.eclipse.sirius.diagram.ui.tools.api.requests.DistributeRequest;
+import org.eclipse.sirius.diagram.ui.tools.internal.commands.SnapCommand;
 
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
@@ -66,7 +82,7 @@ public class SiriusContainerEditPolicy extends ContainerEditPolicy {
                 return UnexecutableCommand.INSTANCE;
             }
         }
-        Command commandToReturn = super.getArrangeCommand(request);
+        Command commandToReturn = doDuplicatedGetArrangeCommand(request);
         // We add a Command to center edges that need to be at the end of the
         // layout.
         if (commandToReturn != null) {
@@ -77,6 +93,93 @@ public class SiriusContainerEditPolicy extends ContainerEditPolicy {
             }
         }
         return commandToReturn;
+    }
+
+    /**
+     * Duplicated from
+     * org.eclipse.gmf.runtime.diagram.ui.editpolicies.ContainerEditPolicy.getArrangeCommand(ArrangeRequest) to override
+     * the SnapCommand.
+     * 
+     * @param request
+     * @return
+     */
+    private Command doDuplicatedGetArrangeCommand(ArrangeRequest request) {
+        if (RequestConstants.REQ_ARRANGE_DEFERRED.equals(request.getType())) {
+            String layoutType = request.getLayoutType();
+            TransactionalEditingDomain editingDomain = ((IGraphicalEditPart) getHost()).getEditingDomain();
+            return new ICommandProxy(new DeferredLayoutCommand(editingDomain, request.getViewAdaptersToArrange(), (IGraphicalEditPart) getHost(), layoutType));
+        }
+
+        String layoutDesc = request.getLayoutType() != null ? request.getLayoutType() : LayoutType.DEFAULT;
+
+        boolean offsetFromBoundingBox = false;
+        List<IGraphicalEditPart> editparts = new ArrayList<IGraphicalEditPart>();
+
+        if ((ActionIds.ACTION_ARRANGE_ALL.equals(request.getType())) || (ActionIds.ACTION_TOOLBAR_ARRANGE_ALL.equals(request.getType()))) {
+            editparts = ((IGraphicalEditPart) getHost()).getChildren();
+            request.setPartsToArrange(editparts);
+        }
+        if ((ActionIds.ACTION_ARRANGE_SELECTION.equals(request.getType())) || (ActionIds.ACTION_TOOLBAR_ARRANGE_SELECTION.equals(request.getType()))) {
+            editparts = request.getPartsToArrange();
+            offsetFromBoundingBox = true;
+        }
+        if (RequestConstants.REQ_ARRANGE_RADIAL.equals(request.getType())) {
+            editparts = request.getPartsToArrange();
+            offsetFromBoundingBox = true;
+            layoutDesc = LayoutType.RADIAL;
+        }
+
+        if (editparts.isEmpty())
+            return null;
+
+        List hints = new ArrayList(2);
+        hints.add(layoutDesc);
+        hints.add(getHost());
+        IAdaptable layoutHint = new ObjectAdapter(hints);
+
+        TransactionalEditingDomain editingDomain = ((IGraphicalEditPart) getHost()).getEditingDomain();
+
+        CompositeTransactionalCommand ctc = new CompositeTransactionalCommand(editingDomain, StringStatics.BLANK);
+        ctc.add(new ArrangeCommand(editingDomain, StringStatics.BLANK, null, editparts, layoutHint, offsetFromBoundingBox));
+
+        // retrieves the preference store from the first edit part
+        IGraphicalEditPart firstEditPart = editparts.get(0);
+        if (firstEditPart.getViewer() instanceof DiagramGraphicalViewer) {
+            IPreferenceStore preferenceStore = ((DiagramGraphicalViewer) firstEditPart.getViewer()).getWorkspaceViewerPreferenceStore();
+            if (preferenceStore != null && preferenceStore.getBoolean(WorkspaceViewerProperties.SNAPTOGRID)) {
+                Command snapCmd = getSnapCommand(request);
+                if (snapCmd != null) {
+                    ctc.add(new CommandProxy(getSnapCommand(request)));
+                }
+            }
+        }
+
+        return new ICommandProxy(ctc);
+
+    }
+
+    /**
+     * Duplicates method from {@link org.eclipse.gmf.runtime.diagram.ui.editpolicies.ContainerEditPolicy} for the
+     * duplication of getArrangeCommand.
+     * 
+     * @param request
+     *            The current request.
+     * @return A command to snap the edit parts of the request.
+     */
+    private Command getSnapCommand(Request request) {
+
+        List editparts = null;
+        if (request instanceof GroupRequest) {
+            editparts = ((GroupRequest) request).getEditParts();
+        } else if (request instanceof ArrangeRequest) {
+            editparts = ((ArrangeRequest) request).getPartsToArrange();
+        }
+
+        TransactionalEditingDomain editingDomain = ((IGraphicalEditPart) getHost()).getEditingDomain();
+        if (editparts != null) {
+            return new ICommandProxy(new SnapCommand(editingDomain, editparts));
+        }
+        return null;
     }
 
     // CHECKSTYLE:ON
--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/commands/SnapCommand.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/commands/SnapCommand.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2007, 2009 IBM Corporation and others.
+ * Copyright (c) 2007, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,32 +11,49 @@
  ****************************************************************************/
 package org.eclipse.sirius.diagram.ui.tools.internal.commands;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.PositionConstants;
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.PrecisionDimension;
 import org.eclipse.draw2d.geometry.PrecisionPoint;
 import org.eclipse.draw2d.geometry.PrecisionRectangle;
+import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.gef.SnapToGrid;
 import org.eclipse.gef.SnapToHelper;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.handles.HandleBounds;
 import org.eclipse.gef.requests.ChangeBoundsRequest;
 import org.eclipse.gmf.runtime.common.core.command.CommandResult;
 import org.eclipse.gmf.runtime.diagram.ui.commands.CommandProxy;
+import org.eclipse.gmf.runtime.diagram.ui.commands.SetBoundsCommand;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.l10n.DiagramUIMessages;
 import org.eclipse.gmf.runtime.diagram.ui.requests.RequestConstants;
 import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
 import org.eclipse.gmf.runtime.emf.commands.core.command.CompositeTransactionalCommand;
+import org.eclipse.gmf.runtime.emf.core.util.EObjectAdapter;
 import org.eclipse.gmf.runtime.notation.Bounds;
 import org.eclipse.gmf.runtime.notation.LayoutConstraint;
 import org.eclipse.gmf.runtime.notation.Node;
 import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.sirius.diagram.DDiagramElement;
+import org.eclipse.sirius.diagram.business.api.query.DDiagramElementQuery;
+import org.eclipse.sirius.diagram.ui.business.api.query.ViewQuery;
+import org.eclipse.sirius.diagram.ui.edit.api.part.IAbstractDiagramNodeEditPart;
+import org.eclipse.sirius.diagram.ui.edit.api.part.IDiagramBorderNodeEditPart;
+import org.eclipse.sirius.diagram.ui.internal.refresh.GMFHelper;
+import org.eclipse.sirius.diagram.ui.internal.refresh.borderednode.CanonicalDBorderItemLocator;
+import org.eclipse.sirius.diagram.ui.tools.api.graphical.edit.styles.IBorderItemOffsets;
+import org.eclipse.sirius.ext.gmf.runtime.editparts.GraphicalHelper;
 
 // Copied from org.eclipse.gmf.runtime.diagram.ui.internal.commands.SnapCommand
 //CHECKSTYLE:OFF
@@ -74,12 +91,13 @@ public class SnapCommand extends AbstractTransactionalCommand {
      * @see org.eclipse.gmf.runtime.emf.commands.core.command.
      * AbstractTransactionalCommand#getAffectedFiles()
      */
+    @Override
     public List getAffectedFiles() {
         if (editparts != null) {
             // we only need the first child since all the edit parts being
             // snapped originate from the same diagram
             IGraphicalEditPart parent = editparts.get(0);
-            View view = (View) parent.getParent().getModel();
+            View view = parent.getPrimaryView();
             return getWorkspaceFiles(view);
         }
         return super.getAffectedFiles();
@@ -89,79 +107,147 @@ public class SnapCommand extends AbstractTransactionalCommand {
      * Executes a snap command for all the desired edit parts.
      * 
      */
+    @Override
     protected CommandResult doExecuteWithResult(IProgressMonitor progressMonitor, IAdaptable info) throws ExecutionException {
 
         CompositeTransactionalCommand snapCommand = new CompositeTransactionalCommand(getEditingDomain(), getLabel());
-
-        for (IGraphicalEditPart newEditPart : editparts) {
-            if (newEditPart.getModel() instanceof Node) {
-
-                LayoutConstraint constraint = ((Node) newEditPart.getModel()).getLayoutConstraint();
-                if (constraint instanceof Bounds) {
-
-                    Bounds bounds = (Bounds) constraint;
-                    ChangeBoundsRequest request = new ChangeBoundsRequest(RequestConstants.REQ_MOVE);
-                    request.setEditParts(newEditPart);
-
-                    // translate all coordinates to device units as a standard
-                    // if necessary
-                    // this is done since moveDelta uses device units
-
-                    PrecisionDimension moveDeltaDim = new PrecisionDimension(bounds.getX() - newEditPart.getFigure().getBounds().x, bounds.getY() - newEditPart.getFigure().getBounds().y);
-                    /*
-                     * Distance in pixels needs to be scaled by the scaling
-                     * factor of the zoom tool, i.e. ScaledRootEditPart
-                     */
-                    newEditPart.getFigure().translateToAbsolute(moveDeltaDim);
-                    PrecisionPoint moveDelta = new PrecisionPoint(moveDeltaDim.preciseWidth(), moveDeltaDim.preciseHeight());
-
-                    // In the case that the figure bounds and model's layout
-                    // constant are the same,
-                    // xDiff and yDiff will evaluate to zero, but snapToHelper
-                    // will still locate the closest
-                    // NorthWest snap locations. In the case that they're not,
-                    // we assume the figure's bounds
-                    // have not been updated, so the moveDelta value will
-                    // simulate a drag to the new location
-                    // In both situations we base the resulting snap location
-                    // off the figure's bounds
-
-                    // snapToGrid logic taken from DragEditPartsTracker.java
-                    request.getExtendedData().clear();
-                    request.setMoveDelta(moveDelta);
-
-                    PrecisionRectangle figureBounds = null;
-                    IFigure figure = newEditPart.getFigure();
-                    if (figure instanceof HandleBounds) {
-                        figureBounds = new PrecisionRectangle(((HandleBounds) figure).getHandleBounds());
-                    } else {
-                        figureBounds = new PrecisionRectangle(figure.getBounds());
-                    }
-
-                    figure.translateToAbsolute(figureBounds);
-
-                    SnapToHelper snapToHelper = newEditPart.getAdapter(SnapToHelper.class);
-                    PrecisionRectangle baseRect = new PrecisionRectangle(figureBounds);
-                    baseRect.translate(moveDelta);
-
-                    if (snapToHelper != null) {
-                        snapToHelper.snapPoint(request, PositionConstants.HORIZONTAL | PositionConstants.VERTICAL, new PrecisionRectangle[] { baseRect }, moveDelta);
-                        request.setMoveDelta(moveDelta);
-                    }
-
-                    Command gefMove = newEditPart.getCommand(request);
-                    if (gefMove != null) {
-                        snapCommand.add(new CommandProxy(gefMove));
-                    }
-
-                }
+        editparts.stream().forEach(newEditPart -> {
+            if (newEditPart instanceof IDiagramBorderNodeEditPart) {
+                handleBorderNode((IDiagramBorderNodeEditPart) newEditPart, snapCommand);
+            } else if (newEditPart instanceof IAbstractDiagramNodeEditPart) {
+                handleNodeAndContainer((IAbstractDiagramNodeEditPart) newEditPart, snapCommand);
             }
-        }
+            // We create a single snapCommand per border node to locate them one after another to avoid location
+            // conflicts.
+            newEditPart.getChildren().stream().filter(IDiagramBorderNodeEditPart.class::isInstance).forEach(borderNodeEditPart -> {
+                snapCommand.add(new SnapCommand(getEditingDomain(), Collections.singletonList((IDiagramBorderNodeEditPart) borderNodeEditPart)));
+            });
 
+            // For each edit part we add a new SnapCommand to snap its children
+
+            @SuppressWarnings("unchecked")
+            List<IGraphicalEditPart> editParts = (List<IGraphicalEditPart>) newEditPart.getChildren().stream().filter(ep -> {
+                return !(ep instanceof IDiagramBorderNodeEditPart) && ep instanceof IGraphicalEditPart;
+            }).collect(Collectors.toList());
+            if (!editParts.isEmpty()) {
+                snapCommand.add(new SnapCommand(getEditingDomain(), editParts));
+            }
+        });
         if (snapCommand != null && snapCommand.canExecute()) {
             snapCommand.execute(progressMonitor, info);
         }
         return CommandResult.newOKCommandResult();
+    }
+
+    private void handleNodeAndContainer(IAbstractDiagramNodeEditPart newEditPart, CompositeTransactionalCommand snapCommand) {
+        if (newEditPart.getModel() instanceof Node) {
+
+            LayoutConstraint constraint = ((Node) newEditPart.getModel()).getLayoutConstraint();
+            if (constraint instanceof Bounds) {
+
+                Bounds bounds = (Bounds) constraint;
+                ChangeBoundsRequest request = new ChangeBoundsRequest(RequestConstants.REQ_MOVE);
+                request.setEditParts(newEditPart);
+
+                // translate all coordinates to device units as a standard
+                // if necessary
+                // this is done since moveDelta uses device units
+
+                PrecisionDimension moveDeltaDim = new PrecisionDimension(bounds.getX() - newEditPart.getFigure().getBounds().x, bounds.getY() - newEditPart.getFigure().getBounds().y);
+                /*
+                 * Distance in pixels needs to be scaled by the scaling
+                 * factor of the zoom tool, i.e. ScaledRootEditPart
+                 */
+                newEditPart.getFigure().translateToAbsolute(moveDeltaDim);
+                PrecisionPoint moveDelta = new PrecisionPoint(moveDeltaDim.preciseWidth(), moveDeltaDim.preciseHeight());
+
+                // In the case that the figure bounds and model's layout
+                // constant are the same,
+                // xDiff and yDiff will evaluate to zero, but snapToHelper
+                // will still locate the closest
+                // NorthWest snap locations. In the case that they're not,
+                // we assume the figure's bounds
+                // have not been updated, so the moveDelta value will
+                // simulate a drag to the new location
+                // In both situations we base the resulting snap location
+                // off the figure's bounds
+
+                // snapToGrid logic taken from DragEditPartsTracker.java
+                request.getExtendedData().clear();
+                request.setMoveDelta(moveDelta);
+
+                PrecisionRectangle figureBounds = null;
+                IFigure figure = newEditPart.getFigure();
+                if (figure instanceof HandleBounds) {
+                    figureBounds = new PrecisionRectangle(((HandleBounds) figure).getHandleBounds());
+                } else {
+                    figureBounds = new PrecisionRectangle(figure.getBounds());
+                }
+
+                figure.translateToAbsolute(figureBounds);
+
+                // We compute the new GMF Absolute Location. The SnapToHelper works with absolute coordinates.
+                Point newGMFAbsoluteLocation = GMFHelper.getAbsoluteLocation((Node) newEditPart.getModel(), true);
+                double scale = GraphicalHelper.getZoom(newEditPart);
+                newGMFAbsoluteLocation.scale(scale);
+                SnapToHelper snapToHelper = newEditPart.getAdapter(SnapToHelper.class);
+                PrecisionRectangle baseRect = new PrecisionRectangle(figureBounds);
+                baseRect.setLocation(newGMFAbsoluteLocation);
+                if (snapToHelper != null) {
+                    PrecisionPoint result = new PrecisionPoint();
+                    snapToHelper.snapPoint(request, PositionConstants.HORIZONTAL | PositionConstants.VERTICAL, new PrecisionRectangle[] { baseRect }, result);
+
+                    request.setMoveDelta(moveDelta.getTranslated(result));
+                }
+
+                Command gefMove = newEditPart.getCommand(request);
+                if (gefMove != null) {
+                    snapCommand.add(new CommandProxy(gefMove));
+                }
+
+            }
+        }
+    }
+
+    /**
+     * We make a specific treatment for border node since we need to use the {@link CanonicalDBorderItemLocator}.
+     * 
+     * @param newEditPart
+     * @param snapCommand
+     */
+    private void handleBorderNode(IDiagramBorderNodeEditPart newEditPart, CompositeTransactionalCommand snapCommand) {
+        if (newEditPart.getModel() instanceof Node) {
+            Node node = (Node) newEditPart.getModel();
+            Node parentNode = (Node) node.eContainer();
+
+            // We use the CanonicalDBorderItemLocator to compute the new border node location in absolute coordinates
+            Dimension spacing = (Dimension) newEditPart.getViewer().getProperty(SnapToGrid.PROPERTY_GRID_SPACING);
+            CanonicalDBorderItemLocator itemLocator = initCDBIL(node, parentNode, spacing);
+            Rectangle newGMFAbsoluteBounds = GMFHelper.getAbsoluteBounds(node, true);
+            Point newValidLocation = itemLocator.getValidLocation(newGMFAbsoluteBounds, node, Collections.singleton(node));
+
+            // We compute the new relative coordinates
+            Point parentAbsoluteLocation = GMFHelper.getAbsoluteLocation(parentNode, true);
+            Point newValidRelativeLocation = newValidLocation.getTranslated(parentAbsoluteLocation.getNegated());
+
+            SetBoundsCommand setBoundsCommand = new SetBoundsCommand(getEditingDomain(), getLabel(), new EObjectAdapter(newEditPart.getNotationView()), newValidRelativeLocation);
+            snapCommand.add(setBoundsCommand);
+        }
+
+    }
+
+    private CanonicalDBorderItemLocator initCDBIL(Node node, Node parentNode, Dimension spacing) {
+        CanonicalDBorderItemLocator itemLocator = new CanonicalDBorderItemLocator(parentNode, PositionConstants.NSEW, true, spacing.height);
+        if (new ViewQuery(node).isForNameEditPart()) {
+            itemLocator.setBorderItemOffset(IBorderItemOffsets.NO_OFFSET);
+        } else {
+            if (new DDiagramElementQuery((DDiagramElement) node.getElement()).isIndirectlyCollapsed()) {
+                itemLocator.setBorderItemOffset(IBorderItemOffsets.COLLAPSE_FILTER_OFFSET);
+            } else {
+                itemLocator.setBorderItemOffset(IBorderItemOffsets.DEFAULT_OFFSET);
+            }
+        }
+        return itemLocator;
     }
 }
 // CHECLSTYLE:ON
