ff06701e4ee887fcab87f39f70c7970096149e21
Laurent Redor <laurent.redor@obeo.fr>
2017-09-08 18:04:06 +0200

    [522108] Fix case of Tree routing style action on tabbar or global menu
    On tabbar or on global Eclipse menu, when an edge and its label is
    selected, the Oblique and Rectilinear actions are enabled but the Tree
    action is disabled.
    This commit fixes this problem.
    
    Bug: 522108
    Change-Id: Ia4723320b99f4e747f9d48f537d1844f3bd15ac2
    Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/actions/TabbarRouterAction.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/actions/TabbarRouterAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2012 THALES GLOBAL SERVICES.
+ * Copyright (c) 2010, 2017 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -56,7 +56,7 @@ public class TabbarRouterAction extends RouterAction {
                 return false;
             }
 
-            ListIterator<?> li = selected.listIterator();
+            ListIterator<?> li = getOperationSet().listIterator();
             while (li.hasNext()) {
                 if (!(li.next() instanceof ITreeBranchEditPart)) {
                     return false;
