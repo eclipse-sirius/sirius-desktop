3ce99ad250dcfa0b438b25b590e611be668ae1b3
Pierre-Charles David <pierre-charles.david@obeo.fr>
2018-02-02 12:00:15 +0100

    [releng] Adapt publish-nightly.sh to the new format of GIT_BRANCH
    Change-Id: Ie6b4b2cd076d6c4d6cd9233cb10de9d43e4136e1
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- releng/org.eclipse.sirius.releng/publish-nightly.sh
+++ releng/org.eclipse.sirius.releng/publish-nightly.sh
@@ -128,7 +128,7 @@ cp -dR "$WKS"/packaging/org.eclipse.sirius.update/target/org.eclipse.sirius.upda
 create_redirect "$TARGET_ROOT/$STREAM/$PLATFORM/tests" "$BUILD_TYPE/$FULL_VERSION/$PLATFORM/tests"
 cp -dR "$WKS"/packaging/org.eclipse.sirius.tests.update/target/org.eclipse.sirius.tests.update-*.zip "$TARGET_ROOT/$STREAM/org.eclipse.sirius.tests-$VERSION-$PLATFORM.zip"
 # Also update the global "latest" links if we are building master
-if [ "master" = "$GIT_BRANCH" ]; then
+if [ "origin/master" = "$GIT_BRANCH" ]; then
     create_redirect "$TARGET_ROOT/latest/$PLATFORM" "$BUILD_TYPE/$FULL_VERSION/$PLATFORM"
     cp -dR "$WKS"/packaging/org.eclipse.sirius.update/target/org.eclipse.sirius.update-*.zip "$TARGET_ROOT/$STREAM/org.eclipse.sirius-$VERSION-$PLATFORM.zip"
     create_redirect "$TARGET_ROOT/latest/$PLATFORM/tests" "$BUILD_TYPE/$FULL_VERSION/$PLATFORM/tests"
