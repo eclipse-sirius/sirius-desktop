47ce61d8534b28b6ed63b2930e75e7bba7386d93
Maxime Porhel <maxime.porhel@obeo.fr>
2018-02-22 09:38:07 +0100

    [531514] Avoid NPE if the border color of the style is null
    Bug: 531514
    Cherry-picked-from: 531513
    Change-Id: I7496497ea448eea75d9b4c244c7dac3d76bd6024
    Signed-off-by: Maxime Porhel <maxime.porhel@obeo.fr>
    (cherry picked from commit 385794f3352250ce80aea951a0636e6d37db525b)

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/figure/ContainerWithTitleBlockFigure.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/figure/ContainerWithTitleBlockFigure.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2017 THALES GLOBAL SERVICES and others.
+ * Copyright (c) 2010, 2018 THALES GLOBAL SERVICES and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -181,7 +181,9 @@ public class ContainerWithTitleBlockFigure extends SiriusDefaultSizeNodeFigure {
     private Color geBorderColor() {
         if (viewNode.getStyle() instanceof BorderedStyle) {
             RGBValues borderColor = ((BorderedStyle) viewNode.getStyle()).getBorderColor();
-            return new Color(null, borderColor.getRed(), borderColor.getGreen(), borderColor.getBlue());
+            if (borderColor != null) {
+                return new Color(null, borderColor.getRed(), borderColor.getGreen(), borderColor.getBlue());
+            }
         }
         return ColorConstants.black;
     }
