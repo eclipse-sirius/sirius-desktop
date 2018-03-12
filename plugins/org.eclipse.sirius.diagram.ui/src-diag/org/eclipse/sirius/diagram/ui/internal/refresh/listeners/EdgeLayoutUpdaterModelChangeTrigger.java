9a5fba73a4b4c1cf22babaaf382b3fd4a0b31957
Laurent Fasani <laurent.fasani@obeo.fr>
2018-03-12 11:45:53 +0100

    [531722] Enhance EdgeLayoutUpdaterModelChangeTrigger performance
    * Add two maps to avoid recompute Edge and Node from notifications too
    many times
    
    Bug: 531722
    Change-Id: I32d65a4feaf6640610c7143e435b1ac0b931b7bb
    Signed-off-by: Laurent Fasani <laurent.fasani@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/internal/refresh/listeners/EdgeLayoutUpdaterModelChangeTrigger.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/internal/refresh/listeners/EdgeLayoutUpdaterModelChangeTrigger.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2016 THALES GLOBAL SERVICES.
+ * Copyright (c) 2014, 2018 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,10 @@ package org.eclipse.sirius.diagram.ui.internal.refresh.listeners;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 import org.eclipse.emf.common.command.Command;
@@ -49,8 +52,7 @@ import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
 
 /**
- * A Model Change Trigger that execute the
- * {@link CenterEdgeEndModelChangeOperation} when features defined in
+ * A Model Change Trigger that execute the {@link CenterEdgeEndModelChangeOperation} when features defined in
  * {@link RefreshEdgeLayoutScopePredicate} are updated.
  * 
  * @author Florian Barbin
@@ -60,26 +62,22 @@ public class EdgeLayoutUpdaterModelChangeTrigger implements ModelChangeTrigger {
     public static final int PRIORITY = FilterListener.COMPOSITE_FILTER_REFRESH_PRIORITY + 1;
 
     /**
-     * List of features for which we consider that the edge layout must be
-     * recompute.
+     * List of features for which we consider that the edge layout must be recompute.
      */
     private static final Set<EStructuralFeature> REFRESH_FEATURES = new HashSet<EStructuralFeature>();
 
     /**
-     * Sublist of <code>REFRESH_FEATURES</code> that also have other features as
-     * consequence.
+     * Sublist of <code>REFRESH_FEATURES</code> that also have other features as consequence.
      */
     private static final Set<EStructuralFeature> REFRESH_FEATURES_WITH_CONSEQUENCE = new HashSet<EStructuralFeature>();
 
     /**
-     * List of features that are standard consequences of
-     * <code>REFRESH_FEATURES_WITH_CONSEQUENCE</code>.
+     * List of features that are standard consequences of <code>REFRESH_FEATURES_WITH_CONSEQUENCE</code>.
      */
     private static final Set<EStructuralFeature> CONSEQUENCE_FEATURES = new HashSet<EStructuralFeature>();
 
     /**
-     * List of features concerning move or resize of the source/target of an
-     * edge.
+     * List of features concerning move or resize of the source/target of an edge.
      */
     private static final Set<EStructuralFeature> MOVE_OR_RESIZE_FEATURES = new HashSet<EStructuralFeature>();
 
@@ -109,8 +107,7 @@ public class EdgeLayoutUpdaterModelChangeTrigger implements ModelChangeTrigger {
     private RefreshEdgeLayoutNotificationFilter refreshEdgeLayoutNotificationFilter;
 
     /**
-     * Constructor. Add this EdgeLayoutUpdaterModelChangeTrigger to the session
-     * event broker of the given session.
+     * Constructor. Add this EdgeLayoutUpdaterModelChangeTrigger to the session event broker of the given session.
      * 
      * @param domain
      *            the editing domain.
@@ -130,6 +127,9 @@ public class EdgeLayoutUpdaterModelChangeTrigger implements ModelChangeTrigger {
     public Option<Command> localChangesAboutToCommit(Collection<Notification> notifications) {
         Command command = null;
 
+        Map<Notification, Edge> notifToEdge = new HashMap<>();
+        Map<Notification, Node> notifToNode = new HashMap<>();
+
         // this collection contains gmf edges for which we already created a
         // CenterEdgeEndModelChangeOperation. This list aims to avoid creating
         // multi operation for a same gmfEdge in the case we are several
@@ -140,13 +140,13 @@ public class EdgeLayoutUpdaterModelChangeTrigger implements ModelChangeTrigger {
             // Only consider notification of
             // RefreshEdgeLayoutNotificationFilter.REFRESH_FEATURES list and for
             // which the source or the target has not been moved
-            if (isRefreshEdgeLayoutNeededForNotification(notification, notifications)) {
-                Option<Edge> optionalGmfEdge = getCorrespondingEdge(notification);
-                if (optionalGmfEdge.some() && edgesWithCreatedCommand.add(optionalGmfEdge.get())) {
+            if (isRefreshEdgeLayoutNeededForNotification(notification, notifications, notifToEdge, notifToNode)) {
+                Optional<Edge> optionalGmfEdge = getCorrespondingEdge(notification, notifToEdge);
+                if (optionalGmfEdge.isPresent() && edgesWithCreatedCommand.add(optionalGmfEdge.get())) {
                     // if there are several notifications, we do not try to
                     // retrieve draw2D informations since they could be out of
                     // date.
-                    boolean useFigure = otherNotificationsAreConsequences(notification, optionalGmfEdge.get(), notifications);
+                    boolean useFigure = otherNotificationsAreConsequences(notification, optionalGmfEdge.get(), notifications, notifToEdge);
                     AbstractModelChangeOperation<Void> operation = new CenterEdgeEndModelChangeOperation(optionalGmfEdge.get(), useFigure);
                     operations.add(operation);
                 }
@@ -183,8 +183,7 @@ public class EdgeLayoutUpdaterModelChangeTrigger implements ModelChangeTrigger {
     }
 
     /**
-     * Dispose this trigger. The trigger is removed from the session event
-     * broker.
+     * Dispose this trigger. The trigger is removed from the session event broker.
      */
     public void dispose() {
         refreshEdgeLayoutNotificationFilter = null;
@@ -195,12 +194,10 @@ public class EdgeLayoutUpdaterModelChangeTrigger implements ModelChangeTrigger {
     }
 
     /**
-     * Test whether the other notifications are consequences of the given one.
-     * For instance, in case of a manual modification of the Sirius routing
-     * style (from Style tab of Properties view), we also update the GMF style
-     * and we add the routing style within the custom features. This method aims
-     * to check whether we are in the case of an individual modification or a
-     * global one.
+     * Test whether the other notifications are consequences of the given one. For instance, in case of a manual
+     * modification of the Sirius routing style (from Style tab of Properties view), we also update the GMF style and we
+     * add the routing style within the custom features. This method aims to check whether we are in the case of an
+     * individual modification or a global one.
      * 
      * @param notification
      *            the notification for which we are notified.
@@ -208,10 +205,11 @@ public class EdgeLayoutUpdaterModelChangeTrigger implements ModelChangeTrigger {
      *            the GMF edge associated to the <code>notification</code>
      * @param notifications
      *            the whole notification list.
-     * @return true if the notifications list contains only notifications
-     *         induced by the first one.
+     * @param notifToview
+     *            Map to retrieve the View if yet computed
+     * @return true if the notifications list contains only notifications induced by the first one.
      */
-    public boolean otherNotificationsAreConsequences(final Notification notification, final Edge gmfEdge, Collection<Notification> notifications) {
+    public boolean otherNotificationsAreConsequences(final Notification notification, final Edge gmfEdge, Collection<Notification> notifications, Map<Notification, Edge> notifToEdge) {
         boolean otherNotificationsAreIndirectlyConcerned = false;
         if (notifications.size() == 1 && REFRESH_FEATURES.contains(notifications.iterator().next().getFeature())) {
             otherNotificationsAreIndirectlyConcerned = true;
@@ -223,8 +221,8 @@ public class EdgeLayoutUpdaterModelChangeTrigger implements ModelChangeTrigger {
                     if (currentNotification == notification) {
                         considerAsConsequence = true;
                     } else {
-                        Option<Edge> optionalEdge = getCorrespondingEdge(currentNotification);
-                        if (optionalEdge.some()) {
+                        Optional<Edge> optionalEdge = getCorrespondingEdge(currentNotification, notifToEdge);
+                        if (optionalEdge.isPresent()) {
                             considerAsConsequence = optionalEdge.get().equals(gmfEdge) && CONSEQUENCE_FEATURES.contains(currentNotification.getFeature());
                         }
                     }
@@ -236,20 +234,23 @@ public class EdgeLayoutUpdaterModelChangeTrigger implements ModelChangeTrigger {
     }
 
     /**
-     * Test whether the edge centering should be refreshed for this
-     * notification.
+     * Test whether the edge centering should be refreshed for this notification.
      * 
      * @param notification
      *            The {@link Notification} to check.
      * @param notifications
      *            the whole notification list.
-     * @return true if this notification concerns the edge ends centering, false
-     *         otherwise.
+     * @param notifToEdge
+     *            Map to retrieve the Edge if yet computed
+     * @param notifToNode
+     *            Map to retrieve the Node if yet computed
+     * @return true if this notification concerns the edge ends centering, false otherwise.
      */
-    private boolean isRefreshEdgeLayoutNeededForNotification(final Notification notification, Collection<Notification> notifications) {
+    private boolean isRefreshEdgeLayoutNeededForNotification(final Notification notification, Collection<Notification> notifications, Map<Notification, Edge> notifToEdge,
+            Map<Notification, Node> notifToNode) {
         if (REFRESH_FEATURES.contains(notification.getFeature())) {
-            Option<Edge> optionalEdge = getCorrespondingEdge(notification);
-            if (optionalEdge.some()) {
+            Optional<Edge> optionalEdge = getCorrespondingEdge(notification, notifToEdge);
+            if (optionalEdge.isPresent()) {
                 final Edge referenceEdge = optionalEdge.get();
                 // Analyze other notifications to detect if the source or the
                 // target of the concerned edges has been moved. In this case we
@@ -262,8 +263,8 @@ public class EdgeLayoutUpdaterModelChangeTrigger implements ModelChangeTrigger {
                         if (currentNotification == notification) {
                             apply = true;
                         } else {
-                            Option<? extends View> optionalView = getCorrespondingView(currentNotification);
-                            if (optionalView.some() && optionalView.get() == referenceEdge.getSource() || optionalView.get() == referenceEdge.getTarget()) {
+                            Optional<? extends View> optionalView = getCorrespondingView(currentNotification, notifToEdge, notifToNode);
+                            if (optionalView.isPresent() && (optionalView.get() == referenceEdge.getSource() || optionalView.get() == referenceEdge.getTarget())) {
                                 // The notification concerns the source or the
                                 // target of the edge, return true only if the
                                 // notification does not concern a move or a
@@ -286,12 +287,16 @@ public class EdgeLayoutUpdaterModelChangeTrigger implements ModelChangeTrigger {
      * 
      * @param notification
      *            The {@link Notification} to analyze
+     * @param notifToEdge
+     *            Map to retrieve the Edge if yet computed
+     * @param notifToNode
+     *            Map to retrieve the Node if yet computed
      * @return an optional {@link View}
      */
-    private Option<? extends View> getCorrespondingView(Notification notification) {
-        Option<? extends View> result = getCorrespondingNode(notification);
-        if (!result.some()) {
-            result = getCorrespondingEdge(notification);
+    private Optional<? extends View> getCorrespondingView(Notification notification, Map<Notification, Edge> notifToEdge, Map<Notification, Node> notifToNode) {
+        Optional<? extends View> result = getCorrespondingNode(notification, notifToNode);
+        if (!result.isPresent()) {
+            result = getCorrespondingEdge(notification, notifToEdge);
         }
         return result;
     }
@@ -301,9 +306,15 @@ public class EdgeLayoutUpdaterModelChangeTrigger implements ModelChangeTrigger {
      * 
      * @param notification
      *            The {@link Notification} to analyze
+     * @param notifToEdge
+     *            Map to retrieve the edge if yet computed
      * @return an optional {@link Edge}
      */
-    private Option<Edge> getCorrespondingEdge(Notification notification) {
+    private Optional<Edge> getCorrespondingEdge(Notification notification, Map<Notification, Edge> notifToEdge) {
+        if (notifToEdge.containsKey(notification)) { // value may be null
+            return Optional.ofNullable(notifToEdge.get(notification));
+        }
+
         Edge gmfEdge = null;
         Object notifier = notification.getNotifier();
         if (notifier instanceof Edge) {
@@ -325,11 +336,9 @@ public class EdgeLayoutUpdaterModelChangeTrigger implements ModelChangeTrigger {
         } else if (notifier instanceof RelativeBendpoints) {
             gmfEdge = (Edge) ((RelativeBendpoints) notifier).eContainer();
         }
-        if (gmfEdge == null) {
-            return Options.newNone();
-        } else {
-            return Options.newSome(gmfEdge);
-        }
+        notifToEdge.put(notification, gmfEdge);
+
+        return Optional.ofNullable(gmfEdge);
     }
 
     /**
@@ -337,9 +346,15 @@ public class EdgeLayoutUpdaterModelChangeTrigger implements ModelChangeTrigger {
      * 
      * @param notification
      *            The {@link Notification} to analyze
+     * @param notifToNode
+     *            Map to retrieve the Node if yet computed
      * @return an optional {@link Node}
      */
-    private Option<Node> getCorrespondingNode(Notification notification) {
+    private Optional<Node> getCorrespondingNode(Notification notification, Map<Notification, Node> notifToNode) {
+        if (notifToNode.containsKey(notification)) { // value may be null
+            return Optional.ofNullable(notifToNode.get(notification));
+        }
+
         Node gmfNode = null;
         Object notifier = notification.getNotifier();
         if (notifier instanceof DNode) {
@@ -355,10 +370,8 @@ public class EdgeLayoutUpdaterModelChangeTrigger implements ModelChangeTrigger {
                 gmfNode = ((Node) container);
             }
         }
-        if (gmfNode == null) {
-            return Options.newNone();
-        } else {
-            return Options.newSome(gmfNode);
-        }
+        notifToNode.put(notification, gmfNode);
+
+        return Optional.ofNullable(gmfNode);
     }
 }
