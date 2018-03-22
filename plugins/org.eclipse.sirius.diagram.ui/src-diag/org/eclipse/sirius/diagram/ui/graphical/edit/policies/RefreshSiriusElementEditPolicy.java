efa1113e2c49e93214b376f63fbfdb32b1281853
Pierre-Charles David <pierre-charles.david@obeo.fr>
2018-03-22 17:22:35 +0100

    [cleanup] Simplify the code in RefreshSiriusElementEditPolicy
    Change-Id: I311810d05e1e6b54db008e2bfb9947074e4b465a
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/graphical/edit/policies/RefreshSiriusElementEditPolicy.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/graphical/edit/policies/RefreshSiriusElementEditPolicy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2010 THALES GLOBAL SERVICES.
+ * Copyright (c) 2007, 2018 THALES GLOBAL SERVICES and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -19,7 +19,6 @@ import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.editpolicies.AbstractEditPolicy;
 import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
-import org.eclipse.sirius.diagram.tools.api.command.IDiagramCommandFactory;
 import org.eclipse.sirius.diagram.tools.api.command.IDiagramCommandFactoryProvider;
 import org.eclipse.sirius.diagram.ui.tools.api.command.GMFCommandWrapper;
 import org.eclipse.sirius.diagram.ui.tools.api.editor.DDiagramEditor;
@@ -27,58 +26,39 @@ import org.eclipse.sirius.diagram.ui.tools.api.requests.RequestConstants;
 import org.eclipse.sirius.viewpoint.DRefreshable;
 
 /**
- * Edit policy that can return a refresh command on the Request
- * {@link RequestConstants#REQ_REFRESH_VIEWPOINT}.
+ * Edit policy that can return a refresh command on the Request {@link RequestConstants#REQ_REFRESH_VIEWPOINT}.
  * 
  * @author ymortier
  */
 public class RefreshSiriusElementEditPolicy extends AbstractEditPolicy {
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.gef.editpolicies.AbstractEditPolicy#understandsRequest(org.eclipse.gef.Request)
-     */
     @Override
-    public boolean understandsRequest(final Request req) {
-        return req.getType() != null && req.getType().equals(RequestConstants.REQ_REFRESH_VIEWPOINT);
+    public boolean understandsRequest(Request req) {
+        return req != null && RequestConstants.REQ_REFRESH_VIEWPOINT.equals(req.getType());
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.gef.editpolicies.AbstractEditPolicy#getCommand(org.eclipse.gef.Request)
-     */
     @Override
-    public Command getCommand(final Request request) {
-        if (request.getType() != null && request.getType().equals(RequestConstants.REQ_REFRESH_VIEWPOINT)) {
-            if (this.getHost() instanceof IGraphicalEditPart && getHost().isActive()) {
-                final IGraphicalEditPart gmfEditPart = (IGraphicalEditPart) this.getHost();
+    public Command getCommand(Request request) {
+        if (understandsRequest(request)) {
+            EditPart host = this.getHost();
+            if (host instanceof IGraphicalEditPart && host.isActive()) {
+                final IGraphicalEditPart gmfEditPart = (IGraphicalEditPart) host;
                 final EObject element = gmfEditPart.resolveSemanticElement();
                 if (element instanceof DRefreshable) {
-                    final DDiagramEditor diagramEditor = (DDiagramEditor) this.getHost().getViewer().getProperty(DDiagramEditor.EDITOR_ID);
-                    final Object adapter = diagramEditor.getAdapter(IDiagramCommandFactoryProvider.class);
-                    final IDiagramCommandFactoryProvider cmdFactoryProvider = (IDiagramCommandFactoryProvider) adapter;
-                    final TransactionalEditingDomain transactionalEditingDomain = TransactionUtil.getEditingDomain(element);
-                    final IDiagramCommandFactory emfCommandFactory = cmdFactoryProvider.getCommandFactory(transactionalEditingDomain);
-                    final DRefreshable refreshable = (DRefreshable) element;
-                    final org.eclipse.emf.common.command.Command cmd = emfCommandFactory.buildRefreshCommand(refreshable);
-                    final TransactionalEditingDomain editingDomain = ((IGraphicalEditPart) getHost()).getEditingDomain();
-                    return new ICommandProxy(new GMFCommandWrapper(editingDomain, cmd));
+                    DDiagramEditor diagramEditor = (DDiagramEditor) host.getViewer().getProperty(DDiagramEditor.EDITOR_ID);
+                    IDiagramCommandFactoryProvider cmdFactoryProvider = diagramEditor.getAdapter(IDiagramCommandFactoryProvider.class);
+                    TransactionalEditingDomain ted = TransactionUtil.getEditingDomain(element);
+                    org.eclipse.emf.common.command.Command cmd = cmdFactoryProvider.getCommandFactory(ted).buildRefreshCommand((DRefreshable) element);
+                    return new ICommandProxy(new GMFCommandWrapper(ted, cmd));
                 }
             }
         }
         return null;
     }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.gef.editpolicies.AbstractEditPolicy#getTargetEditPart(org.eclipse.gef.Request)
-     */
     @Override
     public EditPart getTargetEditPart(final Request request) {
-        if (request.getType() != null && request.getType().equals(RequestConstants.REQ_REFRESH_VIEWPOINT)) {
+        if (understandsRequest(request)) {
             return this.getHost();
         }
         return super.getTargetEditPart(request);
