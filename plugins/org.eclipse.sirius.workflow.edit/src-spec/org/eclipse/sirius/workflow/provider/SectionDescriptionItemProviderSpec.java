b82fdf585de90f18fa2c8b4599d778889d3d71c9
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2018-05-24 09:10:36 +0200

    [509735] Add support for the gap pattern generator in the workflow
    Bug: 509735
    Change-Id: Idcfe25720e79f8b4139f52f2878fcab2a0836c37
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

---
+++ plugins/org.eclipse.sirius.workflow.edit/src-spec/org/eclipse/sirius/workflow/provider/SectionDescriptionItemProviderSpec.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2018 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *
+ */
+package org.eclipse.sirius.workflow.provider;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.edit.command.CommandParameter;
+import org.eclipse.emf.edit.provider.StyledString;
+
+/**
+ * Subclass used to not have to modify the generated code.
+ *
+ * @author sbegaudeau
+ */
+public class SectionDescriptionItemProviderSpec extends SectionDescriptionItemProvider {
+
+    /**
+     * The constructor.
+     *
+     * @param adapterFactory
+     *            The adapter factory
+     */
+    public SectionDescriptionItemProviderSpec(AdapterFactory adapterFactory) {
+        super(adapterFactory);
+    }
+
+    @Override
+    public String getText(Object object) {
+        Object styledText = this.getStyledText(object);
+        if (styledText instanceof StyledString) {
+            return ((StyledString) styledText).getString();
+        }
+        return super.getText(object);
+    }
+
+    @Override
+    public Object getStyledText(Object object) {
+        return Utils.computeLabel(this, object, "_UI_SectionDescription_type"); //$NON-NLS-1$
+    }
+
+    @Override
+    protected CommandParameter createChildParameter(Object feature, Object child) {
+        Utils.addNoopNavigationOperations(child);
+        return super.createChildParameter(feature, child);
+    }
+}
