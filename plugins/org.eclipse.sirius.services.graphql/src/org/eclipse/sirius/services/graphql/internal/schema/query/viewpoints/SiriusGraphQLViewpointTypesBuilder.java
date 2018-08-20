0b0e707be116ec2449f6030c6a7de04bdfadce25
Pierre-Charles David <pierre-charles.david@obeo.fr>
2018-08-20 15:03:36 +0200

    [cleanup] Remove unused import
    Change-Id: If01329d492f6aa0dda9490182bc053e3d9af7e03
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.sirius.services.graphql/src/org/eclipse/sirius/services/graphql/internal/schema/query/viewpoints/SiriusGraphQLViewpointTypesBuilder.java
+++ plugins/org.eclipse.sirius.services.graphql/src/org/eclipse/sirius/services/graphql/internal/schema/query/viewpoints/SiriusGraphQLViewpointTypesBuilder.java
@@ -16,7 +16,6 @@ import static org.eclipse.sirius.services.graphql.internal.schema.query.viewpoin
 
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
