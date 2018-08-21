439351165960464c8623456e00ba5faff8fa3059
Pierre Guilet <pierre.guilet@obeo.fr>
2018-08-21 15:00:04 +0200

    [527109] Fix tests
    Bug: 527109
    Change-Id: Ied468facaf8acb10c7eb3bf380d4845b15e17f4a
    Signed-off-by: Pierre Guilet <pierre.guilet@obeo.fr>

--- plugins/org.eclipse.sirius.tests.swtbot/src/org/eclipse/sirius/tests/swtbot/tabbar/TabBarTest.java
+++ plugins/org.eclipse.sirius.tests.swtbot/src/org/eclipse/sirius/tests/swtbot/tabbar/TabBarTest.java
@@ -89,14 +89,14 @@ public class TabBarTest extends AbstractSiriusSwtBotGefTestCase {
 
     private static final String FILE_DIR = "/";
 
-    private static final String[] DIAGRAM_TOOLBARDROPDOWNBUTTONS_TOOLTIPS = { "Arrange All", "Select &All", "Layers", "Filters", Messages.PasteFormatAction_toolTipText,
-            Messages.DefaultModeAction_statusOn };
+    private static final String[] DIAGRAM_TOOLBARDROPDOWNBUTTONS_TOOLTIPS = { "Arrange All", "Select &All", "Layers", "Filters", Messages.DefaultModeAction_statusOn,
+            Messages.PasteFormatAction_toolTipText };
 
     private static final String[] DIAGRAM_TOOLBARBUTTONS_TOOLTIPS = { Messages.SiriusDiagramActionBarContributor_refreshDiagram, Messages.SelectHiddenElementsAction_tooltip,
             Messages.SelectPinnedElementsAction_tooltip, "Zoom In (Ctrl+=)", "Zoom Out (Ctrl+-)", Messages.SaveAsImageFileAction_label };
 
     private static final String[] CONTAINER_TOOLBARDROPDOWNBUTTONS_TOOLTIPS = { "Arrange Selection", "Align Left", DistributeAction.getTooltip(DistributeAction.GAPS_HORIZONTALLY), "Straighten to top",
-            "Font Color", "Fill &Color", "Li&ne Color", "Line Style" };
+            Messages.DefaultModeAction_statusOn, "Font Color", "Fill &Color", "Li&ne Color", "Line Style" };
 
     private static final String[] CONTAINER_TOOLBARBUTTONS_TOOLTIPS = { Messages.PinElementsEclipseAction_text, Messages.UnpinElementsEclipseAction_text, Messages.CopyFormatAction_toolTipText,
             Messages.SiriusDiagramActionBarContributor_hideElement, Messages.SiriusDiagramActionBarContributor_hideLabel, Messages.SiriusDiagramActionBarContributor_deleteFromDiagram,
@@ -413,12 +413,15 @@ public class TabBarTest extends AbstractSiriusSwtBotGefTestCase {
             }
 
             Predicate<Field> privateEnclosingClassAccessor = new Predicate<Field>() {
+
                 @Override
                 public boolean apply(Field input) {
                     return input.getType() == item.getClass().getEnclosingClass() && "this$0".equals(input.getName());
                 }
             };
+
             final Collection<Class<?>> acceptedNonDisposedTypes = Arrays.asList(IAction.class, Listener.class, IPropertyChangeListener.class);
+
             Predicate<Field> acceptedNonDisposedField = new Predicate<Field>() {
                 @Override
                 public boolean apply(Field input) {
