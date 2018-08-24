cafe4ee18279a95fb1c8c413d914965cc7e599b0
Pierre-Charles David <pierre-charles.david@obeo.fr>
2018-08-24 14:53:12 +0200

    [542839] Remove unused ElementInitializers interface
    It was internal, so no API change to document.
    
    Bug: 542839
    Change-Id: Ic9ae214717a8e176351b8f921cdfbd66aaa34055
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/internal/providers/SiriusElementTypes.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/internal/providers/SiriusElementTypes.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 THALES GLOBAL SERVICES.
+ * Copyright (c) 2007, 2019 THALES GLOBAL SERVICES and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -33,7 +33,7 @@ import org.eclipse.swt.graphics.Image;
 /**
  * @not-generated now that we have a new type, the dimension edge type
  */
-public class SiriusElementTypes extends ElementInitializers {
+public class SiriusElementTypes {
 
     /**
      * @was-generated
