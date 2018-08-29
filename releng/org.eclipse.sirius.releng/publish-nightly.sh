d1f6920804fb5f8fc5229a12615668b1f602214c
Laurent Redor <laurent.redor@obeo.fr>
2018-08-29 10:35:36 +0200

    [version] Bump version to v4.1.10
    Change-Id: If50b42ef2dc6e18c46f4961620ab773067e38081
    Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>

--- releng/org.eclipse.sirius.releng/publish-nightly.sh
+++ releng/org.eclipse.sirius.releng/publish-nightly.sh
@@ -32,7 +32,7 @@ provided by the Hudson build.
 set -e
 
 # The full version (should be taken as an argument)
-export VERSION="4.1.9"
+export VERSION="4.1.10"
 
 # The type of build being published
 export BUILD_TYPE="nightly"
