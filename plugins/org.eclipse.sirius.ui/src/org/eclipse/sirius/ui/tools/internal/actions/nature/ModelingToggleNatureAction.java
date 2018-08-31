7c2d48735031993ce3ee753d8e588ff058483f5c
Pierre-Charles David <pierre-charles.david@obeo.fr>
2018-08-31 09:49:22 +0200

    [cleanup] Remove unnecessary instanceof test
    Change-Id: I8f6928ff8d03ea6cc9d8243017f562191d279460
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/tools/internal/actions/nature/ModelingToggleNatureAction.java
+++ plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/tools/internal/actions/nature/ModelingToggleNatureAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2017 THALES GLOBAL SERVICES and others.
+ * Copyright (c) 2012, 2018 THALES GLOBAL SERVICES and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -61,7 +61,7 @@ public class ModelingToggleNatureAction extends AbstractHandler {
                 } else if (object instanceof IProjectNature) {
                     IProjectNature nature = (IProjectNature) object;
                     selectedProjects.add(nature.getProject());
-                } else if (Platform.getAdapterManager().getAdapter(object, IProject.class) instanceof IProject) {
+                } else if (Platform.getAdapterManager().getAdapter(object, IProject.class) != null) {
                     selectedProjects.add(Platform.getAdapterManager().getAdapter(object, IProject.class));
                 }
             }
