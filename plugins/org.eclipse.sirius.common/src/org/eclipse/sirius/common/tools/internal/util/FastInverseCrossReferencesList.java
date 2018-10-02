647bc013450ff647f4a7bd8b607589badf6f6f4a
Pierre-Charles David <pierre-charles.david@obeo.fr>
2018-10-02 10:02:49 +0200

    [527277] Remove "All rights reserved"
    This part has actually been removed from the standard header for
    EPLv2.
    
    Bug: 527277
    Change-Id: Id5fe822d9ca898524f22cf9af776f5384328abb1
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.sirius.common/src/org/eclipse/sirius/common/tools/internal/util/FastInverseCrossReferencesList.java
+++ plugins/org.eclipse.sirius.common/src/org/eclipse/sirius/common/tools/internal/util/FastInverseCrossReferencesList.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2005-2017 IBM Corporation, CEA, and others.
- * All rights reserved.   This program and the accompanying materials
+ * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * https://www.eclipse.org/legal/epl-2.0/
--- plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/business/api/descriptor/ComposedImageDescriptor.java
+++ plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/business/api/descriptor/ComposedImageDescriptor.java
@@ -2,7 +2,7 @@
  * <copyright> 
  *
  * Copyright (c) 2002-2007 IBM Corporation and others.
- * All rights reserved.   This program and the accompanying materials
+ * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * https://www.eclipse.org/legal/epl-2.0/
