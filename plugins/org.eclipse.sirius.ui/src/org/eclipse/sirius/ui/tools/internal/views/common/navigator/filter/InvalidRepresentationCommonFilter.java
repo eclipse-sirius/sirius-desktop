afe9a7ee58cb90eb51c2067e3b2f41f6d824dd7c
Laurent Fasani <laurent.fasani@obeo.fr>
2018-11-27 18:37:34 +0100

    [541734] Enhance the Model Explorer filter for invalid representations
    * The old "Representations known as dangling" filter is renamed into
    "Representations known as invalid".
    * It allows to filter invalid representations (dangling and not
    reachable representations)
    
    Bug: 541734
    Cherry-picked-from : 541513 commit
    df59f5237a6f39ba8162c57c206fbb6dcd121470
    Change-Id: I09a7e4c4c606208dbc4369a9eb88d0868f98823d
    Signed-off-by: Laurent Fasani <laurent.fasani@obeo.fr>

--- plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/tools/internal/views/common/navigator/filter/InvalidRepresentationCommonFilter.java
+++ plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/tools/internal/views/common/navigator/filter/InvalidRepresentationCommonFilter.java
@@ -1,11 +1,9 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2017 THALES GLOBAL SERVICES.
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License 2.0
+ * Copyright (c) 2014, 2018 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-2.0/
- *
- * SPDX-License-Identifier: EPL-2.0
+ * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
  *    Obeo - initial API and implementation
@@ -21,20 +19,24 @@ import org.eclipse.sirius.viewpoint.DRepresentation;
 import org.eclipse.sirius.viewpoint.DRepresentationDescriptor;
 
 /**
- * Filter to allow the user to hide {@link RepresentationItemImpl} or {@link DRepresentationDescriptor} if there is no
- * target semantic element or if this element no more belongs to a session.
+ * Filter to allow the user to hide {@link RepresentationItemImpl} or {@link DRepresentationDescriptor} if
+ * <ul>
+ * <li>there is no target semantic element or if this element no more belongs to a session.</li>
+ * <li>the representation can not be reached because it has been deleted or because the representationDescriptor.repPath
+ * is malformed (null or with a bad fragment or segment).</li>
+ * </ul>
  * 
  * @author mporhel
  * 
  */
-public class DanglingRepresentationCommonFilter extends ViewerFilter {
+public class InvalidRepresentationCommonFilter extends ViewerFilter {
 
     @Override
     public boolean select(Viewer viewer, Object parentElement, Object element) {
         boolean select = true;
         DRepresentationDescriptor repDesc = null;
         if (element instanceof DRepresentation) {
-            select = !new DRepresentationQuery((DRepresentation) element).isDanglingRepresentation();
+            repDesc = new DRepresentationQuery((DRepresentation) element).getRepresentationDescriptor();
         } else if (element instanceof DRepresentationDescriptor) {
             repDesc = (DRepresentationDescriptor) element;
         } else if (element instanceof RepresentationItemImpl) {
@@ -42,7 +44,7 @@ public class DanglingRepresentationCommonFilter extends ViewerFilter {
         }
 
         if (repDesc != null) {
-            select = !new DRepresentationDescriptorQuery(repDesc).isDangling();
+            select = new DRepresentationDescriptorQuery(repDesc).isRepresentationValid();
         }
 
         return select;
