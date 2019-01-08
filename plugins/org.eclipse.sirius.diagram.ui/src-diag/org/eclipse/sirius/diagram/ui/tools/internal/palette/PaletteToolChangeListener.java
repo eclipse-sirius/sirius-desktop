71a9adb2fb791d87b6cb97d066fbf6039e110cb4
Pierre-Charles David <pierre-charles.david@obeo.fr>
2019-01-08 09:34:43 +0100

    [cleanup] Add missing copyright header to PaletteToolChangeListener
    Change-Id: I427d57e9db00e18c919b0391f7418d7cdd3070f5
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/palette/PaletteToolChangeListener.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/palette/PaletteToolChangeListener.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2018-2019 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.sirius.diagram.ui.tools.internal.palette;
 
 import org.eclipse.gmf.runtime.notation.Diagram;
