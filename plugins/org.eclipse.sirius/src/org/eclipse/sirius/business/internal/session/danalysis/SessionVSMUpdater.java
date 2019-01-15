eclipse-sirius/sirius-desktop

commit 7b1d06e9c69f2b9ee5130c77132aeeacbdbf3c62

[cleanup] Extract method in SessionVSMUpdater
The refactoring does not improve anything, but by clarifying what the
code is trying to do it makes it clear that the current implementation
to find all the VSMs is rather dumb.

Change-Id: Ia94e4cb503f137acf555dfb9147b3cc0d9c9f085
Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

plugins/org.eclipse.sirius/src/org/eclipse/sirius/business/internal/session/danalysis/SessionVSMUpdater.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2018 THALES GLOBAL SERVICES and Obeo.
+ * Copyright (c) 2007, 2019 THALES GLOBAL SERVICES and Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -14,11 +14,12 @@ package org.eclipse.sirius.business.internal.session.danalysis;
 
 import java.io.IOException;
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.workspace.util.WorkspaceSynchronizer;
 import org.eclipse.sirius.business.api.componentization.ViewpointRegistryListener2;
@@ -30,7 +31,6 @@ import org.eclipse.sirius.viewpoint.Messages;
 import org.eclipse.sirius.viewpoint.SiriusPlugin;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Iterables;
 
 /**
  * Reload the VSMs used inside a session when the global registry detects their
@@ -53,8 +53,7 @@ public class SessionVSMUpdater implements ViewpointRegistryListener2 {
 
     @Override
     public void modelerDesciptionFilesLoaded() {
-        Collection<Resource> allResources = new ArrayList<Resource>(session.getTransactionalEditingDomain().getResourceSet().getResources());
-        for (Resource res : Iterables.filter(allResources, new ResourceFileExtensionPredicate(SiriusUtil.DESCRIPTION_MODEL_EXTENSION, false))) {
+        for (Resource res : findAllVSMResources(session)) {
             // Unload emtpy odesign.
             if (!res.isModified() && res.isLoaded() && res.getContents().isEmpty()) {
                 session.unregisterResourceInCrossReferencer(res);
@@ -92,5 +91,17 @@ public class SessionVSMUpdater implements ViewpointRegistryListener2 {
             }
         }
         session.notifyListeners(SessionListener.VSM_UPDATED);
+    }
+
+    /**
+     * Dumb implementation based on exhaustive search in the ResourceSet and file extension matching.
+     */
+    private static List<Resource> findAllVSMResources(DAnalysisSessionImpl session) {
+        // @formatter:off
+        EList<Resource> resources = session.getTransactionalEditingDomain().getResourceSet().getResources();
+        return resources.stream()
+                .filter(new ResourceFileExtensionPredicate(SiriusUtil.DESCRIPTION_MODEL_EXTENSION, true))
+                .collect(Collectors.toList());
+        // @formatter:on
     }
 }

commit 5558cd1c6ec5fabc58102bcc69b5512d8d07c27d

[fix] Revert change in SessionVSMUpdater
Commit 7b1d06e9c caused a regression: in the context the
SessionVSMUpdater, we can not reliably navigate inside the model as we
are in the process of reloading the VSMs, so their elements are
proxies. Revert to the previous code with an added comment about this.

Change-Id: Ib039c7c6e316733e9256f61ae535486aa8d33a8a
Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

plugins/org.eclipse.sirius/src/org/eclipse/sirius/business/internal/session/danalysis/SessionVSMUpdater.java
@@ -19,10 +19,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.workspace.util.WorkspaceSynchronizer;
 import org.eclipse.sirius.business.api.componentization.ViewpointRegistryListener2;
+import org.eclipse.sirius.business.api.helper.SiriusUtil;
 import org.eclipse.sirius.business.api.session.SessionListener;
+import org.eclipse.sirius.business.internal.migration.resource.ResourceFileExtensionPredicate;
 import org.eclipse.sirius.tools.api.interpreter.InterpreterRegistry;
 import org.eclipse.sirius.viewpoint.Messages;
 import org.eclipse.sirius.viewpoint.SiriusPlugin;
@@ -30,8 +33,7 @@ import org.eclipse.sirius.viewpoint.SiriusPlugin;
 import com.google.common.base.Preconditions;
 
 /**
- * Reload the VSMs used inside a session when the global registry detects their
- * content has changed.
+ * Reload the VSMs used inside a session when the global registry detects their content has changed.
  * 
  * @author pcdavid
  */
@@ -83,13 +85,14 @@ public class SessionVSMUpdater implements ViewpointRegistryListener2 {
         }
     }
 
+    /**
+     * Finds all the VSM resources in the session. Note that at the time we are called here, we are in the middle of
+     * reloading some of these VSMs, so the model elements they contain are "proxified", so we can not do a proper
+     * navigation in the models to find the resources and have to rely on checking the file extensions of the resources
+     * in the session.
+     */
     private static List<Resource> findAllVSMResources(DAnalysisSessionImpl session) {
-        // @formatter:off
-        return session.allAnalyses().stream()
-                      .flatMap(analysis -> analysis.getOwnedViews().stream())
-                      .map(view -> view.getViewpoint().eResource())
-                      .distinct()
-                      .collect(Collectors.toList());
-        // @formatter:on
+        EList<Resource> resources = session.getTransactionalEditingDomain().getResourceSet().getResources();
+        return resources.stream().filter(new ResourceFileExtensionPredicate(SiriusUtil.DESCRIPTION_MODEL_EXTENSION, true)).collect(Collectors.toList());
     }
 }