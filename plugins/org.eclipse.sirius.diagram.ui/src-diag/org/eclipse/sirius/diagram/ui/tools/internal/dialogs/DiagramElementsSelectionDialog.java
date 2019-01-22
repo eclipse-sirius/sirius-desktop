6f3a0c9d30d1a7d656efe9bb817c13debac28d2b
Pierre-Charles David <pierre-charles.david@obeo.fr>
2019-01-22 15:30:31 +0100

    [cleanup] Fix JDT warning on vararg method override
    Change-Id: I5c1409acf24a4d64bf4baea115c1eadf456279a3
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/dialogs/DiagramElementsSelectionDialog.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/dialogs/DiagramElementsSelectionDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2018 THALES GLOBAL SERVICES and others.
+ * Copyright (c) 2010, 2019 THALES GLOBAL SERVICES and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -169,7 +169,7 @@ public class DiagramElementsSelectionDialog {
         }
 
         @Override
-        public void setInitialSelections(Object[] selectedElements) {
+        public void setInitialSelections(Object... selectedElements) {
             setInitialElementSelections(Lists.newArrayList(selectedElements));
         }
 
