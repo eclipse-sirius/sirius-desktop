f9d9a213a6b20cf8a8b25adc4b004b8c7c5c3a25
Guillaume Coutable <guillaume.coutable@obeo.fr>
2019-01-23 10:26:13 +0100

    [543902] Allow to execute CustomWidget model operation
    Handle the additional CustomOperation held by the CustomWidget model
    operation
    
    Bug: 543902
    Change-Id: I7602363b51ec42bba512b311536ba69463823e4a
    Cherry-picked-from: 537135
    Signed-off-by: Guillaume Coutable <guillaume.coutable@obeo.fr>

--- plugins/org.eclipse.sirius.properties.core/src/org/eclipse/sirius/properties/core/internal/preprocessor/CustomDescriptionPreprocessor.java
+++ plugins/org.eclipse.sirius.properties.core/src/org/eclipse/sirius/properties/core/internal/preprocessor/CustomDescriptionPreprocessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Obeo.
+ * Copyright (c) 2017, 2019 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -12,13 +12,24 @@
  *******************************************************************************/
 package org.eclipse.sirius.properties.core.internal.preprocessor;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.sirius.common.interpreter.api.IInterpreter;
+import org.eclipse.sirius.common.interpreter.api.IVariableManager;
 import org.eclipse.sirius.properties.CustomDescription;
+import org.eclipse.sirius.properties.CustomOperation;
+import org.eclipse.sirius.properties.PropertiesFactory;
 import org.eclipse.sirius.properties.PropertiesPackage;
 import org.eclipse.sirius.properties.core.api.DefaultDescriptionPreprocessorWithFiltering;
+import org.eclipse.sirius.properties.core.api.OverridesProvider;
 import org.eclipse.sirius.properties.core.api.PreconfiguredPreprocessorUtils;
+import org.eclipse.sirius.properties.core.api.TransformationCache;
+import org.eclipse.sirius.properties.core.internal.converter.SiriusInitialOperationAdapter;
+import org.eclipse.sirius.viewpoint.description.tool.InitialOperation;
 
 /**
  * Preprocessor for {@link CustomDescription}.
@@ -45,4 +56,28 @@ public class CustomDescriptionPreprocessor extends DefaultDescriptionPreprocesso
         featuresToCopy.add(PropertiesPackage.Literals.ABSTRACT_CUSTOM_DESCRIPTION__CUSTOM_OPERATIONS);
         return featuresToCopy;
     }
+    
+    @Override
+    protected void processManyValuedFeatureByCopying(EStructuralFeature manyValuedFeature, CustomDescription processedDescription, CustomDescription currentDescription, TransformationCache cache,
+            IInterpreter interpreter, IVariableManager variableManager, OverridesProvider overridesProvider) {
+        if (PropertiesPackage.eINSTANCE.getAbstractCustomDescription_CustomOperations().equals(manyValuedFeature)) {
+            List<CustomOperation> transformedCustomOperations = new ArrayList<>();
+            
+            currentDescription.getCustomOperations().forEach(customOperation -> {
+                CustomOperation processedCustomOperation = PropertiesFactory.eINSTANCE.createCustomOperation();
+                processedCustomOperation.setName(customOperation.getName());
+                processedCustomOperation.setDocumentation(customOperation.getDocumentation());
+                
+                InitialOperation processedInitialOperation = EcoreUtil.copy(customOperation.getInitialOperation());
+                processedInitialOperation.eAdapters().add(new SiriusInitialOperationAdapter(EcoreUtil.getURI(customOperation.getInitialOperation())));
+                processedCustomOperation.setInitialOperation(processedInitialOperation);
+                transformedCustomOperations.add(processedCustomOperation);
+            });
+            
+            processedDescription.getCustomOperations().addAll(transformedCustomOperations);
+        } else {
+            super.processManyValuedFeatureByCopying(manyValuedFeature, processedDescription, currentDescription, cache, interpreter, variableManager, overridesProvider);
+        }
+    }
+    
 }
