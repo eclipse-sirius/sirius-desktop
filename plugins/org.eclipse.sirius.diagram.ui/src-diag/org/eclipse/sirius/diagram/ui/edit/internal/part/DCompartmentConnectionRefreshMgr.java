b4cb16ea16567db32567e07f522db5377baca398
Laurent Redor <laurent.redor@obeo.fr>
2019-03-19 16:20:56 +0100

    [545533] Pb of edge visibility with collapsed compartment
    By default, GMF does not consider edges on ContainerEditPart, parent of
    the ShapeCompartmentEditPart, during the refreshConnections mechanism of
    the ShapeCompartmentEditPart. But in Sirius, it is possible to have
    such.
    This commit adds the edges pointing to ContainerEditPart (as source or
    as target, parent of the ShapeCompartmentEditPart, in the
    refreshConnections mechanism.
    
    Bug: 545533
    Change-Id: I18054275645e4a38c7858c8c6857ce84c68c767b
    Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/edit/internal/part/DCompartmentConnectionRefreshMgr.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/edit/internal/part/DCompartmentConnectionRefreshMgr.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 THALES GLOBAL SERVICES.
+ * Copyright (c) 2010, 2019 THALES GLOBAL SERVICES.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -12,12 +12,21 @@
  *******************************************************************************/
 package org.eclipse.sirius.diagram.ui.edit.internal.part;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.draw2d.Connection;
 import org.eclipse.gef.ConnectionEditPart;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.ConnectionNodeEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeCompartmentEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeCompartmentEditPart.ConnectionRefreshMgr;
+import org.eclipse.gmf.runtime.notation.Diagram;
+import org.eclipse.gmf.runtime.notation.Edge;
+import org.eclipse.gmf.runtime.notation.View;
 
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
@@ -32,6 +41,7 @@ import com.google.common.collect.Sets;
 public class DCompartmentConnectionRefreshMgr extends ConnectionRefreshMgr {
 
     private Predicate safeConnection = new Predicate() {
+        @Override
         public boolean apply(Object input) {
             boolean selected = true;
             if (input instanceof ConnectionEditPart) {
@@ -49,11 +59,56 @@ public class DCompartmentConnectionRefreshMgr extends ConnectionRefreshMgr {
     };
 
     /**
+     * Return the set of {@link ConnectionNodeEditPart}s contained in the supplied shape compartment and in the parent
+     * edit part.<BR>
+     * This method returns the same result as {@link ConnectionRefreshMgr#getConnectionNodes(ShapeCompartmentEditPart)}
+     * but it also considers edges having the parent of the <code>scep</code> as source or target.<BR>
+     * The modified code is only the addition of the condition <code>endPoint.equals(scep.getParent())</code> to the 2
+     * statements <code>if (isChildOf(scep, endPoint))</code>.
+     * 
+     * @param scep
+     *            a shape compartment.
+     * @return a {@link Set} of {@link ConnectionNodeEditPart}.
+     */
+
+    protected Set getSpecificConnectionNodes(ShapeCompartmentEditPart scep) {
+        Set<Object> endPoints = new HashSet<>();
+        Object modelObject = scep.getModel();
+        if (scep.getViewer() != null && modelObject instanceof View && ((View) modelObject).getDiagram() != null) {
+            Diagram diagram = ((View) modelObject).getDiagram();
+            Map registry = scep.getViewer().getEditPartRegistry();
+            List edges = diagram.getEdges();
+            Iterator edgesIterator = edges.iterator();
+
+            while (edgesIterator.hasNext()) {
+                Edge edge = (Edge) edgesIterator.next();
+                EditPart endPoint = (EditPart) registry.get(edge.getSource());
+                if (isChildOf(scep, endPoint) || (endPoint != null && endPoint.equals(scep.getParent()))) {
+                    Object cep = registry.get(edge);
+                    if (cep != null) {
+                        endPoints.add(cep);
+                    }
+                    continue;
+                }
+                endPoint = (EditPart) registry.get(edge.getTarget());
+                if (isChildOf(scep, endPoint) || (endPoint != null && endPoint.equals(scep.getParent()))) {
+                    Object cep = registry.get(edge);
+                    if (cep != null) {
+                        endPoints.add(cep);
+                    }
+                }
+            }
+        }
+        return endPoints;
+    }
+
+    /**
      * {@inheritDoc}
      */
     @Override
     protected Set getConnectionNodes(ShapeCompartmentEditPart scep) {
-        Set<?> connectionsNodes = super.getConnectionNodes(scep);
+        Set<?> connectionsNodes = getSpecificConnectionNodes(scep);
+
         if (connectionsNodes != null) {
             Iterable<?> filteredConnectionNodes = Iterables.filter(connectionsNodes, safeConnection);
             return Sets.newHashSet(filteredConnectionNodes);
