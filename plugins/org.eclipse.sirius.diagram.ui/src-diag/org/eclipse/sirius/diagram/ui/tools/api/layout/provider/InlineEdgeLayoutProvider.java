3e89e5b581599a856a5d5d2aaf7f25afee34733a
Pierre-Charles David <pierre-charles.david@obeo.fr>
2019-12-11 16:00:10 +0100

    [fix] Fix infinite loop
    Change-Id: Iee383e0eda669e29805fbf6faf832947379f7e1a
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/api/layout/provider/InlineEdgeLayoutProvider.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/api/layout/provider/InlineEdgeLayoutProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2012 THALES GLOBAL SERVICES.
+ * Copyright (c) 2007, 2020 THALES GLOBAL SERVICES and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -833,6 +833,7 @@ public class InlineEdgeLayoutProvider extends DefaultLayoutProvider {
     private void horizontalAlign(final AbstractEdgeViewOrdering viewOrdering) {
         boolean again = true;
         while (again) {
+            again = false;
             final Iterator iterMoveDescriptors = this.connectionsToMoveEdgeDescriptor.values().iterator();
             while (iterMoveDescriptors.hasNext()) {
                 final MoveEdgeDescriptor currentDescriptor = (MoveEdgeDescriptor) iterMoveDescriptors.next();
