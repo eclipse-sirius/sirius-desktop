401a627c34cbcfb0b8994def0b1a4a3dee66bc35
Pierre-Charles David <pierre-charles.david@obeo.fr>
2020-07-03 12:31:40 +0200

    [releng] Upgrade to Tycho 2.0.0
    This version needs, at minimal version, Java 11 and Maven 3.6.3.
    
    Change-Id: If4e285c50966aa494e1a74353b9ff5660d4b18d9
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ readonly TESTS_SUITES=${3:-junit,swtbot-sequence,swtbot}
 readonly BUILD_POM="packaging/org.eclipse.sirius.parent/pom.xml"
 readonly USE_P2_MIRRORS="true"
 
-SWT_GTK3=0 mvn -Dplatform-version-name="$PLATFORM" -Declipse.p2.mirrors="$USE_P2_MIRRORS" -f "$BUILD_POM" -P full,headless,headless-server,"$TESTS_SUITES" clean "$GOAL"
+mvn -Dplatform-version-name="$PLATFORM" -Declipse.p2.mirrors="$USE_P2_MIRRORS" -f "$BUILD_POM" -P full,"$TESTS_SUITES" clean "$GOAL"
