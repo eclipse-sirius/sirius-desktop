c60c82c668eba963572a79b80ea70943e343eaa2
Laurent Redor <laurent.redor@obeo.fr>
2020-08-19 10:45:49 +0200

    [566186] Fix NPE when using palette customization
    This NPE exists since Sirius 6.0.0 and the addition of "Generic
    Connection Creation Tool" (bugzilla 528002 [1]) as default tool.
    
    In PaletteManagerImpl.addGenericConnectionTool() the tool entry was
    added using the wrong API:
    > container.getChildren().add(paletteEntry);
    instead of
    > container.add(paletteEntry);
    
    The last also sets the parent field of paletteEntry.
    
    [1] https://bugs.eclipse.org/bugs/show_bug.cgi?id=528002
    
    Bug: 566186
    Change-Id: I9471a57be9fb0e8e6d718b6b43096939ca624f65
    Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/palette/PaletteManagerImpl.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/palette/PaletteManagerImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2018 THALES GLOBAL SERVICES and others.
+ * Copyright (c) 2010, 2020 THALES GLOBAL SERVICES and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -245,7 +245,7 @@ public class PaletteManagerImpl implements PaletteManager {
         paletteEntry.setSmallIcon(descIcon);
         paletteEntry.setLargeIcon(descIcon);
         final PaletteContainer container = paletteRoot.getDefaultEntry().getParent();
-        container.getChildren().add(paletteEntry);
+        container.add(paletteEntry);
     }
 
     /**
