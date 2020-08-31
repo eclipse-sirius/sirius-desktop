6a3150a1c6fe725e3e62c970882454034c8722dd
Pierre-Charles David <pierre-charles.david@obeo.fr>
2020-08-31 09:19:55 +0200

    [version] Bump version to 6.3.4
    Change-Id: I68aa9ddd7f3f97898c44e4b53adb8ef177de45e4
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- releng/org.eclipse.sirius.releng/publish-nightly-jiro.sh
+++ releng/org.eclipse.sirius.releng/publish-nightly-jiro.sh
@@ -35,7 +35,7 @@ provided by the Jenkins build.
 set -e
 
 # The full version (should be taken as an argument)
-export VERSION="6.3.3"
+export VERSION="6.3.4"
 
 # The type of build being published
 export BUILD_TYPE="nightly"
