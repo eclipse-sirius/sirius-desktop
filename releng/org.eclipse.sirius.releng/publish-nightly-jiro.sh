286d7d31c3d19bf39c607e39354e69e6ab847b80
Pierre-Charles David <pierre-charles.david@obeo.fr>
2020-10-12 09:55:39 +0200

    [version] Bump version to 6.3.5
    Change-Id: Ibd7c5d27e1648638c658a2fca09ce18766a65889
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- releng/org.eclipse.sirius.releng/publish-nightly-jiro.sh
+++ releng/org.eclipse.sirius.releng/publish-nightly-jiro.sh
@@ -35,7 +35,7 @@ provided by the Jenkins build.
 set -e
 
 # The full version (should be taken as an argument)
-export VERSION="6.3.4"
+export VERSION="6.3.5"
 
 # The type of build being published
 export BUILD_TYPE="nightly"
--- releng/org.eclipse.sirius.releng/publish-nightly.sh
+++ releng/org.eclipse.sirius.releng/publish-nightly.sh
@@ -32,7 +32,7 @@ provided by the Hudson build.
 set -e
 
 # The full version (should be taken as an argument)
-export VERSION="6.3.4"
+export VERSION="6.3.5"
 
 # The type of build being published
 export BUILD_TYPE="nightly"
