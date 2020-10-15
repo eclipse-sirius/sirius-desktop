edc8fd8cf20909dcba85312899d7e5c731015167
Nathalie Lepine <nathalie.lepine@obeo.fr>
2020-10-15 16:47:35 +0200

    [571400] Add a cache for ISequenceEvent.getLifeline
    Bug: 571400
    Cherry-picked-from: 567517
    Change-Id: I8bf3ca2ba6c018aeb48caae85b8a94101aa5acab
    Signed-off-by: Nathalie Lepine <nathalie.lepine@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.sequence/src/org/eclipse/sirius/diagram/sequence/business/internal/elements/AbstractSequenceNode.java
+++ plugins/org.eclipse.sirius.diagram.sequence/src/org/eclipse/sirius/diagram/sequence/business/internal/elements/AbstractSequenceNode.java
@@ -27,6 +27,9 @@ import org.eclipse.sirius.ext.base.Options;
  * @author mporhel, pcdavid
  */
 abstract class AbstractSequenceNode extends AbstractSequenceElement implements ISequenceNode {
+
+    protected Option<Lifeline> cachedLifeline;
+
     /**
      * Constructor.
      * 
@@ -77,7 +80,14 @@ abstract class AbstractSequenceNode extends AbstractSequenceElement implements I
 
     @Override
     public Option<Lifeline> getLifeline() {
-        return getParentLifeline();
+        Option<Lifeline> result;
+        if (cachedLifeline != null) {
+            result = cachedLifeline;
+        } else {
+            result = getParentLifeline();
+            cachedLifeline = result;
+        }
+        return result;
     }
 
     /**
