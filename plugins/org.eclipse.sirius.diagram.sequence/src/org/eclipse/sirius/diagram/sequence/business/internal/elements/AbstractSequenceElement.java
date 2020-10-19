54b85109d5d3f3e7700ebd7f95fe15bad5ab7909
Maxime Porhel <maxime.porhel@obeo.fr>
2020-10-19 10:43:45 +0200

    [571400] Reorganize ISequenceElement implementations
    - Remove duplicated code between Execution/State/EndOfLife which is
    already in their superclass.
    - Reorder methods in Execution and State to ease the comparison.
    - Move getParentLifeline() from AbstractSequenceElement to
    AbstractSequenceNode : the other sequence elements will not retrieve the
    lifeline in their parent views.
    
    Bug: 571400
    Cherry-picked-from: 567517
    Change-Id: I9135795e92be2fc7bb1333d4bdf93611de1da3ec
    Signed-off-by: Maxime Porhel <maxime.porhel@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.sequence/src/org/eclipse/sirius/diagram/sequence/business/internal/elements/AbstractSequenceElement.java
+++ plugins/org.eclipse.sirius.diagram.sequence/src/org/eclipse/sirius/diagram/sequence/business/internal/elements/AbstractSequenceElement.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2015 THALES GLOBAL SERVICES and others.
+ * Copyright (c) 2010, 2021 THALES GLOBAL SERVICES and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -47,9 +47,8 @@ public abstract class AbstractSequenceElement extends AdapterImpl implements ISe
     }
 
     /**
-     * Generic test function used by all the more specific predicates: checks
-     * that a diagram element is valid, is part of a sequence diagram, and has
-     * the specified type of mapping (or a sub-type).
+     * Generic test function used by all the more specific predicates: checks that a diagram element is valid, is part
+     * of a sequence diagram, and has the specified type of mapping (or a sub-type).
      * 
      * This method handle edge mapping imports.
      * 
@@ -112,25 +111,6 @@ public abstract class AbstractSequenceElement extends AdapterImpl implements ISe
         } else {
             return Options.newNone();
         }
-    }
-
-    /**
-     * Tries to find a lifeline among the ancestors of this element (including
-     * the element itself).
-     * 
-     * @return option on the parent lifeline of this sequenceElement
-     */
-    protected Option<Lifeline> getParentLifeline() {
-        View current = view;
-        do {
-            Option<Lifeline> lifeline = ISequenceElementAccessor.getLifeline(current);
-            if (lifeline.some()) {
-                return lifeline;
-            } else {
-                current = (View) current.eContainer();
-            }
-        } while (current != null && !(current instanceof Diagram));
-        return Options.newNone();
     }
 
     @Override
--- plugins/org.eclipse.sirius.diagram.sequence/src/org/eclipse/sirius/diagram/sequence/business/internal/elements/EndOfLife.java
+++ plugins/org.eclipse.sirius.diagram.sequence/src/org/eclipse/sirius/diagram/sequence/business/internal/elements/EndOfLife.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2015 THALES GLOBAL SERVICES and others.
+ * Copyright (c) 2010, 2021 THALES GLOBAL SERVICES and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -28,11 +28,9 @@ import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
 
 /**
- * Represents the EndOfLife marker which can appear at the bottom of a lifeline.
- * This element can be present even in the case where the lifeline is not
- * explicitly destroyed by a destruction message. In that case, it is used as a
- * convenience to allow the user to resize the lifeline vertically by dragging
- * the EndOfLife marker.
+ * Represents the EndOfLife marker which can appear at the bottom of a lifeline. This element can be present even in the
+ * case where the lifeline is not explicitly destroyed by a destruction message. In that case, it is used as a
+ * convenience to allow the user to resize the lifeline vertically by dragging the EndOfLife marker.
  * 
  * @author mporhel, pcdavid
  */
@@ -45,8 +43,7 @@ public class EndOfLife extends AbstractSequenceNode {
     public static final int VISUAL_ID = 3001;
 
     /**
-     * Predicate to check whether a Sirius DDiagramElement represents an
-     * EndOfLife.
+     * Predicate to check whether a Sirius DDiagramElement represents an EndOfLife.
      */
     private enum SiriusElementPredicate implements Predicate<DDiagramElement> {
         INSTANCE;
@@ -78,19 +75,12 @@ public class EndOfLife extends AbstractSequenceNode {
     }
 
     /**
-     * Returns a predicate to check whether a Sirius DDiagramElement represents
-     * an EndOfLife.
+     * Returns a predicate to check whether a Sirius DDiagramElement represents an EndOfLife.
      * 
-     * @return a predicate to check whether a Sirius DDiagramElement represents
-     *         an EndOfLife.
+     * @return a predicate to check whether a Sirius DDiagramElement represents an EndOfLife.
      */
     public static Predicate<DDiagramElement> viewpointElementPredicate() {
         return SiriusElementPredicate.INSTANCE;
-    }
-
-    @Override
-    public Option<Lifeline> getLifeline() {
-        return getParentLifeline();
     }
 
     /**
@@ -123,11 +113,10 @@ public class EndOfLife extends AbstractSequenceNode {
     }
 
     /**
-     * Tests whether this EOL marker (and thus the associated lifeline) is
-     * explicitly destroyed by a destruction message.
+     * Tests whether this EOL marker (and thus the associated lifeline) is explicitly destroyed by a destruction
+     * message.
      * 
-     * @return <code>true</code> if this EOL marker is the target of a
-     *         destruction message.
+     * @return <code>true</code> if this EOL marker is the target of a destruction message.
      */
     public boolean isExplicitelyDestroyed() {
         return getDestructionMessage().some();
