0895696684babe0efd8f291b144c2914aa0dc636
Laurent Redor <laurent.redor@obeo.fr>
2021-03-04 09:27:16 +0100

    [571115] Blank regeneration of the VSM table editor
    In this commit, the copyright has been changed in table.genmodel,
    because when Pierre-Charles modified it in commit efc8b2ad [1], he has
    partially changed the line ending format. In consequence, the format is
    wrong when the generation is done on Windows.
    Furthermore, the regen of editor has not been done after the
    modification of the Copyright by Pierre in commit 75ddffc0 [2].
    
    * Regen done with Acceleo 2.8.0
    * Regen done without modification in MM
    * 2 clean-up have been launched after regen
    
    [1] https://git.eclipse.org/c/sirius/org.eclipse.sirius.git/commit/?id=efc8b2ad33e3463ec313769e7d2823e928db35da
    [2] https://git.eclipse.org/c/sirius/org.eclipse.sirius.git/commit/?id=75ddffc002cd3db43dd1808d5f4385616a242cfe
    
    Bug: 571115
    Change-Id: I70e0129e57681a9d704f33a50d6d4b25e6a093b3
    Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>

--- plugins/org.eclipse.sirius.editor.table/src-gen/org/eclipse/sirius/table/editor/properties/sections/description/linemapping/LineMappingDomainClassPropertySection.java
+++ plugins/org.eclipse.sirius.editor.table/src-gen/org/eclipse/sirius/table/editor/properties/sections/description/linemapping/LineMappingDomainClassPropertySection.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2018 THALES GLOBAL SERVICES.
+ * Copyright (c) 2007, 2021 THALES GLOBAL SERVICES.
  * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License 2.0
+ * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * https://www.eclipse.org/legal/epl-2.0/
  *
