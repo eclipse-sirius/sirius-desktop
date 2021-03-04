f8041c506857b986a5300a36b6059b6be3db34d4
Laurent Redor <laurent.redor@obeo.fr>
2021-03-04 09:39:51 +0100

    [571115] Regeneration of the VSM table editor
    * Regen done with Acceleo 2.8.0
    * 2 clean-up has been launched after regen
    
    Bug: 571115
    Change-Id: Id20dc5d65ad055220a43691d6fae3411d612b14f
    Signed-off-by: Laurent Redor <laurent.redor@obeo.fr>

---
+++ plugins/org.eclipse.sirius.editor.table/src-gen/org/eclipse/sirius/table/editor/properties/filters/description/celleditortool/CellEditorToolQualifiedClassNameFilter.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2021 THALES GLOBAL SERVICES.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.table.editor.properties.filters.description.celleditortool;
+
+// Start of user code specific imports
+
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.sirius.editor.properties.filters.common.ViewpointPropertyFilter;
+import org.eclipse.sirius.table.metamodel.table.description.DescriptionPackage;
+
+// End of user code specific imports
+
+/**
+ * A filter for the qualifiedClassName property section.
+ */
+public class CellEditorToolQualifiedClassNameFilter extends ViewpointPropertyFilter {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected EStructuralFeature getFeature() {
+        return DescriptionPackage.eINSTANCE.getCellEditorTool_QualifiedClassName();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected boolean isRightInputType(Object arg0) {
+        return arg0 instanceof org.eclipse.sirius.table.metamodel.table.description.CellEditorTool;
+    }
+
+    // Start of user code user methods
+
+    // End of user code user methods
+
+}
