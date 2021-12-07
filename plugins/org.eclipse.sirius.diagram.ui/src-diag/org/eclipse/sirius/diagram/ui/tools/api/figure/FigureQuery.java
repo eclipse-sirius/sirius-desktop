321a766810e2c27e7a1a2ced155568535c431b12
Steve Monnier <steve.monnier@obeo.fr>
2021-12-06 16:43:50 -0800

    [577162] Add query that converts LabelViewConstant to VisualID
    Addition of a query that link the LabelViewConstant used in edge label
    figures to the VisualID of edge label edit parts.
    
    Bug: 577162
    Change-Id: I5301460f1ce722b9ef3d9b3d1f072f4802257e37
    Signed-off-by: Steve Monnier <steve.monnier@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/api/figure/FigureQuery.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/api/figure/FigureQuery.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 THALES GLOBAL SERVICES.
+ * Copyright (c) 2011, 2021 THALES GLOBAL SERVICES.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -14,7 +14,11 @@ package org.eclipse.sirius.diagram.ui.tools.api.figure;
 
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Label;
+import org.eclipse.gmf.runtime.diagram.ui.internal.util.LabelViewConstants;
 import org.eclipse.gmf.runtime.draw2d.ui.figures.WrapLabel;
+import org.eclipse.sirius.diagram.ui.internal.edit.parts.DEdgeBeginNameEditPart;
+import org.eclipse.sirius.diagram.ui.internal.edit.parts.DEdgeEndNameEditPart;
+import org.eclipse.sirius.diagram.ui.internal.edit.parts.DEdgeNameEditPart;
 import org.eclipse.sirius.ext.base.Option;
 import org.eclipse.sirius.ext.base.Options;
 import org.eclipse.sirius.ext.gmf.runtime.gef.ui.figures.SiriusWrapLabel;
@@ -84,6 +88,31 @@ public class FigureQuery {
             } else if (labelFigure.get() instanceof Label) {
                 result = Options.newSome(((Label) labelFigure.get()).getText());
             }
+        }
+        return result;
+    }
+
+    /**
+     * Convert the LabelViewConstant value to the equivalent EditPart VisualID.
+     * 
+     * @param labelViewConstant
+     *            the value to convert
+     * @return the conversion to VisualID
+     */
+    public int edgeLabelViewConstantToVisualID(int labelViewConstant) {
+        int result = -1;
+        switch (labelViewConstant) {
+        case LabelViewConstants.SOURCE_LOCATION:
+            result = DEdgeBeginNameEditPart.VISUAL_ID;
+            break;
+        case LabelViewConstants.MIDDLE_LOCATION:
+            result = DEdgeNameEditPart.VISUAL_ID;
+            break;
+        case LabelViewConstants.TARGET_LOCATION:
+            result = DEdgeEndNameEditPart.VISUAL_ID;
+            break;
+        default:
+            break;
         }
         return result;
     }
