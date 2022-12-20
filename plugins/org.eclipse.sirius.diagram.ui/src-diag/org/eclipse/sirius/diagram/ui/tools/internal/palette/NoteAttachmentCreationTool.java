a4c0ae1f8c5147cbdf350d18b271f3464eb42397
Laurent Fasani <laurent.fasani@obeo.fr>
2022-12-20 15:48:34 +0100

    [581251] Avoid infinite loop when creating a NoteAttachment
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=581251
    Change-Id: I00aa71ea744599496e95c5ddb0720b8460f42879

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/palette/NoteAttachmentCreationTool.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/palette/NoteAttachmentCreationTool.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 THALES GLOBAL SERVICES.
+ * Copyright (c) 2010, 2022 THALES GLOBAL SERVICES.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -16,8 +16,8 @@ import org.eclipse.gmf.runtime.diagram.ui.tools.ConnectionCreationTool;
 import org.eclipse.gmf.runtime.emf.type.core.IElementType;
 
 /**
- * Specific ConnectionCreationTool to have the same behavior as usual in
- * Sirius (2 clicks for link creation instead of one click with drag).
+ * Specific ConnectionCreationTool to have the same behavior as usual in Sirius (2 clicks for link creation instead of
+ * one click with drag).
  * 
  * @author <a href="mailto:laurent.redor@obeo.fr">Laurent Redor</a>
  * 
@@ -54,5 +54,20 @@ public class NoteAttachmentCreationTool extends ConnectionCreationTool {
             handleFinished();
         }
         return true;
+    }
+
+    /**
+     * Overridden to make sure the exposeHelper is reset to null. <br/>
+     * Otherwise QueuedAutoexpose is rescheduled from setAutoexposeHelper creating an infinite loop in
+     * org.eclipse.gef.tools.TargetingTool.doAutoexpose()<br/>
+     * This happens if the user has clicked near the border of the diagram. In this case, exposeHelper is set with
+     * SiriusScroll and the NoteCreation is possible. So when the creation is done QueuedAutoexpose is executed in loop.
+     * 
+     * @see org.eclipse.gmf.runtime.diagram.ui.tools.ConnectionCreationTool#deactivate()
+     */
+    @Override
+    protected boolean handleCreateConnection() {
+        setAutoexposeHelper(null);
+        return super.handleCreateConnection();
     }
 }
