dee34dd814a54f279c2a40753597d357d169acf5
Laurent Redor <laurent.redor@obeo.fr>
2023-01-19 14:01:39 +0100

    [releng] Bump version to 7.0.8
    Change-Id: I10c5eb191df842eb7f25bd1628afa1dc981cd3d4

--- releng/org.eclipse.sirius.releng/publish-nightly-jiro.sh
+++ releng/org.eclipse.sirius.releng/publish-nightly-jiro.sh
@@ -35,7 +35,7 @@ provided by the Jenkins build.
 set -e
 
 # The full version (should be taken as an argument)
-export VERSION="7.0.7"
+export VERSION="7.0.8"
 
 # The type of build being published
 export BUILD_TYPE="nightly"
