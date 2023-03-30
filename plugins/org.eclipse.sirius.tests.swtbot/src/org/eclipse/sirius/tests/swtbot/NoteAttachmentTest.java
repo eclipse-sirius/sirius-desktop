24584a2da1c6d4e04249a9db38a4cf06097654c9
Laurent Redor <laurent.redor@obeo.fr>
2023-03-30 15:01:35 +0200

    [581740] Add test to reveal NoteAttachment creation bug
    The creation of a NoteAttachment between 2 nodes should be forbidden.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=581740
    Change-Id: I389c44ed3163f5fce5cea4e606428aedb353ba85

--- plugins/org.eclipse.sirius.tests.swtbot/src/org/eclipse/sirius/tests/swtbot/NoteAttachmentTest.java
+++ plugins/org.eclipse.sirius.tests.swtbot/src/org/eclipse/sirius/tests/swtbot/NoteAttachmentTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Obeo.
+ * Copyright (c) 2017, 2023 Obeo and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -17,17 +17,24 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.NoteEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.internal.editparts.NoteAttachmentEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.internal.editparts.TextEditPart;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.sirius.diagram.DDiagram;
+import org.eclipse.sirius.diagram.ui.internal.edit.parts.DNodeListEditPart;
+import org.eclipse.sirius.diagram.ui.internal.edit.parts.SiriusNoteEditPart;
+import org.eclipse.sirius.diagram.ui.internal.edit.parts.SiriusTextEditPart;
+import org.eclipse.sirius.ext.gmf.runtime.editparts.GraphicalHelper;
 import org.eclipse.sirius.tests.swtbot.support.api.AbstractSiriusSwtBotGefTestCase;
 import org.eclipse.sirius.tests.swtbot.support.api.business.UIResource;
 import org.eclipse.sirius.tests.swtbot.support.api.condition.CheckSelectedCondition;
 import org.eclipse.sirius.tests.swtbot.support.api.editor.SWTBotSiriusDiagramEditor;
 import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefConnectionEditPart;
+import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditPart;
 import org.eclipse.swtbot.swt.finder.waits.DefaultCondition;
 
 /**
@@ -78,11 +85,71 @@ public class NoteAttachmentTest extends AbstractSiriusSwtBotGefTestCase {
             }
         });
         // Select the note attachment
-        editor.click(new Point(417, 103));
+        editor.click(new Point(417, 111));
         bot.waitUntil(new CheckSelectedCondition(editor, attachmentToNote.get()));
         // Select the text attachment
-        editor.click(new Point(433, 211));
+        editor.click(new Point(433, 223));
         bot.waitUntil(new CheckSelectedCondition(editor, attachmentToText.get()));
 
+    }
+
+    /**
+     * Checks that a Note attachment can be created between a Node and a Note.
+     * 
+     * @see "https://bugs.eclipse.org/bugs/show_bug.cgi?id=581740"
+     */
+    public void test_NoteAttachmentCreation_betweenNodeAndNote() {
+        // Create the note attachment
+        createNoteAttachment("NewEClass2", DNodeListEditPart.class, "A note", SiriusNoteEditPart.class);
+
+        // Check that a note attachment has been created.
+        checkNoteAttachmentEditPartFrom("NewEClass2", 1);
+    }
+
+    /**
+     * Checks that a Note attachment can be created between a Node and a Text.
+     * 
+     * @see "https://bugs.eclipse.org/bugs/show_bug.cgi?id=581740"
+     */
+    public void test_NoteAttachmentCreation_betweenNodeAndText() {
+        // Create the note attachment
+        createNoteAttachment("NewEClass2", DNodeListEditPart.class, "A text", SiriusTextEditPart.class);
+
+        // Check that a note attachment has been created.
+        checkNoteAttachmentEditPartFrom("NewEClass2", 1);
+    }
+
+    /**
+     * Checks that a Note attachment can not be created between a Node and another Node.
+     * 
+     * @see "https://bugs.eclipse.org/bugs/show_bug.cgi?id=581740"
+     */
+    public void test_NoteAttachmentCreation_betweenNodeAndNode() {
+        // Create the note attachment
+        createNoteAttachment("NewEClass2", DNodeListEditPart.class, "A", DNodeListEditPart.class);
+
+        // Check that a note attachment has been created.
+        checkNoteAttachmentEditPartFrom("NewEClass2", 0);
+    }
+
+    private void createNoteAttachment(String sourceName, Class<? extends EditPart> expectedSourceClass, String targetName, Class<? extends EditPart> expectedTargetClass) {
+        SWTBotGefEditPart source = editor.getEditPart(sourceName, expectedSourceClass);
+        SWTBotGefEditPart target = editor.getEditPart(targetName, expectedTargetClass);
+
+        Point sourcePoint = GraphicalHelper.getAbsoluteBoundsIn100Percent((IGraphicalEditPart) source.part()).getCenter();
+        Point targetPoint = GraphicalHelper.getAbsoluteBoundsIn100Percent((IGraphicalEditPart) target.part()).getCenter();
+
+        editor.activateTool("Note Attachment");
+        editor.click(sourcePoint);
+        editor.click(targetPoint);
+    }
+
+    private void checkNoteAttachmentEditPartFrom(String sourceName, int expectedNumberOfNoteAttachment) {
+        SWTBotGefEditPart sourcePart = editor.getEditPart(sourceName).parent();
+        assertEquals("Bad number of note attachmeent from " + sourceName, expectedNumberOfNoteAttachment, sourcePart.sourceConnections().size());
+        for (int i = 0; i < expectedNumberOfNoteAttachment; i++) {
+            SWTBotGefConnectionEditPart swtBotConnectionEditPart = sourcePart.sourceConnections().get(i);
+            assertTrue("The connection from " + sourceName + ", at index " + i + " is not a note attachment.", swtBotConnectionEditPart.part() instanceof NoteAttachmentEditPart);
+        }
     }
 }
