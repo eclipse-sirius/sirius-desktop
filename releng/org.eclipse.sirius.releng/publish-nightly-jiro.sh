2111df802985faf3c7c960153d532a32448b12d3
Laurent Fasani <laurent.fasani@obeo.fr>
2023-07-17 15:05:47 +0200

    [releng] Bump version to 6.6.2
    Signed-off-by: Laurent Fasani <laurent.fasani@obeo.fr>

--- releng/org.eclipse.sirius.releng/publish-nightly-jiro.sh
+++ releng/org.eclipse.sirius.releng/publish-nightly-jiro.sh
@@ -35,7 +35,7 @@ provided by the Jenkins build.
 set -e
 
 # The full version (should be taken as an argument)
-export VERSION="6.6.1"
+export VERSION="6.6.2"
 
 # The type of build being published
 export BUILD_TYPE="nightly"
