5c09bc7569c5b4d027376ba31513520b999bdb72
Glenn Plouhinec <glenn.plouhinec@obeo.fr>
2023-07-27 10:53:34 +0200

    [18] Synchronized nodes are no longer overlapped
    When synchronized nodes are created from another context (e.g. from
    another diagram), these nodes are positioned on the diagram to avoid
    overlapping with other nodes.
    
    Bug: https://github.com/eclipse-sirius/sirius-desktop/issues/18
    Signed-off-by: Glenn Plouhinec <glenn.plouhinec@obeo.fr>

--- plugins/org.eclipse.sirius.tests.swtbot/src/org/eclipse/sirius/tests/swtbot/GroupElementsInOneOtherTests.java
+++ plugins/org.eclipse.sirius.tests.swtbot/src/org/eclipse/sirius/tests/swtbot/GroupElementsInOneOtherTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2020, 2023 THALES GLOBAL SERVICES.
+ * Copyright (c) 2010, 2023 THALES GLOBAL SERVICES.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -24,7 +24,6 @@ import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
-import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.sirius.diagram.DDiagram;
 import org.eclipse.sirius.diagram.ui.business.api.view.SiriusLayoutDataManager;
 import org.eclipse.sirius.diagram.ui.edit.api.part.AbstractDiagramContainerEditPart;
@@ -52,21 +51,21 @@ import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditPart;
  */
 public class GroupElementsInOneOtherTests extends AbstractSiriusSwtBotGefTestCase {
 
-    private static final String PATH = "data/unit/tools/creation/popupMenu/VP-1859/";
+    private static final String PATH = "data/unit/tools/creation/popupMenu/VP-1859/"; //$NON-NLS-1$
 
-    private static final String SEMANTIC_RESOURCE_NAME = "vp-1859.ecore";
+    private static final String SEMANTIC_RESOURCE_NAME = "vp-1859.ecore"; //$NON-NLS-1$
 
-    private static final String SESSION_RESOURCE_NAME = "vp-1859.aird";
+    private static final String SESSION_RESOURCE_NAME = "vp-1859.aird"; //$NON-NLS-1$
 
-    private static final String MODELER_RESOURCE_NAME = "vp-1859.odesign";
+    private static final String MODELER_RESOURCE_NAME = "vp-1859.odesign"; //$NON-NLS-1$
 
-    private static final String FIRST_REPRESENTATION_NAME = "VP-1859-RealCase";
+    private static final String FIRST_REPRESENTATION_NAME = "VP-1859-RealCase"; //$NON-NLS-1$
 
-    private static final String FIRST_REPRESENTATION_INSTANCE_NAME = "new " + FIRST_REPRESENTATION_NAME;
+    private static final String FIRST_REPRESENTATION_INSTANCE_NAME = "new " + FIRST_REPRESENTATION_NAME; //$NON-NLS-1$
 
-    private static final String GROUP_TOOL_NAME = "Group in new Package";
+    private static final String GROUP_TOOL_NAME = "Group in new Package"; //$NON-NLS-1$
 
-    private static final String CREATION_TOOL_NAME = "Create 2 packages outside the selected element";
+    private static final String CREATION_TOOL_NAME = "Create 2 packages outside the selected element"; //$NON-NLS-1$
 
     private UIResource sessionAirdResource;
 
@@ -78,8 +77,6 @@ public class GroupElementsInOneOtherTests extends AbstractSiriusSwtBotGefTestCas
     private SWTBotGefEditPart class1ChildOfDiagramBot;
 
     private SWTBotGefEditPart class3ChildOfDiagramBot;
-
-    private SWTBotGefEditPart class4ChildOfDiagramBot;
 
     @Override
     protected void onSetUpBeforeClosingWelcomePage() throws Exception {
@@ -96,7 +93,7 @@ public class GroupElementsInOneOtherTests extends AbstractSiriusSwtBotGefTestCas
      */
     @Override
     protected void onSetUpAfterOpeningDesignerPerspective() throws Exception {
-        sessionAirdResource = new UIResource(designerProject, "/", SESSION_RESOURCE_NAME);
+        sessionAirdResource = new UIResource(designerProject, "/", SESSION_RESOURCE_NAME); //$NON-NLS-1$
         localSession = designerPerspective.openSessionFromFile(sessionAirdResource);
         closeOutline();
         editor = (SWTBotSiriusDiagramEditor) openRepresentation(localSession.getOpenedSession(), FIRST_REPRESENTATION_NAME, FIRST_REPRESENTATION_INSTANCE_NAME, DDiagram.class, true, true);
@@ -107,7 +104,6 @@ public class GroupElementsInOneOtherTests extends AbstractSiriusSwtBotGefTestCas
 
         class1ChildOfDiagramBot = diagramEditPartBot.descendants(IsInstanceOf.instanceOf(DNodeEditPart.class)).get(0);
         class3ChildOfDiagramBot = diagramEditPartBot.descendants(IsInstanceOf.instanceOf(DNodeEditPart.class)).get(2);
-        class4ChildOfDiagramBot = diagramEditPartBot.descendants(IsInstanceOf.instanceOf(DNodeEditPart.class)).get(3);
     }
 
     /**
@@ -124,16 +120,19 @@ public class GroupElementsInOneOtherTests extends AbstractSiriusSwtBotGefTestCas
         editor.select(class1ChildOfDiagramBot, class3ChildOfDiagramBot);
         // Get the center of the visible part of the editor before the group
         // action (in logical coordinates, not in screen coordinates)
-        Point absoluteEditorCenter = getEditorVisiblePart(class4ChildOfDiagramBot.part()).getCenter();
-        GraphicalHelper.screen2logical(absoluteEditorCenter, (IGraphicalEditPart) class4ChildOfDiagramBot.part());
+        GraphicalEditPart centeredEPackageEP = (GraphicalEditPart) editor.getEditPart("centeredEPackage", AbstractDiagramContainerEditPart.class).part();
+        Point expectedPosition = centeredEPackageEP.getFigure().getBounds().getRight().getCopy().translate(30, 0);
+        if (GraphicalHelper.getZoom(centeredEPackageEP) == ZoomLevel.ZOOM_50.getAmount()) {
+            expectedPosition = new Point(943, 468);
+        }
         // Launch the group action
         editor.clickContextMenu(GROUP_TOOL_NAME);
 
         // Check that the top-left corner does not overlap another figure at the
         // same level (brother edit parts).
-        SWTBotGefEditPart package5Bot = editor.getEditPart("Package5", AbstractDiagramContainerEditPart.class);
-        Point package5AbsoluteLocation = editor.getAbsoluteLocation("Package5", AbstractDiagramContainerEditPart.class);
-        assertFalse("The location (top-left corner) of the new figure should not overlaped an existing figure",
+        SWTBotGefEditPart package5Bot = editor.getEditPart("Package5", AbstractDiagramContainerEditPart.class); //$NON-NLS-1$
+        Point package5AbsoluteLocation = editor.getAbsoluteLocation("Package5", AbstractDiagramContainerEditPart.class); //$NON-NLS-1$
+        assertFalse("The location (top-left corner) of the new figure should not overlaped an existing figure", //$NON-NLS-1$
                 isThereFigureAtLocation(((GraphicalEditPart) editor.mainEditPart().part()).getContentPane(), package5AbsoluteLocation,
                         Collections.singleton(((org.eclipse.gef.GraphicalEditPart) package5Bot.part()).getFigure())));
 
@@ -141,7 +140,7 @@ public class GroupElementsInOneOtherTests extends AbstractSiriusSwtBotGefTestCas
         // Get the absolute location of p4 package (in logical coordinates, not
         // in screen coordinates)
         if (GraphicalHelper.getZoom(package5Bot.part()) != ZoomLevel.ZOOM_200.getAmount()) {
-            GraphicTestsSupportHelp.assertEquals("The newly created Package5 should be at the center of the visible part of the diagram", absoluteEditorCenter, package5AbsoluteLocation, 5, 5);
+            GraphicTestsSupportHelp.assertEquals("The newly created Package5 should be at the center of the visible part of the diagram", expectedPosition, package5AbsoluteLocation, 5, 5); //$NON-NLS-1$
         } else {
             // We don't make the same test with zoom at 200% because the middle
             // of the visible
@@ -152,7 +151,7 @@ public class GroupElementsInOneOtherTests extends AbstractSiriusSwtBotGefTestCas
                 // Check the screen resolution to fail only when it is as expected
                 Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
                 if (screenSize.width == 1440 || screenSize.height == 900) {
-                    fail("The newly created Package5 should be at the center of the visible part of the diagram. expected:<" + expectedPoint + "> but was:<" + package5AbsoluteLocation + ">");
+                    fail("The newly created Package5 should be at the center of the visible part of the diagram. expected:<" + expectedPoint + "> but was:<" + package5AbsoluteLocation + ">"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
                 }
             }
         }
@@ -199,23 +198,23 @@ public class GroupElementsInOneOtherTests extends AbstractSiriusSwtBotGefTestCas
             // Check that the two new packages top-left corner does not overlap
             // each
             // other.
-            Point package5AbsoluteLocation = editor.getAbsoluteLocation("Package5", AbstractDiagramContainerEditPart.class);
-            Point package6AbsoluteLocation = editor.getAbsoluteLocation("Package6", AbstractDiagramContainerEditPart.class);
+            Point package5AbsoluteLocation = editor.getAbsoluteLocation("Package5", AbstractDiagramContainerEditPart.class); //$NON-NLS-1$
+            Point package6AbsoluteLocation = editor.getAbsoluteLocation("Package6", AbstractDiagramContainerEditPart.class); //$NON-NLS-1$
 
-            assertFalse("The location (top-left corner) of the first created figure should not overlaped the location (top-left corner) of the second created figure.",
+            assertFalse("The location (top-left corner) of the first created figure should not overlaped the location (top-left corner) of the second created figure.", //$NON-NLS-1$
                     package5AbsoluteLocation.equals(package6AbsoluteLocation));
             int padding = SiriusLayoutDataManager.PADDING;
             if (isSnapToGridEnabled) {
                 padding = 50;
 
             }
-            assertEquals("The x coordinate of Package6 should have a delta of " + padding + " pixels with the x coordinate of the Package5.", package5AbsoluteLocation.x + padding,
+            assertEquals("The x coordinate of Package6 should have a delta of " + padding + " pixels with the x coordinate of the Package5.", package5AbsoluteLocation.x + padding, //$NON-NLS-1$ //$NON-NLS-2$
                     package6AbsoluteLocation.x);
-            assertEquals("The y coordinate of Package6 should have a delta of " + padding + " pixels with the y coordinate of the Package5.", package5AbsoluteLocation.y + padding,
+            assertEquals("The y coordinate of Package6 should have a delta of " + padding + " pixels with the y coordinate of the Package5.", package5AbsoluteLocation.y + padding, //$NON-NLS-1$ //$NON-NLS-2$
                     package6AbsoluteLocation.y);
             if (isSnapToGridEnabled) {
-                checkLocationAlignOnGrid(package5AbsoluteLocation, "Package5", padding);
-                checkLocationAlignOnGrid(package6AbsoluteLocation, "Package6", padding);
+                checkLocationAlignOnGrid(package5AbsoluteLocation, "Package5", padding); //$NON-NLS-1$
+                checkLocationAlignOnGrid(package6AbsoluteLocation, "Package6", padding); //$NON-NLS-1$
             }
         } finally {
             editor.setSnapToGrid(false);
@@ -272,7 +271,6 @@ public class GroupElementsInOneOtherTests extends AbstractSiriusSwtBotGefTestCas
         diagramEditPartBot = null;
         class1ChildOfDiagramBot = null;
         class3ChildOfDiagramBot = null;
-        class4ChildOfDiagramBot = null;
         // Reopen outline
         new DesignerViews(bot).openOutlineView();
         SWTBotUtils.waitAllUiEvents();
@@ -292,7 +290,7 @@ public class GroupElementsInOneOtherTests extends AbstractSiriusSwtBotGefTestCas
     private void checkLocationAlignOnGrid(Point location, String elementNameToDisplay, int gridSpacing) {
         boolean locationIsOK = (location.x % gridSpacing) == 0 || (location.y % gridSpacing) == 0;
         if (!locationIsOK) {
-            fail("For " + elementNameToDisplay + ", the x or y coordinate of the top left corner should be on the grid (grid spacing = " + gridSpacing + "), but was: " + location + ".");
+            fail("For " + elementNameToDisplay + ", the x or y coordinate of the top left corner should be on the grid (grid spacing = " + gridSpacing + "), but was: " + location + "."); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
         }
     }
 }
