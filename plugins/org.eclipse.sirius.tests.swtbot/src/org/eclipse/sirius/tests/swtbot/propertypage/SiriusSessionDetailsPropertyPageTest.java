3b1e96bd4bfcac35d2dff08617caa440149441a8
Maxime Porhel <maxime.porhel@obeo.fr>
2023-08-09 12:55:09 +0200

    [26] Correctly identify Sequence diagram in Sirius Session Details
    Bug: https://github.com/eclipse-sirius/sirius-desktop/issues/26
    Signed-off-by: Maxime Porhel <maxime.porhel@obeo.fr>

--- plugins/org.eclipse.sirius.tests.swtbot/src/org/eclipse/sirius/tests/swtbot/propertypage/SiriusSessionDetailsPropertyPageTest.java
+++ plugins/org.eclipse.sirius.tests.swtbot/src/org/eclipse/sirius/tests/swtbot/propertypage/SiriusSessionDetailsPropertyPageTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2022 THALES GLOBAL SERVICES.
+ * Copyright (c) 2021, 2023 THALES GLOBAL SERVICES.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -28,7 +28,6 @@ import org.eclipse.sirius.tests.swtbot.support.api.condition.TreeItemSelected;
 import org.eclipse.sirius.tests.swtbot.support.api.editor.SWTBotSiriusDiagramEditor;
 import org.eclipse.sirius.tests.swtbot.support.api.view.DesignerViews;
 import org.eclipse.sirius.tests.swtbot.support.utils.SWTBotUtils;
-import org.eclipse.sirius.tools.api.Messages;
 import org.eclipse.sirius.ui.business.internal.session.SiriusSessionDetailsPropertyPage;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swtbot.eclipse.finder.waits.Conditions;
@@ -51,6 +50,8 @@ public class SiriusSessionDetailsPropertyPageTest extends AbstractSiriusSwtBotGe
 
     private static final String MODEL = "My.ecore";
 
+    private static final String MODEL2 = "My.interactions";
+
     private static final String SESSION_FILE = "representations.aird";
 
     private static final String REPRESENTATION_DECRIPTION_NAME = "Entities";
@@ -61,13 +62,13 @@ public class SiriusSessionDetailsPropertyPageTest extends AbstractSiriusSwtBotGe
 
     @Override
     protected void onSetUpBeforeClosingWelcomePage() throws Exception {
-        copyFileToTestProject(Activator.PLUGIN_ID, DATA_UNIT_DIR, MODEL, SESSION_FILE);
+        copyFileToTestProject(Activator.PLUGIN_ID, DATA_UNIT_DIR, MODEL, MODEL2, SESSION_FILE);
     }
 
     @Override
     protected void onSetUpAfterOpeningDesignerPerspective() throws Exception {
         sessionAirdResource = new UIResource(designerProject, "/", SESSION_FILE);
-        localSession = designerPerspective.openSessionFromFile(sessionAirdResource);
+        localSession = designerPerspective.openSessionFromFile(sessionAirdResource, true);
         closeOutline();
         session = localSession.getOpenedSession();
         editor = (SWTBotSiriusDiagramEditor) openRepresentation(session, REPRESENTATION_DECRIPTION_NAME, REPRESENTATION_NAME, DDiagram.class);
@@ -143,66 +144,66 @@ public class SiriusSessionDetailsPropertyPageTest extends AbstractSiriusSwtBotGe
     }
 
     private String getExpectedSessionDetails() {
-        StringBuilder strBuilder = new StringBuilder();
-        strBuilder.append("\n");
-        strBuilder.append("*** Dependencies\n");
-        strBuilder.append("\n");
-        strBuilder.append("General projects dependencies (direct and transitive) (0)\n");
-        strBuilder.append("\n");
-        strBuilder.append("Image projects dependencies (direct) (0)\n");
-        strBuilder.append("\n");
-        strBuilder.append("*** Resources\n");
-        strBuilder.append("\n");
-        strBuilder.append("Session Resources (1)\n");
-        strBuilder.append("  platform:/resource/DesignerTestProject/representations.aird - 183 elements - x bytes\n");
-        strBuilder.append("\n");
-        strBuilder.append("Semantic Resources (1)\n");
-        strBuilder.append("  platform:/resource/DesignerTestProject/My.ecore - 7 elements - x bytes\n");
-        strBuilder.append("\n");
-        strBuilder.append("*** Viewpoints\n");
-        strBuilder.append("\n");
-        strBuilder.append("Active viewpoints (1)\n");
-        strBuilder.append("  Design loaded from resource platform:/plugin/org.eclipse.sirius.sample.ecore.design/description/ecore.odesign\n");
-        strBuilder.append("\n");
-        strBuilder.append("Inactive viewpoints (0)\n");
-        strBuilder.append("\n");
-        strBuilder.append("*** Representations\n");
-        strBuilder.append("\n");
-        strBuilder.append("All representations: 5\n");
-        strBuilder.append("  Diagram: 4\n");
-        strBuilder.append("  Sequence: 0\n");
-        strBuilder.append("  EditionTable: 1\n");
-        strBuilder.append("  CrossTable: 0\n");
-        strBuilder.append("  Tree: 0\n");
-        strBuilder.append("\n");
-        strBuilder.append("Loaded representations:  3\n");
-        strBuilder.append("Representation elements in loaded representations:  15\n");
-        strBuilder.append("\n");
-        strBuilder.append("Loaded representations containing elements with no semantic target (red cross decorator) (1)\n");
-        strBuilder.append("  Classes in root package - uid: _KjzRUGu6EeucgZBzLx9xaw\n");
-        strBuilder.append(Messages.SessionQuery_LoadedBrokenRepsInfo + "\n");
-        strBuilder.append("\n");
-        strBuilder.append("Invalid representations (3)\n");
-        strBuilder.append("  no target package entities BROKEN NoTarget - uid: _XlP0gAj6Ee2OwOZgulCHPg\n");
-        strBuilder.append("  root package entities BROKEN NoRepPath - uid: _TMvtwAj7Ee2OwOZgulCHPg\n");
-        strBuilder.append("  root package entities BROKEN NoRep - uid: _Uf3ihgj7Ee2OwOZgulCHPg\n");
-        strBuilder.append(Messages.SessionQuery_InvalidRepsInfo + "\n");
-        strBuilder.append("\n");
-        strBuilder.append("Representation descriptors details (5)\n");
-        strBuilder.append(
-                "  root package entities - uid: _8t7_oGu3EeuuXbLvG4gakA - description: Entities - viewpoint: Design - repPath: platform:/resource/DesignerTestProject/representations.aird#_8tukQGu3EeuuXbLvG4gakA -> {eClass: ecore::EPackage, name: root}  [Loaded][Diagram]\n");
-        strBuilder.append(
-                "  Classes in root package - uid: _KjzRUGu6EeucgZBzLx9xaw - description: Classes - viewpoint: Design - repPath: platform:/resource/DesignerTestProject/representations.aird#_Kh3-sGu6EeucgZBzLx9xaw -> {eClass: ecore::EPackage, name: root}  [Loaded][EditionTable]\n");
-        strBuilder.append(
-                "  no target package entities BROKEN NoTarget - uid: _XlP0gAj6Ee2OwOZgulCHPg - description: Entities - viewpoint: Design - repPath: platform:/resource/DesignerTestProject/representations.aird#_XlNYQAj6Ee2OwOZgulCHPg -> null  [Invalid][Loaded][Diagram]\n");
-        strBuilder.append(
-                "  root package entities BROKEN NoRepPath - uid: _TMvtwAj7Ee2OwOZgulCHPg - description: Entities - viewpoint: Design - repPath: null -> {eClass: ecore::EPackage, name: root}  [Invalid][Diagram]\n");
-        strBuilder.append(
-                "  root package entities BROKEN NoRep - uid: _Uf3ihgj7Ee2OwOZgulCHPg - description: Entities - viewpoint: Design - repPath: platform:/resource/DesignerTestProject/representations.aird#_Uf2UYAj7Ee2OwOZgulCHPg -> {eClass: ecore::EPackage, name: root}  [Invalid][Diagram]\n");
-        strBuilder.append("\n");
-        strBuilder.append("Representations opened in an editor (1)\n");
-        strBuilder.append("  root package entities - uid: _8t7_oGu3EeuuXbLvG4gakA\n");
+        String expectedReport = """
 
-        return strBuilder.toString();
+                *** Dependencies
+
+                General projects dependencies (direct and transitive) (0)
+
+                Image projects dependencies (direct) (0)
+
+                *** Resources
+
+                Session Resources (1)
+                  platform:/resource/DesignerTestProject/representations.aird - 227 elements - x bytes
+
+                Semantic Resources (2)
+                  platform:/resource/DesignerTestProject/My.ecore - 7 elements - x bytes
+                  platform:/resource/DesignerTestProject/My.interactions - 2 elements - x bytes
+
+                *** Viewpoints
+
+                Active viewpoints (2)
+                  Interactions loaded from resource platform:/plugin/org.eclipse.sirius.sample.interactions.design/description/interaction.odesign
+                  Design loaded from resource platform:/plugin/org.eclipse.sirius.sample.ecore.design/description/ecore.odesign
+
+                Inactive viewpoints (0)
+
+                *** Representations
+
+                All representations: 6
+                  Diagram: 4
+                  Sequence: 1
+                  EditionTable: 1
+                  CrossTable: 0
+                  Tree: 0
+
+                Loaded representations:  4
+                Representation elements in loaded representations:  18
+
+                Loaded representations containing elements with no semantic target (red cross decorator) (1)
+                  Classes in root package - uid: _KjzRUGu6EeucgZBzLx9xaw
+                Tip: The next (manual or automatic) refresh  will remove those elements. The red cross is displayed when the represented element has been detached or deleted from the model.
+
+                Invalid representations (3)
+                  no target package entities BROKEN NoTarget - uid: _XlP0gAj6Ee2OwOZgulCHPg
+                  root package entities BROKEN NoRepPath - uid: _TMvtwAj7Ee2OwOZgulCHPg
+                  root package entities BROKEN NoRep - uid: _Uf3ihgj7Ee2OwOZgulCHPg
+                Information: A representation is invalid when:
+                  - it has no target represented element or its target element has been detached or deleted
+                  - it can not be reached because it has been deleted or because of a technical issue
+
+                Representation descriptors details (6)
+                  root package entities - uid: _8t7_oGu3EeuuXbLvG4gakA - description: Entities - viewpoint: Design - repPath: platform:/resource/DesignerTestProject/representations.aird#_8tukQGu3EeuuXbLvG4gakA -> {eClass: ecore::EPackage, name: root}  [Loaded][Diagram]
+                  Classes in root package - uid: _KjzRUGu6EeucgZBzLx9xaw - description: Classes - viewpoint: Design - repPath: platform:/resource/DesignerTestProject/representations.aird#_Kh3-sGu6EeucgZBzLx9xaw -> {eClass: ecore::EPackage, name: root}  [Loaded][EditionTable]
+                  no target package entities BROKEN NoTarget - uid: _XlP0gAj6Ee2OwOZgulCHPg - description: Entities - viewpoint: Design - repPath: platform:/resource/DesignerTestProject/representations.aird#_XlNYQAj6Ee2OwOZgulCHPg -> null  [Invalid][Loaded][Diagram]
+                  root package entities BROKEN NoRepPath - uid: _TMvtwAj7Ee2OwOZgulCHPg - description: Entities - viewpoint: Design - repPath: null -> {eClass: ecore::EPackage, name: root}  [Invalid][Diagram]
+                  root package entities BROKEN NoRep - uid: _Uf3ihgj7Ee2OwOZgulCHPg - description: Entities - viewpoint: Design - repPath: platform:/resource/DesignerTestProject/representations.aird#_Uf2UYAj7Ee2OwOZgulCHPg -> {eClass: ecore::EPackage, name: root}  [Invalid][Diagram]
+                  Sequence Diagram on rootInteraction - uid: _3xGxUDagEe6u54Qv7g9TQA - description: Sequence Diagram on Interaction - viewpoint: Interactions - repPath: platform:/resource/DesignerTestProject/representations.aird#_3xFjMDagEe6u54Qv7g9TQA -> {eClass: interactions::Interaction, name: rootInteraction}  [Loaded][Sequence]
+
+                Representations opened in an editor (1)
+                  root package entities - uid: _8t7_oGu3EeuuXbLvG4gakA
+                 """;
+        return expectedReport;
     }
 }
