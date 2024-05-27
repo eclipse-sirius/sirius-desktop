ca76e29fcd73fb8075e4351871b855f90b70b40b
Laurent Redor <laurent.redor@obeo.fr>
2024-05-27 16:38:57 +0200

    [396] Adapt NodeCreationEditPolicy to compute the target location
    - Constructors of CreationUtil have been improved to directly have
    LayoutData as parameter instead of location and size. Indeed, the
    location is relative to the parent edit part and for border nodes, the
    parent edit part is not systematically the "host" of the called policy.
    - In NodeCreationEditPolicy, in case of border nodes, the "host edit
    part" is not systematically considered (to compute the RootLayoutData
    for example). The correct edit part, a parent of the host, is computed
    according to the mapping defined in the used tool.
    
    Bug: https://github.com/eclipse-sirius/sirius-desktop/issues/396

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/graphical/edit/policies/NodeCreationEditPolicy.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/graphical/edit/policies/NodeCreationEditPolicy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2009 THALES GLOBAL SERVICES.
+ * Copyright (c) 2007, 2024 THALES GLOBAL SERVICES.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -12,23 +12,34 @@
  *******************************************************************************/
 package org.eclipse.sirius.diagram.ui.graphical.edit.policies;
 
+import java.util.Optional;
+
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.emf.transaction.util.TransactionUtil;
+import org.eclipse.gef.EditPart;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.requests.CreateRequest;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.figures.ResizableCompartmentFigure;
 import org.eclipse.gmf.runtime.notation.Node;
+import org.eclipse.sirius.diagram.AbstractDNode;
 import org.eclipse.sirius.diagram.DDiagramElementContainer;
 import org.eclipse.sirius.diagram.DNode;
+import org.eclipse.sirius.diagram.description.AbstractNodeMapping;
+import org.eclipse.sirius.diagram.description.NodeMapping;
 import org.eclipse.sirius.diagram.description.tool.ContainerCreationDescription;
 import org.eclipse.sirius.diagram.description.tool.NodeCreationDescription;
+import org.eclipse.sirius.diagram.model.business.internal.helper.MappingHelper;
 import org.eclipse.sirius.diagram.tools.api.command.IDiagramCommandFactory;
 import org.eclipse.sirius.diagram.tools.api.command.IDiagramCommandFactoryProvider;
 import org.eclipse.sirius.diagram.ui.business.internal.query.RequestQuery;
+import org.eclipse.sirius.diagram.ui.business.internal.view.RootLayoutData;
+import org.eclipse.sirius.diagram.ui.edit.api.part.AbstractDiagramContainerEditPart;
+import org.eclipse.sirius.diagram.ui.internal.edit.parts.AbstractDNodeListCompartmentEditPart;
 import org.eclipse.sirius.diagram.ui.tools.api.draw2d.ui.figures.FigureUtilities;
 import org.eclipse.sirius.diagram.ui.tools.api.editor.DDiagramEditor;
 import org.eclipse.sirius.viewpoint.description.tool.AbstractToolDescription;
@@ -44,28 +55,41 @@ public class NodeCreationEditPolicy extends SiriusContainerEditPolicy {
      */
     @Override
     protected Command getCreateCommand(CreateRequest request) {
-        if (!(getHost().getModel() instanceof Node)) {
+
+        AbstractToolDescription tool = getTool(request);
+        GraphicalEditPart hostEditPartToUse = (GraphicalEditPart) getHost();
+        if (getHost() instanceof GraphicalEditPart) {
+            boolean isBorderNodeCreationRequest = new RequestQuery(request).isDropOrCreationOfBorderNode();
+            if (tool instanceof NodeCreationDescription nodeCreationDescriptionTool && isBorderNodeCreationRequest) {
+                // Search the correct edit part for this tool. It can be different than the host in case of "extra
+                // mappings" defined in the tool.
+                Optional<GraphicalEditPart> optionalEditPartToUse = getParentEditPartWithExpectedMapping((GraphicalEditPart) getHost(), nodeCreationDescriptionTool.getNodeMappings());
+                if (optionalEditPartToUse.isPresent()) {
+                    hostEditPartToUse = optionalEditPartToUse.get();
+                }
+            }
+        }
+        if (!(hostEditPartToUse.getModel() instanceof Node)) {
             return null;
         }
+        EObject containerElement = ((Node) hostEditPartToUse.getModel()).getElement();
 
-        EObject containerElement = ((Node) getHost().getModel()).getElement();
-        AbstractToolDescription tool = getTool(request);
         /*
-         * Dispatch to the appropriate specialized command depending on the type
-         * of the container element and the nature of the tool.
+         * Dispatch to the appropriate specialized command depending on the type of the container element and the nature
+         * of the tool.
          */
         Command result = null;
         if (containerElement instanceof DDiagramElementContainer) {
             DDiagramElementContainer viewNodeContainer = (DDiagramElementContainer) containerElement;
             if (tool instanceof NodeCreationDescription) {
-                result = getCreateNodeInContainerCommand(request, (NodeCreationDescription) tool, viewNodeContainer);
+                result = getCreateNodeInContainerCommand(request, (NodeCreationDescription) tool, viewNodeContainer, hostEditPartToUse);
             } else if (tool instanceof ContainerCreationDescription) {
                 result = getCreateContainerInContainerCommand(request, (ContainerCreationDescription) tool, viewNodeContainer);
             }
         } else if (containerElement instanceof DNode) {
             DNode viewNode = (DNode) containerElement;
             if (tool instanceof NodeCreationDescription) {
-                result = getCreateNodeOnNodeCommand(request, (NodeCreationDescription) tool, viewNode);
+                result = getCreateNodeOnNodeCommand(request, (NodeCreationDescription) tool, viewNode, hostEditPartToUse);
             }
         }
         return result;
@@ -82,8 +106,8 @@ public class NodeCreationEditPolicy extends SiriusContainerEditPolicy {
      *            the node on which to create the new (bordered) node.
      * @return a command to create the new node.
      */
-    protected Command getCreateNodeOnNodeCommand(CreateRequest request, NodeCreationDescription tool, DNode viewnode) {
-        CreationUtil creationUtil = new CreationUtil(request, getDiagramCommandFactory(), getRealLocation(request), getHost());
+    protected Command getCreateNodeOnNodeCommand(CreateRequest request, NodeCreationDescription tool, DNode viewnode, EditPart parentEditPartToUse) {
+        CreationUtil creationUtil = new CreationUtil(getDiagramCommandFactory(), getRealLayoutData(request, parentEditPartToUse), parentEditPartToUse);
         return creationUtil.getNodeCreationCommand(viewnode, tool);
     }
 
@@ -99,7 +123,7 @@ public class NodeCreationEditPolicy extends SiriusContainerEditPolicy {
      * @return a command to create the new container.
      */
     protected Command getCreateContainerInContainerCommand(CreateRequest request, ContainerCreationDescription tool, DDiagramElementContainer viewNodeContainer) {
-        CreationUtil creationUtil = new CreationUtil(request, getDiagramCommandFactory(), getRealLocation(request), getHost());
+        CreationUtil creationUtil = new CreationUtil(request, getDiagramCommandFactory(), getRealLocation(request, getHost()), getHost());
         return creationUtil.getContainerCreationDescription(viewNodeContainer, tool);
     }
 
@@ -112,10 +136,13 @@ public class NodeCreationEditPolicy extends SiriusContainerEditPolicy {
      *            the node creation tool description.
      * @param viewNodeContainer
      *            the container on which to create the new (bordered) node.
+     * @param parentEditPartToUse
+     *            parent EditPart to retrieve layout data (can be same as getHost(), but can also be another edit part
+     *            according to extra mapping defined in tool, for border nodes for example).
      * @return a command to create the new node.
      */
-    protected Command getCreateNodeInContainerCommand(CreateRequest request, NodeCreationDescription tool, DDiagramElementContainer viewNodeContainer) {
-        CreationUtil creationUtil = new CreationUtil(request, getDiagramCommandFactory(), getRealLocation(request), getHost());
+    protected Command getCreateNodeInContainerCommand(CreateRequest request, NodeCreationDescription tool, DDiagramElementContainer viewNodeContainer, EditPart parentEditPartToUse) {
+        CreationUtil creationUtil = new CreationUtil(getDiagramCommandFactory(), getRealLayoutData(request, parentEditPartToUse), parentEditPartToUse);
         return creationUtil.getNodeCreationCommand(viewNodeContainer, tool);
     }
 
@@ -135,18 +162,34 @@ public class NodeCreationEditPolicy extends SiriusContainerEditPolicy {
     }
 
     /**
-     * Computes the real location where the element must be created from the raw
-     * information passed in the request.
+     * Computes the real location where the element must be created from the raw information passed in the request.
      * 
      * @param request
      *            the creation request.
+     * @param parentEditPartToUse
+     *            parent EditPart to retrieve layout data (can be same as getHost(), but can also be another edit part
+     *            according to extra mapping defined in tool, for border nodes for example).
      * @return the real location where the element must be created.
      */
-    protected Point getRealLocation(final CreateRequest request) {
+    protected Point getRealLocation(final CreateRequest request, EditPart parentEditPartToUse) {
+        return getRealLayoutData(request, parentEditPartToUse).getLocation();
+    }
+
+    /**
+     * Computes the real location where the element must be created from the raw information passed in the request.
+     * 
+     * @param request
+     *            the creation request.
+     * @param parentEditPartToUse
+     *            parent EditPart to retrieve layout data (can be same as getHost(), but can also be another edit part
+     *            according to extra mapping defined in tool, for border nodes for example).
+     * @return the real location where the element must be created.
+     */
+    protected RootLayoutData getRealLayoutData(final CreateRequest request, EditPart parentEditPartToUse) {
         Point location = request.getLocation().getCopy();
         final Point realLocation;
-        if (location != null && getHost() instanceof GraphicalEditPart) {
-            final IFigure fig = ((GraphicalEditPart) getHost()).getFigure();
+        if (location != null && parentEditPartToUse instanceof GraphicalEditPart graphicalParentEditPart) {
+            final IFigure fig = graphicalParentEditPart.getFigure();
             fig.translateToRelative(location);
             final Point containerLocation = fig.getBounds().getLocation();
             location = new Point(location.x - containerLocation.x, location.y - containerLocation.y);
@@ -160,7 +203,7 @@ public class NodeCreationEditPolicy extends SiriusContainerEditPolicy {
                 } else {
                     scrollOffset = ((ResizableCompartmentFigure) fig).getScrollPane().getViewport().getViewLocation();
                 }
-                final Point shiftFromMarginOffset = FigureUtilities.getShiftFromMarginOffset((ResizableCompartmentFigure) fig, isBorderNodeCreationRequest, getHost());
+                final Point shiftFromMarginOffset = FigureUtilities.getShiftFromMarginOffset((ResizableCompartmentFigure) fig, isBorderNodeCreationRequest, parentEditPartToUse);
                 realLocation = new Point(location.x + scrollOffset.x - shiftFromMarginOffset.x, location.y + scrollOffset.y - shiftFromMarginOffset.y);
 
             } else {
@@ -169,7 +212,30 @@ public class NodeCreationEditPolicy extends SiriusContainerEditPolicy {
         } else {
             realLocation = location;
         }
-        return realLocation;
+        return new RootLayoutData(parentEditPartToUse, realLocation.getCopy(), CreationUtil.adaptRequestSizeToZoomFactor(request, parentEditPartToUse));
+    }
+
+    private Optional<GraphicalEditPart> getParentEditPartWithExpectedMapping(GraphicalEditPart editPart, EList<NodeMapping> nodeMappings) {
+        Optional<GraphicalEditPart> result = Optional.empty();
+        if (editPart instanceof AbstractDNodeListCompartmentEditPart) {
+            result = getParentEditPartWithExpectedMapping((GraphicalEditPart) editPart.getParent(), nodeMappings);
+        } else if (editPart.getParent() instanceof AbstractDiagramContainerEditPart && ((AbstractDiagramContainerEditPart) editPart.getParent()).isRegionContainer()) {
+            result = getParentEditPartWithExpectedMapping((AbstractDiagramContainerEditPart) editPart.getParent(), nodeMappings);
+        } else {
+            for (NodeMapping nodeMapping : nodeMappings) {
+                if (editPart != null && editPart.getModel() instanceof Node node && node.getElement() instanceof AbstractDNode abstractDNode
+                        && abstractDNode.getMapping() instanceof AbstractNodeMapping abstractNodegetMapping) {
+                    if (MappingHelper.getAllBorderedNodeMappings(abstractNodegetMapping).contains(nodeMapping)) {
+                        result = Optional.of(editPart);
+                        break;
+                    }
+                }
+            }
+        }
+        if (result.isEmpty() && editPart.getParent() instanceof GraphicalEditPart graphicalEditPart) {
+            result = getParentEditPartWithExpectedMapping(graphicalEditPart, nodeMappings);
+        }
+        return result;
     }
 
     /**
