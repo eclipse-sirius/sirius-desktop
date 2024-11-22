dcdc88f4723cfa29ad50df2c5aac708261dae458
Florian Barbin <florian.barbin@obeo.fr>
2024-11-22 15:14:56 +0100

    [508] Make part of the Sirius debugging view API
    * Create a new feature org.eclipse.sirius.debug.feature
    * This feature will reference a new plugin org.eclipse.sirius.debug that
    expose the AbstractDebugView and the TabularReport classes
    
    Bug: https://github.com/eclipse-sirius/sirius-desktop/issues/508
    Signed-off-by: Florian Barbin <florian.barbin@obeo.fr>

--- plugins/org.eclipse.sirius.ui.debug/src/org/eclipse/sirius/ui/debug/VSMAnalyzer.java
+++ plugins/org.eclipse.sirius.ui.debug/src/org/eclipse/sirius/ui/debug/VSMAnalyzer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 Obeo.
+ * Copyright (c) 2019, 2024 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -30,6 +30,7 @@ import com.google.common.collect.Ordering;
  * @author pcdavid
  *
  */
+@SuppressWarnings("nls")
 public class VSMAnalyzer {
     private final Group vsm;
 
