eclipse-sirius/sirius-desktop

commit 7f5ea293a59a5a380a974028f2509c45b8573c1b

[467] Fix edge creation tool to create only the expected edge
Bug: https://github.com/eclipse-sirius/sirius-desktop/issues/467
Signed-off-by: Séraphin Costa <seraphin.costa@obeosoft.com>

plugins/org.eclipse.sirius.diagram/src/org/eclipse/sirius/diagram/business/internal/helper/task/CreateDEdgeTask.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2019 THALES GLOBAL SERVICES.
+ * Copyright (c) 2007, 2024 THALES GLOBAL SERVICES.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -123,7 +123,7 @@ public class CreateDEdgeTask extends AbstractCommandTask implements ICreationTas
             EdgeMapping bestEdgeMapping = bestMappingGetter.getBestEdgeMapping(tool.getEdgeMappings());
             if (bestEdgeMapping != null) {
                 Option<DEdge> createdDEdgeOption = dDiagramSynchronizer.createEdgeMapping(mappingManager, mappingsToEdgeTargets, bestEdgeMapping, edgeToMappingBasedDecoration,
-                        edgeToSemanticBasedDecoration);
+                        edgeToSemanticBasedDecoration, sourceView, targetView);
                 if (createdDEdgeOption.some()) {
                     createdDEdges.add(createdDEdgeOption.get());
                 }

plugins/org.eclipse.sirius.diagram/src/org/eclipse/sirius/diagram/business/internal/sync/DDiagramSynchronizer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2021 THALES GLOBAL SERVICES and others.
+ * Copyright (c) 2009, 2024 THALES GLOBAL SERVICES and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -1176,13 +1176,18 @@ public class DDiagramSynchronizer {
      *            mapping based decorations.
      * @param edgeToSemanticBasedDecoration
      *            semantic based decorations
+     * @param sourceView
+     *            the source view of the edge
+     * @param targetView
+     *            the target view of the edge
      * @return an Option on DEdge
      */
     public Option<DEdge> createEdgeMapping(DiagramMappingsManager mappingManager, final Map<DiagramElementMapping, Collection<EdgeTarget>> mappingsToEdgeTargets, final EdgeMapping mapping,
-            final Map<EdgeMapping, Collection<MappingBasedDecoration>> edgeToMappingBasedDecoration, final Map<String, Collection<SemanticBasedDecoration>> edgeToSemanticBasedDecoration) {
+            final Map<EdgeMapping, Collection<MappingBasedDecoration>> edgeToMappingBasedDecoration, final Map<String, Collection<SemanticBasedDecoration>> edgeToSemanticBasedDecoration,
+            EdgeTarget sourceView, EdgeTarget targetView) {
 
         edgesDones = new HashSet<DDiagramElement>();
-        Collection<DEdge> newEdges = createDEdgeOnMapping(mappingManager, mappingsToEdgeTargets, mapping, edgeToMappingBasedDecoration, edgeToSemanticBasedDecoration);
+        Collection<DEdge> newEdges = createDEdgeOnMapping(mappingManager, mappingsToEdgeTargets, mapping, edgeToMappingBasedDecoration, edgeToSemanticBasedDecoration, sourceView, targetView);
         edgesDones.clear();
         if (!newEdges.isEmpty()) {
             return Options.newSome(newEdges.iterator().next());
@@ -1191,13 +1196,22 @@ public class DDiagramSynchronizer {
     }
 
     private Collection<DEdge> createDEdgeOnMapping(DiagramMappingsManager mappingManager, final Map<DiagramElementMapping, Collection<EdgeTarget>> mappingsToEdgeTargets, final EdgeMapping mapping,
-            final Map<EdgeMapping, Collection<MappingBasedDecoration>> edgeToMappingBasedDecoration, final Map<String, Collection<SemanticBasedDecoration>> edgeToSemanticBasedDecoration) {
+            final Map<EdgeMapping, Collection<MappingBasedDecoration>> edgeToMappingBasedDecoration, final Map<String, Collection<SemanticBasedDecoration>> edgeToSemanticBasedDecoration,
+            EdgeTarget sourceView, EdgeTarget targetView) {
         Collection<DEdge> newEdges = new HashSet<DEdge>();
         if (this.accessor.getPermissionAuthority().canEditInstance(this.diagram)) {
             final SetIntersection<DEdgeCandidate> status = createEdgeCandidates(mappingsToEdgeTargets, mapping, edgeToMappingBasedDecoration, edgeToSemanticBasedDecoration);
 
             for (final DEdgeCandidate candidate : status.getNewElements()) {
-                newEdges.add(this.sync.createNewEdge(mappingManager, candidate, mappingsToEdgeTargets, edgeToMappingBasedDecoration, edgeToSemanticBasedDecoration));
+                boolean candidateIsSync = new DiagramElementMappingQuery(mapping).isSynchronizedAndCreateElement(diagram);
+                boolean candidateMatch = candidate.getSourceView() == sourceView && candidate.getTargetView() == targetView;
+                boolean candidateMatchInv = candidate.getSourceView() == targetView && candidate.getTargetView() == sourceView;
+                // an edge is created either:
+                // - if the mapping is synchronized,
+                // - or if this edge candidate has the expected mapping/source/target associated to the tool.
+                if (candidateIsSync || candidateMatch || candidateMatchInv) {
+                    newEdges.add(this.sync.createNewEdge(mappingManager, candidate, mappingsToEdgeTargets, edgeToMappingBasedDecoration, edgeToSemanticBasedDecoration));
+                }
             }
         }
         return newEdges;

plugins/org.eclipse.sirius.tests.junit/data/unit/tool/bugEdge/My.ecore
@@ -1,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ecore:EPackage xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xmlns:ecore="http://www.eclipse.org/emf/2002/Ecore" name="pkg">
+  <eClassifiers xsi:type="ecore:EClass" name="cls1">
+    <eStructuralFeatures xsi:type="ecore:EReference" name="ref" eType="#//cls3"/>
+  </eClassifiers>
+  <eClassifiers xsi:type="ecore:EClass" name="cls2"/>
+  <eClassifiers xsi:type="ecore:EClass" name="cls3"/>
+  <eClassifiers xsi:type="ecore:EClass" name="cls4"/>
+  <eSubpackages name="a"/>
+  <eSubpackages name="b"/>
+</ecore:EPackage>

plugins/org.eclipse.sirius.tests.junit/data/unit/tool/bugEdge/project.odesign
@@ -1,0 +1,144 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<description:Group xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:description="http://www.eclipse.org/sirius/description/1.1.0" xmlns:description_1="http://www.eclipse.org/sirius/diagram/description/1.1.0" xmlns:style="http://www.eclipse.org/sirius/diagram/description/style/1.1.0" xmlns:tool="http://www.eclipse.org/sirius/diagram/description/tool/1.1.0" xmlns:tool_1="http://www.eclipse.org/sirius/description/tool/1.1.0" name="project" version="15.4.0.202401051836">
+  <ownedViewpoints name="EdgeTest">
+    <ownedRepresentations xsi:type="description_1:DiagramDescription" name="DiagEdgeUnsync" domainClass="ecore::EPackage" enablePopupBars="true">
+      <metamodel href="http://www.eclipse.org/emf/2002/Ecore#/"/>
+      <defaultLayer name="Default">
+        <nodeMappings name="Class1" synchronizationLock="true" domainClass="ecore::EClassifier">
+          <style xsi:type="style:SquareDescription" labelSize="12" labelPosition="node" resizeKind="NSEW">
+            <borderColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='black']"/>
+            <labelColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='black']"/>
+            <color xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='gray']"/>
+          </style>
+        </nodeMappings>
+        <edgeMappings name="Relation1" createElements="false" sourceMapping="//@ownedViewpoints[name='EdgeTest']/@ownedRepresentations[name='DiagEdgeUnsync']/@defaultLayer/@nodeMappings[name='Class1']" targetMapping="//@ownedViewpoints[name='EdgeTest']/@ownedRepresentations[name='DiagEdgeUnsync']/@defaultLayer/@nodeMappings[name='Class1']" targetFinderExpression="aql:self.eType" sourceFinderExpression="aql:self.eContainer()" domainClass="ecore::EReference" useDomainElement="true">
+          <style sizeComputationExpression="2">
+            <strokeColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='gray']"/>
+            <centerLabelStyleDescription labelSize="12" labelExpression="aql:self.name">
+              <labelColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='black']"/>
+            </centerLabelStyleDescription>
+          </style>
+        </edgeMappings>
+        <toolSections name="Section1">
+          <ownedTools xsi:type="tool:EdgeCreationDescription" name="CreateEdge1" precondition="aql:true" edgeMappings="//@ownedViewpoints[name='EdgeTest']/@ownedRepresentations[name='DiagEdgeUnsync']/@defaultLayer/@edgeMappings[name='Relation1']" connectionStartPrecondition="aql:true">
+            <sourceVariable name="source"/>
+            <targetVariable name="target"/>
+            <sourceViewVariable name="sourceView"/>
+            <targetViewVariable name="targetView"/>
+            <initialOperation>
+              <firstModelOperations xsi:type="tool_1:ChangeContext" browseExpression="source">
+                <subModelOperations xsi:type="tool_1:CreateInstance" typeName="ecore::EReference" referenceName="eStructuralFeatures">
+                  <subModelOperations xsi:type="tool_1:SetValue" featureName="eType" valueExpression="var:target"/>
+                  <subModelOperations xsi:type="tool_1:SetValue" featureName="name" valueExpression="newRef"/>
+                </subModelOperations>
+              </firstModelOperations>
+            </initialOperation>
+          </ownedTools>
+        </toolSections>
+      </defaultLayer>
+    </ownedRepresentations>
+    <ownedRepresentations xsi:type="description_1:DiagramDescription" name="DiagInvEdge" domainClass="ecore::EPackage" enablePopupBars="true">
+      <metamodel href="http://www.eclipse.org/emf/2002/Ecore#/"/>
+      <defaultLayer name="Default">
+        <nodeMappings name="Class2" synchronizationLock="true" domainClass="ecore::EClassifier">
+          <style xsi:type="style:SquareDescription" labelSize="12" labelPosition="node" resizeKind="NSEW">
+            <borderColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='black']"/>
+            <labelColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='black']"/>
+            <color xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='gray']"/>
+          </style>
+        </nodeMappings>
+        <edgeMappings name="Relation2" createElements="false" sourceMapping="//@ownedViewpoints[name='EdgeTest']/@ownedRepresentations[name='DiagInvEdge']/@defaultLayer/@nodeMappings[name='Class2']" targetMapping="//@ownedViewpoints[name='EdgeTest']/@ownedRepresentations[name='DiagInvEdge']/@defaultLayer/@nodeMappings[name='Class2']" targetFinderExpression="aql:if self.changeable then self.eType else self.eContainer() endif" sourceFinderExpression="aql:if self.changeable then self.eContainer() else self.eType endif" domainClass="ecore::EReference" useDomainElement="true">
+          <style sizeComputationExpression="2">
+            <strokeColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='gray']"/>
+            <centerLabelStyleDescription labelSize="12" labelExpression="aql:self.name">
+              <labelColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='black']"/>
+            </centerLabelStyleDescription>
+          </style>
+          <conditionnalStyles predicateExpression="aql:self.changeable">
+            <style sourceArrow="InputArrow" sizeComputationExpression="2">
+              <strokeColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='gray']"/>
+              <centerLabelStyleDescription labelSize="12" labelExpression="aql:self.name">
+                <labelColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='black']"/>
+              </centerLabelStyleDescription>
+            </style>
+          </conditionnalStyles>
+        </edgeMappings>
+        <toolSections name="Section2">
+          <ownedTools xsi:type="tool:EdgeCreationDescription" name="CreateEdge2" precondition="aql:true" edgeMappings="//@ownedViewpoints[name='EdgeTest']/@ownedRepresentations[name='DiagInvEdge']/@defaultLayer/@edgeMappings[name='Relation2']" connectionStartPrecondition="aql:true">
+            <sourceVariable name="source"/>
+            <targetVariable name="target"/>
+            <sourceViewVariable name="sourceView"/>
+            <targetViewVariable name="targetView"/>
+            <initialOperation>
+              <firstModelOperations xsi:type="tool_1:ChangeContext" browseExpression="source">
+                <subModelOperations xsi:type="tool_1:CreateInstance" typeName="ecore::EReference" referenceName="eStructuralFeatures">
+                  <subModelOperations xsi:type="tool_1:SetValue" featureName="eType" valueExpression="var:target"/>
+                  <subModelOperations xsi:type="tool_1:SetValue" featureName="name" valueExpression="newRef"/>
+                </subModelOperations>
+              </firstModelOperations>
+            </initialOperation>
+          </ownedTools>
+        </toolSections>
+      </defaultLayer>
+    </ownedRepresentations>
+    <ownedRepresentations xsi:type="description_1:DiagramDescription" name="DiagEdgeDepEdge" domainClass="ecore::EPackage" enablePopupBars="true">
+      <defaultLayer name="Default">
+        <nodeMappings name="Class3" synchronizationLock="true" domainClass="ecore::EClassifier">
+          <style xsi:type="style:SquareDescription" labelSize="12" labelPosition="node" resizeKind="NSEW">
+            <borderColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='black']"/>
+            <labelColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='black']"/>
+            <color xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='gray']"/>
+          </style>
+        </nodeMappings>
+        <nodeMappings name="Reference" semanticCandidatesExpression="aql:self.eClassifiers->filter(ecore::EClass).eReferences" synchronizationLock="true" domainClass="ecore::EReference">
+          <style xsi:type="style:SquareDescription" labelSize="12" labelPosition="node" resizeKind="NSEW">
+            <borderColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='black']"/>
+            <labelColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='black']"/>
+            <color xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='orange']"/>
+          </style>
+        </nodeMappings>
+        <edgeMappings name="Relation3" createElements="false" sourceMapping="//@ownedViewpoints[name='EdgeTest']/@ownedRepresentations[name='DiagEdgeDepEdge']/@defaultLayer/@nodeMappings[name='Class3']" targetMapping="//@ownedViewpoints[name='EdgeTest']/@ownedRepresentations[name='DiagEdgeDepEdge']/@defaultLayer/@nodeMappings[name='Class3']" targetFinderExpression="aql:self.eType" sourceFinderExpression="aql:self.eContainer()" domainClass="ecore::EReference" useDomainElement="true">
+          <style sizeComputationExpression="2">
+            <strokeColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='gray']"/>
+            <centerLabelStyleDescription labelSize="12" labelExpression="aql:self.name">
+              <labelColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='black']"/>
+            </centerLabelStyleDescription>
+          </style>
+        </edgeMappings>
+        <edgeMappings name="Relation4" synchronizationLock="true" sourceMapping="//@ownedViewpoints[name='EdgeTest']/@ownedRepresentations[name='DiagEdgeDepEdge']/@defaultLayer/@nodeMappings[name='Class3']" targetMapping="//@ownedViewpoints[name='EdgeTest']/@ownedRepresentations[name='DiagEdgeDepEdge']/@defaultLayer/@nodeMappings[name='Reference']" targetFinderExpression="aql:self" sourceFinderExpression="aql:self.eContainer()" domainClass="ecore::EReference" useDomainElement="true">
+          <style sizeComputationExpression="2">
+            <strokeColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='gray']"/>
+            <centerLabelStyleDescription labelSize="12" labelExpression="aql:self.name">
+              <labelColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='black']"/>
+            </centerLabelStyleDescription>
+          </style>
+        </edgeMappings>
+        <edgeMappings name="Relation5" sourceMapping="//@ownedViewpoints[name='EdgeTest']/@ownedRepresentations[name='DiagEdgeDepEdge']/@defaultLayer/@nodeMappings[name='Reference']" targetMapping="//@ownedViewpoints[name='EdgeTest']/@ownedRepresentations[name='DiagEdgeDepEdge']/@defaultLayer/@nodeMappings[name='Class3']" targetFinderExpression="aql:self.eType" sourceFinderExpression="aql:self" domainClass="ecore::EReference" useDomainElement="true">
+          <style sizeComputationExpression="2">
+            <strokeColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='gray']"/>
+            <centerLabelStyleDescription labelSize="12" labelExpression="aql:self.name">
+              <labelColor xsi:type="description:SystemColor" href="environment:/viewpoint#//@systemColors/@entries[name='black']"/>
+            </centerLabelStyleDescription>
+          </style>
+        </edgeMappings>
+        <toolSections name="Section3">
+          <ownedTools xsi:type="tool:EdgeCreationDescription" name="CreateEdge3" precondition="aql:true" edgeMappings="//@ownedViewpoints[name='EdgeTest']/@ownedRepresentations[name='DiagEdgeDepEdge']/@defaultLayer/@edgeMappings[name='Relation3']" connectionStartPrecondition="aql:true">
+            <sourceVariable name="source"/>
+            <targetVariable name="target"/>
+            <sourceViewVariable name="sourceView"/>
+            <targetViewVariable name="targetView"/>
+            <initialOperation>
+              <firstModelOperations xsi:type="tool_1:ChangeContext" browseExpression="source">
+                <subModelOperations xsi:type="tool_1:CreateInstance" typeName="ecore::EReference" referenceName="eStructuralFeatures">
+                  <subModelOperations xsi:type="tool_1:SetValue" featureName="eType" valueExpression="var:target"/>
+                  <subModelOperations xsi:type="tool_1:SetValue" featureName="name" valueExpression="newRef"/>
+                </subModelOperations>
+              </firstModelOperations>
+            </initialOperation>
+          </ownedTools>
+        </toolSections>
+      </defaultLayer>
+    </ownedRepresentations>
+    <ownedJavaExtensions qualifiedClassName="my.project.design.Services"/>
+  </ownedViewpoints>
+</description:Group>

plugins/org.eclipse.sirius.tests.junit/src/org/eclipse/sirius/tests/suite/diagram/AllDiagramPluginsTests.java
@@ -56,6 +56,7 @@ import org.eclipse.sirius.tests.unit.api.session.TablesAndEntitiesDirtyTest;
 import org.eclipse.sirius.tests.unit.api.startup.StartupRepresentationsTests;
 import org.eclipse.sirius.tests.unit.api.tools.CreateEdgeOnEdgeTest;
 import org.eclipse.sirius.tests.unit.api.tools.CreateEdgeTest;
+import org.eclipse.sirius.tests.unit.api.tools.CreateEdgeUnsyncBugTest;
 import org.eclipse.sirius.tests.unit.api.tools.CreateEdgeViewTest;
 import org.eclipse.sirius.tests.unit.api.tools.CreateNodeViewTest;
 import org.eclipse.sirius.tests.unit.api.tools.DeleteTest;
@@ -530,6 +531,7 @@ public class AllDiagramPluginsTests {
         suite.addTestSuite(ForToolTests.class);
         suite.addTestSuite(ExtensionPaletteToolTest.class);
         suite.addTestSuite(CreateEdgeTest.class);
+        suite.addTest(new JUnit4TestAdapter(CreateEdgeUnsyncBugTest.class));
         suite.addTestSuite(SiriusControlAndDeleteRepresentationTest.class);
         suite.addTestSuite(ToolFilterDescriptionListenerTests.class);
         suite.addTestSuite(CreateNodeViewTest.class);

commit d2bda89e147a41bc7fd69b26d87a049a1e70b5a3

[467] Fix regression with reference based mappings or extra mappings
Commit 7f5ea293a59a5a380a974028f2509c45b8573c1b introduced a check on
sourceView/targetView for DEdge automatic creation by CreateDEdgeTask
triggered after the execution of the Edge creation tools model
operations.

However several use cases have been broken with unit tests:
 - HideRevealEdgeLabelsTest
 - EdgeCreationPositionTest
 - EdgeCreationPositionWithSnapToGridTest
 - EdgeWithBorderNodeCreationPositionWithSnapToGridTest

Bug: https://github.com/eclipse-sirius/sirius-desktop/issues/467
Change-Id: I6e39e400230d7c3ad7ef651f34b6e43381454cd6
Signed-off-by: Maxime Porhel <maxime.porhel@obeo.fr>

plugins/org.eclipse.sirius.diagram/src/org/eclipse/sirius/diagram/business/internal/helper/task/CreateDEdgeTask.java
@@ -121,9 +121,11 @@ public class CreateDEdgeTask extends AbstractCommandTask implements ICreationTas
         for (EObject semanticElt : getAllSemantics()) {
             BestMappingGetter bestMappingGetter = new BestMappingGetter(sourceView, targetView, semanticElt);
             EdgeMapping bestEdgeMapping = bestMappingGetter.getBestEdgeMapping(tool.getEdgeMappings());
+
             if (bestEdgeMapping != null) {
+
                 Option<DEdge> createdDEdgeOption = dDiagramSynchronizer.createEdgeMapping(mappingManager, mappingsToEdgeTargets, bestEdgeMapping, edgeToMappingBasedDecoration,
-                        edgeToSemanticBasedDecoration, sourceView, targetView);
+                        edgeToSemanticBasedDecoration, semanticElt);
                 if (createdDEdgeOption.some()) {
                     createdDEdges.add(createdDEdgeOption.get());
                 }

plugins/org.eclipse.sirius.diagram/src/org/eclipse/sirius/diagram/business/internal/sync/DDiagramSynchronizer.java
@@ -1176,18 +1176,16 @@ public class DDiagramSynchronizer {
      *            mapping based decorations.
      * @param edgeToSemanticBasedDecoration
      *            semantic based decorations
-     * @param sourceView
-     *            the source view of the edge
-     * @param targetView
-     *            the target view of the edge
+     * @param expectedSemantic
+     *            the expected semantic target of the created DEdge elements
      * @return an Option on DEdge
      */
     public Option<DEdge> createEdgeMapping(DiagramMappingsManager mappingManager, final Map<DiagramElementMapping, Collection<EdgeTarget>> mappingsToEdgeTargets, final EdgeMapping mapping,
             final Map<EdgeMapping, Collection<MappingBasedDecoration>> edgeToMappingBasedDecoration, final Map<String, Collection<SemanticBasedDecoration>> edgeToSemanticBasedDecoration,
-            EdgeTarget sourceView, EdgeTarget targetView) {
+            EObject expectedSemantic) {
 
         edgesDones = new HashSet<DDiagramElement>();
-        Collection<DEdge> newEdges = createDEdgeOnMapping(mappingManager, mappingsToEdgeTargets, mapping, edgeToMappingBasedDecoration, edgeToSemanticBasedDecoration, sourceView, targetView);
+        Collection<DEdge> newEdges = createDEdgeOnMapping(mappingManager, mappingsToEdgeTargets, mapping, edgeToMappingBasedDecoration, edgeToSemanticBasedDecoration, expectedSemantic);
         edgesDones.clear();
         if (!newEdges.isEmpty()) {
             return Options.newSome(newEdges.iterator().next());
@@ -1197,21 +1195,28 @@ public class DDiagramSynchronizer {
 
     private Collection<DEdge> createDEdgeOnMapping(DiagramMappingsManager mappingManager, final Map<DiagramElementMapping, Collection<EdgeTarget>> mappingsToEdgeTargets, final EdgeMapping mapping,
             final Map<EdgeMapping, Collection<MappingBasedDecoration>> edgeToMappingBasedDecoration, final Map<String, Collection<SemanticBasedDecoration>> edgeToSemanticBasedDecoration,
-            EdgeTarget sourceView, EdgeTarget targetView) {
+            EObject expectedSemantic) {
         Collection<DEdge> newEdges = new HashSet<DEdge>();
         if (this.accessor.getPermissionAuthority().canEditInstance(this.diagram)) {
             final SetIntersection<DEdgeCandidate> status = createEdgeCandidates(mappingsToEdgeTargets, mapping, edgeToMappingBasedDecoration, edgeToSemanticBasedDecoration);
 
             for (final DEdgeCandidate candidate : status.getNewElements()) {
                 boolean candidateIsSync = new DiagramElementMappingQuery(mapping).isSynchronizedAndCreateElement(diagram);
-                boolean candidateMatch = candidate.getSourceView() == sourceView && candidate.getTargetView() == targetView;
-                boolean candidateMatchInv = candidate.getSourceView() == targetView && candidate.getTargetView() == sourceView;
-                // an edge is created either:
-                // - if the mapping is synchronized,
-                // - or if this edge candidate has the expected mapping/source/target associated to the tool.
-                if (candidateIsSync || candidateMatch || candidateMatchInv) {
+
+                // CreateDEdgeTask has found an EdgeMapping corresponding to the executed Edge creation tool
+                // configuration and its model operations execution.
+                // An edge is created in the following excluding cases:
+                // - the mapping is synchronized,
+                // - this edge candidate has the expected semantic element
+                // Compared to DnDTasksOperations::addCandidate there is no filter on mapping.isUseDomainElement()
+                // For more complex cases like creation of unsynchronized edges whose semantic target would not be
+                // created or affected by the model operation,
+                // specifier has to use on CreateEdgeView model operations instead of relying on automatic creation by
+                // the EdgeCreationTool (automatic refresh will not create such element either).
+                if (candidateIsSync || candidate.getSemantic() == expectedSemantic) {
                     newEdges.add(this.sync.createNewEdge(mappingManager, candidate, mappingsToEdgeTargets, edgeToMappingBasedDecoration, edgeToSemanticBasedDecoration));
                 }
+
             }
         }
         return newEdges;