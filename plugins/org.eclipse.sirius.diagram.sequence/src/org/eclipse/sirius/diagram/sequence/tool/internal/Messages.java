5ccb5a9a8af75f9908d4a8e2bf2c74dad333d2dc
Steve Monnier <steve.monnier@obeo.fr>
2024-12-09 23:11:46 -0500

    [518] Use all bounds for layout instead of filtering
    In SequenceHorizontal/VerticalLayout we can scan the map of bounds
    without filtering for instance roles/lifelines/lost messages so the
    interaction container can adapt to more "unusual" construction (like 10
    recursively contained combined fragment).
    
    Bug: https://github.com/eclipse-sirius/sirius-desktop/issues/518
    Signed-off-by: Steve Monnier <steve.monnier@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.sequence/src/org/eclipse/sirius/diagram/sequence/tool/internal/Messages.java
+++ plugins/org.eclipse.sirius.diagram.sequence/src/org/eclipse/sirius/diagram/sequence/tool/internal/Messages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2021 Obeo.
+ * Copyright (c) 2015, 2024 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -66,6 +66,9 @@ public final class Messages {
 
     @TranslatableMessage
     public static String InteractionUse_nonInsteractionUseNode;
+
+    @TranslatableMessage
+    public static String InteractionContainer_multipleElementDetected;
 
     @TranslatableMessage
     public static String InverseRelativeNodePositionOperation_operationName;
