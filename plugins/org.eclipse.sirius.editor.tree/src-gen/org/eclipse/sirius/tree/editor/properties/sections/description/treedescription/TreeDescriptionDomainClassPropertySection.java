657e056ba10e427856a5edb79effca04b8e6eaed
Maxime Porhel <maxime.porhel@obeo.fr>
2025-10-05 18:32:43 +0200

    [633] Add VSM color CSS class to Text controls of tree property sections
    Bug: https://github.com/eclipse-sirius/sirius-desktop/issues/633
    Signed-off-by: Maxime Porhel <maxime.porhel@obeo.fr>

--- plugins/org.eclipse.sirius.editor.tree/src-gen/org/eclipse/sirius/tree/editor/properties/sections/description/treedescription/TreeDescriptionDomainClassPropertySection.java
+++ plugins/org.eclipse.sirius.editor.tree/src-gen/org/eclipse/sirius/tree/editor/properties/sections/description/treedescription/TreeDescriptionDomainClassPropertySection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2018 THALES GLOBAL SERVICES.
+ * Copyright (c) 2007, 2025 THALES GLOBAL SERVICES.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -104,6 +104,7 @@ public class TreeDescriptionDomainClassPropertySection extends AbstractTextPrope
          * We set the color as it's a TypeName
          */
         text.setBackground(SiriusEditor.getColorRegistry().get("green"));
+        text.setData("org.eclipse.e4.ui.css.CssClassName", "siriusVSMEditorTypeName");
 
         text.setToolTipText(getToolTipText());
 
