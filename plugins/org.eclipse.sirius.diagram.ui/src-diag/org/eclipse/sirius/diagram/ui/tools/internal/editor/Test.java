7f7f2a87be0d17b8dbbc8aa6521493c5bc61885d
Laurent Redor <laurent.redor@obeo.fr>
2025-10-20 17:27:16 +0200

    [DRAFT] Simple failure test class added for Guava OpenRewrite #883
    Sample for the issue
    https://github.com/openrewrite/rewrite-migrate-java/issues/883

--- plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/editor/Test.java
+++ plugins/org.eclipse.sirius.diagram.ui/src-diag/org/eclipse/sirius/diagram/ui/tools/internal/editor/Test.java
@@ -2,13 +2,19 @@ package org.eclipse.sirius.diagram.ui.tools.internal.editor;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Sets;
 
 class Test {
     public static void test() {
-        final List<ClassCastException> result = new ArrayList<ClassCastException>();
         List<Exception> myExceptions = new ArrayList<Exception>();
-        result.addAll(Sets.newHashSet(Iterables.filter(myExceptions, ClassCastException.class)));
+        Predicate<Exception> isTrue = new Predicate<Exception>() {
+
+            @Override
+            public boolean apply(Exception input) {
+                return false;
+            }
+        };
+        Iterable<Exception> iterable = Iterables.filter(myExceptions, isTrue);
     }
 }
