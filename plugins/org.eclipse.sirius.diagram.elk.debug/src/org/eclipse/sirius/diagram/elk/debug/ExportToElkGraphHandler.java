1e5c98085df6c08d63b21f4eb2b89625c7964f99
Nicolas Peransin <nicolas.peransin@obeo.fr>
2026-09-09 09:54:34 +0200

    [750] ELK debug files are loaded in Workspace
    Files produced by ELK Debug would not appear in workspace unless
    Users refresh their container.
    
    Bug: https://github.com/eclipse-sirius/sirius-desktop/issues/750
    Signed-off-by: Nicolas Peransin <nicolas.peransin@obeo.fr>

--- plugins/org.eclipse.sirius.diagram.elk.debug/src/org/eclipse/sirius/diagram/elk/debug/ExportToElkGraphHandler.java
+++ plugins/org.eclipse.sirius.diagram.elk.debug/src/org/eclipse/sirius/diagram/elk/debug/ExportToElkGraphHandler.java
@@ -23,7 +23,7 @@ import java.util.Optional;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
@@ -99,9 +99,9 @@ public abstract class ExportToElkGraphHandler extends AbstractHandler {
 
         Path exportedPath;
 
-        IFile targetFolder;
+        IContainer targetFolder;
 
-        public SaveToFileWorkspaceModifyOperation(ElkNode graph, String diagramName, IFile targetFolder) {
+        public SaveToFileWorkspaceModifyOperation(ElkNode graph, String diagramName, IContainer targetFolder) {
             super(ResourcesPlugin.getWorkspace().getRuleFactory().refreshRule(targetFolder));
             this.graph = graph;
             this.diagramName = diagramName;
@@ -111,7 +111,9 @@ public abstract class ExportToElkGraphHandler extends AbstractHandler {
         @Override
         protected void execute(IProgressMonitor monitor) throws CoreException, InvocationTargetException, InterruptedException {
             exportedPath = saveToFile(graph, diagramName);
-            targetFolder.refreshLocal(IResource.DEPTH_ONE, monitor);
+            if (targetFolder != null) {
+                targetFolder.refreshLocal(IResource.DEPTH_INFINITE, monitor);
+            }
         }
 
         /**
@@ -177,7 +179,7 @@ public abstract class ExportToElkGraphHandler extends AbstractHandler {
 
         // Store the ELK graph result in a file
         Path result = null;
-        Optional<IFile> optionalTargetFolder = getTracer().getTargetFolderIfInWorkspace();
+        Optional<IContainer> optionalTargetFolder = getTracer().getTargetFolderIfInWorkspace();
         if (optionalTargetFolder.isEmpty()) {
             result = saveToFile(layoutMapping.getLayoutGraph(), getDiagramName(diagramEditPart));
         } else {
